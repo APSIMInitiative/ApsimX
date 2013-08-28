4eaa2e494d6106ab97dc42b8ea9a1bfa8ac14bbf
Justin Fainges <Justin.Fainges@csiro.au>
2013-08-28 15:54:05 +1000

    Change to R script.

--- RTestSuite/RunTest.R
+++ RTestSuite/RunTest.R
@@ -5,29 +5,26 @@ library(RSQLite)
 source("../RTestSuite/tests.R")
 
 args <- commandArgs(TRUE)
-#args <- c("C:\\ApsimX\\ApsimX\\Tests", "C:\\ApsimX\\ApsimX\\Tests")
+#args <- c("C:\\ApsimX\\ApsimX\\Tests\\", "C:\\ApsimX\\ApsimX\\Tests\\")
 
 # needed to cut the trailing '\' off... worked ok on local, but not remote.
 args[1] <- substr(args[1], 1, nchar(args[1]) - 1)
-print(args[1])
 setwd(args[1])
 
 # read control file
-print(args[1])
 wb <- loadWorkbook("sensibility.xlsx", FALSE)
 sheets <- getSheets(wb)
 
 #run tests on each sheet
 for (ind in c(1:length(sheets))){
-  # get list of sims to test in this suite TODO handle 'all'
+  dbName <- list.files(args[1], pattern="^.*\\.(db)$")
+  # get list of sims to test in this suite TODO: handle 'all' - do we want to do this in a db?
   master <- readWorksheet(wb, sheet = ind, header = FALSE)
   simsToTest <- master[master[1] == "sim", -1]
-  simsToTest <- simsToTest[sapply(simsToTest, function(x) !any(is.na(x)))] #remove NA columns
-  dbName <- list.files(args[1], pattern="^.*\\.(db)$")
+  simsToTest <- simsToTest[sapply(simsToTest, function(x) !any(is.na(x)))] #remove NA columns  
   
   for (sim in c(1:ncol(simsToTest)))
   {
-    print(as.character(simsToTest[sim]))
     db <- dbConnect(SQLite(), dbname = dbName)
     
     #get report ID and extract relevant info from table
@@ -45,13 +42,11 @@ for (ind in c(1:length(sheets))){
     #get tests to run
     tests <- master[master[1] == "test",]
     
-    for (i in c(1:nrow(tests))) {
-      
+    for (i in c(1:nrow(tests))) {    
       #get columns to run them on
       if (tests[i, "Col6"] != "all"){
          cols <- tests[i,c(grep("Col6",colnames(tests)):ncol(tests))]
          cols <- cols[sapply(cols, function(x) !any(is.na(x)))] #remove NA columns
-         print(unlist(cols))
          if (ncol(readSimOutput) > 1) 
            simOutput <- subset(readSimOutput, select=unlist(cols))
       }
@@ -61,18 +56,13 @@ for (ind in c(1:length(sheets))){
       # retrieve the test name and run it
       # test must accept a data frame
       func <- match.fun(tests$Col2[i])
-      ifelse(i == 1,
-             #testing
-       #  results <- paste(tests$Col2[i], func(simOutput, tests$Col3[i], tests$Col4[i], tests$Col5[i])),
-       #   results <- c(results, paste(tests$Col2[i], func(simOutput, tests$Col3[i], tests$Col4[i], tests$Col5[i]))))
-      
+      ifelse(i == 1,      
       results <- func(simOutput, tests$Col3[i], tests$Col4[i], tests$Col5[i]),
       results <- c(results, func(simOutput, tests$Col3[i], tests$Col4[i], tests$Col5[i])))
     }
   }
 }
-#print(results)
-print(all(results))
+print(results)
+#print(all(results))
 
 if (all(results) == FALSE) stop("One or more tests failed.")
-
--- RTestSuite/tests.R
+++ RTestSuite/tests.R
@@ -1,18 +1,20 @@
 # ApsimX Tests
 # All tests must return TRUE or FALSE
 # All tests must accept unknown arguments (by adding ...)
+# Note that R will pass the data in as a list.
+# You might need to unlist() it to get it to work.
 
 ############# AllPos #############
 # Return true if all values in x >= 0
-# @Param x:multi - numerical vector or data frame
+# @Param x:multi - list
 ##################################
 AllPos <- function(x, ...) {
   all(ifelse(x >= 0, TRUE, FALSE))
-  }
+}
 
 ############# GreaterThan ########
 # Return true if all values in x > y
-# @Param x:multi - numerical vector or data frame
+# @Param x:multi - list
 # @Param y:double - minimum allowed value (exclusive)
 ##################################
 GreaterThan <- function (x, y, ...) {
@@ -21,7 +23,7 @@ GreaterThan <- function (x, y, ...) {
 
 ############# LessThan ############
 # Return true if all values in x < y
-# @Param x:multi - numerical vector or data frame
+# @Param x:multi - list
 # @Param y:double - maximum allowed value (exclusive)
 ##################################
 LessThan <- function (x, y, ...) {
@@ -30,10 +32,21 @@ LessThan <- function (x, y, ...) {
 
 ############# Between ############
 # Return true if all values in x <= y and >= z
-# @Param x:multi - numerical vector or data frame
+# @Param x:multi - list
 # @Param y:double - minimum allowed value (inclusive)
 # @Param z:double - maximum allowed value (inclusive)
 ##################################
 Between <- function (x, y, z, ...) {
   all(ifelse(x >= as.numeric(y) &  x <= as.numeric(z), TRUE, FALSE))
+}
+
+############# Mean ############
+# Return true if mean of vector x is within +-y% of z
+# @Param x:multi - list
+# @Param y:double - percentage tolerance
+# @Param z:double - reference value
+##################################
+Mean <- function (x, y, z, ...) {
+  x <- unlist(x)
+  ifelse(mean(x) < z + z * y / 100 &  mean(x) > z - z * y / 100, TRUE, FALSE)
 }
