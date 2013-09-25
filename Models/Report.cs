7b19aa13e44b5f9de96b2a01b5ebba0716bd7a2f
Dean Holzworth <Dean.Holzworth@csiro.au>
2013-09-25 15:46:41 +1000

    Modified REPORT to handle classes/structures and arrays with changing lengths.

--- Models/Report.cs
+++ Models/Report.cs
@@ -14,13 +14,222 @@ namespace Models
     [PresenterName("UserInterface.Presenters.ReportPresenter")]
     public class Report : Model
     {
+        /// <summary>
+        /// A variable class for looking after multiple values for a single variable. 
+        /// The variable might be a scalar, an array or a class or structure. This class 
+        /// has members for returning names, types and values ready for DataStore. It 
+        /// can handle array sizes changing through a simulation. It "flattens" arrays and structures
+        /// e.g. if the variable is sw_dep and has 3 elements then
+        ///      Names -> sw_dep(1), sw_dep(2), sw_dep(3)
+        ///      Types ->    double,    double,    double
+        ///      
+        /// e.g. if the variable is a struct {double A; double B; double C;}
+        ///      Names -> struct.A, struct.B, struct.C
+        /// </summary>
+        private class VariableMember
+        {
+            private MemberInfo Info;
+            private object Model;
+            private string FullName;
+            private List<string> _Names = new List<string>();
+            private List<Type> _Types = new List<Type>();
+            private List<object> _Values = new List<object>();
+            private int MaxNumArrayElements;
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            public VariableMember(string FullName, MemberInfo Info, object Model)
+            {
+                this.FullName = FullName;
+                this.Info = Info;
+                this.Model = Model;
+            }
+
+            /// <summary>
+            /// Analyse the variable to "flatten" arrays and classes to a list of names and types.
+            /// </summary>
+            public void Analyse()
+            {
+                // Work out the type of data we're dealing with.
+                Type T = null;
+                if (Info is PropertyInfo)
+                    T = (Info as PropertyInfo).PropertyType;
+                else if (Info is FieldInfo)
+                    T = (Info as FieldInfo).FieldType;
+
+                if (T != null && _Values.Count > 0)
+                {
+                    if (T.IsArray)
+                    {
+                        // Array - calculate the maximum number of array elements and analyse each array element
+                        // on row 0 for a name and type.
+                        MaxNumArrayElements = CalcMaxNumArrayElements();
+
+                        Array Arr = _Values[0] as Array;
+                        for (int Col = 0; Col < MaxNumArrayElements; Col++)
+                        {
+                            string Heading = FullName + "(" + (Col + 1).ToString() + ")";
+                            if (Col < Arr.Length)
+                                AnalyseValue(Heading, Arr.GetValue(Col));
+                        }
+                    }
+                    else
+                    {
+                        AnalyseValue(FullName, _Values[0]);
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Return a list of names for this variable after flattening out arrays and 
+            /// structures.
+            /// </summary>
+            public string[] Names { get { return _Names.ToArray(); } }
+
+            /// <summary>
+            /// Return a list of types for this variable.
+            /// </summary>
+            public Type[] Types { get { return _Types.ToArray(); } }
+
+            /// <summary>
+            /// Return an array of values for the specified row.
+            /// </summary>
+            public object[] Values(int Row)
+            {
+                List<object> AllValues = new List<object>();
+
+                // Work out the type of data we're dealing with.
+                Type T = null;
+                if (Info is PropertyInfo)
+                    T = (Info as PropertyInfo).PropertyType;
+                else if (Info is FieldInfo)
+                    T = (Info as FieldInfo).FieldType;
+
+                if (T != null)
+                {
+                    if (T.IsArray)
+                    {
+                        // Add required columns
+                        Array Arr = _Values[Row] as Array;
+                        for (int Col = 0; Col < MaxNumArrayElements; Col++)
+                        {
+                            string Heading = FullName + "(" + (Col + 1).ToString() + ")";
+                            if (Col < Arr.Length)
+                                AddValueToList(AllValues, Row, Heading, Arr.GetValue(Col));
+                        }
+                    }
+                    else
+                    {
+                        AddValueToList(AllValues, Row, FullName, _Values[Row]);
+                    }
+                }
+                return AllValues.ToArray();
+            }
+
+            /// <summary>
+            /// Return the number of values stored in this variable.
+            /// </summary>
+            public int NumValues { get { return _Values.Count; } }
+
+            /// <summary>
+            /// Store the current value in our array of values.
+            /// </summary>
+            public void StoreValue()
+            {
+                object Value = null;
+                if (Info != null && Model != null)
+                {
+                    if (Info is FieldInfo)
+                        Value = (Info as FieldInfo).GetValue(Model);
+                    else if (Info is PropertyInfo)
+                        Value = (Info as PropertyInfo).GetValue(Model, null);
+                }
+                _Values.Add(Value);
+            }
+
+            /// <summary>
+            /// Analyse the value passed in and store it's name and type in the
+            /// Names and Types List.
+            /// </summary>
+            private void AnalyseValue(string Name, object Value)
+            {
+                Type T = Value.GetType();
+
+                // Scalar
+                if (T == typeof(DateTime) || T == typeof(string) || !T.IsClass)
+                {
+                    // Built in type.
+                    _Names.Add(Name);
+                    _Types.Add(T);
+                }
+                else
+                {
+                    // class or struct.
+                    foreach (PropertyInfo Property in Model.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                    {
+                        _Names.Add(Name + "." + Property.Name);
+                        _Types.Add(Property.PropertyType);
+                    }
+                    foreach (FieldInfo Field in Model.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public))
+                    {
+                        _Names.Add(Name + "." + Field.Name);
+                        _Types.Add(Field.FieldType);
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Add the specified Value to the AllValues list. If Value is a class then it will add 
+            /// class fields and property values as well.
+            /// </summary>
+            private void AddValueToList(List<object> AllValues, int Row, string Name, object Value)
+            {
+                Type T = Value.GetType();
+
+                // Scalar
+                if (T == typeof(DateTime) || T == typeof(string) || !T.IsClass)
+                {
+                    // Built in type.
+                    AllValues.Add(Value);
+                }
+                else
+                {
+                    // class or struct.
+                    foreach (PropertyInfo Property in Model.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                    {
+                        AllValues.Add(Property.GetValue(Value, null));
+                    }
+                    foreach (FieldInfo Field in Model.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public))
+                    {
+                        AllValues.Add(Field.GetValue(Value));
+                    }
+                }
+            }
+
+            /// <summary>
+            /// Calculate the maximum number of array elements.
+            /// </summary>
+            private int CalcMaxNumArrayElements()
+            {
+                int MaxNumValues = 0;
+                foreach (object Value in _Values)
+                {
+                    if (Value != null)
+                        MaxNumValues = Math.Max(MaxNumValues, (Value as Array).Length);
+                }
+                return MaxNumValues;
+            }
+        }
+
+        
         // Links.
         [Link] private DataStore DataStore = null;
         [Link] private Zone Paddock = null;
         [Link] private Simulation Simulation = null;
 
         // privates
-        bool HaveCreatedTable = false;
+        private List<VariableMember> Members = null;
 
         // Properties read in.
         public string[] Variables {get; set;}
@@ -34,7 +243,6 @@ namespace Models
         /// </summary>
         public override void OnInitialised()
         {
-            HaveCreatedTable = false;
             foreach (string Event in Events)
             {
                 string ComponentName = Utility.String.ParentName(Event, '.');
@@ -58,47 +266,69 @@ namespace Models
         /// </summary>
         public void OnReport()
         {
-            // Get all variable values.
-            List<object> Values = new List<object>();
-            List<string> Names = new List<string>();
-            List<Type> Types = new List<Type>();
-            foreach (string VariableName in Variables)
-            {
-                object Value = Paddock.Get(VariableName);
-                // If the value is an array then put each array value into Values individually.
-                if (Value is Array)
-                {
-                    Array Arr = Value as Array;
-                    for (int i = 0; i < Arr.Length; i++)
-                    {
-                        Names.Add(VariableName + "(" + (i+1).ToString() + ")");
-                        Values.Add(Arr.GetValue(i));
-                        Types.Add(Arr.GetValue(i).GetType());
-                    }
-                }
-                else
-                {
-                    // Scalar
-                    Values.Add(Value);
-                    Names.Add(VariableName);
-                    if (Value == null)
-                        Types.Add(typeof(int));
-                    else
-                        Types.Add(Value.GetType());
-                }
-            }
-
-            if (!HaveCreatedTable)
-            {
-                DataStore.CreateTable(Name, Names.ToArray(), Types.ToArray());
-                HaveCreatedTable = true;
-            }
-
-            DataStore.WriteToTable(Simulation.Name, Name, Values.ToArray());
+            if (Members == null)
+                FindVariableMembers();
+            foreach (VariableMember Variable in Members)
+                Variable.StoreValue();
         }
 
+        /// <summary>
+        /// Fill the Members list with VariableMember objects for each variable.
+        /// </summary>
+        private void FindVariableMembers()
+        {
+            Members = new List<VariableMember>();
+
+            List<string> Names = new List<string>();
+            List<Type> Types = new List<Type>();
+            foreach (string FullVariableName in Variables)
+            {
+                string ParentName = Utility.String.ParentName(FullVariableName);
+                string VariableName = Utility.String.ChildName(FullVariableName);
+                object Parent = Paddock.Get(ParentName);
+
+                MemberInfo FoundMember = null;
+                if (Parent != null)
+                {
+                    MemberInfo[] Info = Parent.GetType().GetMember(VariableName);
+                    if (Info.Length > 0)
+                        FoundMember = Info[0];
+                }
+                Members.Add(new VariableMember(FullVariableName, FoundMember, Parent));
+            }
+        }
+
+        /// <summary>
+        /// Simulation has completed - write the report table.
+        /// </summary>
         public override void OnCompleted()
         {
+            // Write and store a table in the DataStore
+            if (Members.Count > 0)
+            {
+                List<string> AllNames = new List<string>();
+                List<Type> AllTypes = new List<Type>();
+                foreach (VariableMember Variable in Members)
+                {
+                    Variable.Analyse();
+                    AllNames.AddRange(Variable.Names);
+                    AllTypes.AddRange(Variable.Types);
+                }
+                DataStore.CreateTable(Name, AllNames.ToArray(), AllTypes.ToArray());
+
+                List<object> AllValues = new List<object>();
+                for (int Row = 0; Row < Members[0].NumValues; Row++)
+                {
+                    AllValues.Clear();
+                    foreach (VariableMember Variable in Members)
+                    {
+                        AllValues.AddRange(Variable.Values(Row));
+                    }
+                    DataStore.WriteToTable(Simulation.Name, Name, AllValues.ToArray());
+                }
+            }
+
+            // Unsubscribe to all events.
             foreach (string Event in Events)
             {
                 string ComponentName = Utility.String.ParentName(Event, '.');
@@ -109,7 +339,6 @@ namespace Models
 
                 ComponentEvent.RemoveEventHandler(Component, new NullTypeDelegate(OnReport));
             }
-
         }
 
     }
