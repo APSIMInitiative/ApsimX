e0b73a1a0c34da94e6c301bef83b5bdac2aa4ce4
Justin Fainges <Justin.Fainges@csiro.au>
2014-01-07 14:17:35 +1000

    ID 1815
    Moved loop logic from batch command to R script.

--- Tests/RTestSuite/RunTest.bat
+++ Tests/RTestSuite/RunTest.bat
@@ -1,6 +1,17 @@
-@echo off
+rem @echo off
 
 for /R Tests %%A in (*.apsimx) do (
   "C:\Program Files\R\R-3.0.1\bin\x64\RScript" Tests\RTestSuite\RunTest.R "%%~dpnA" %OS% %BUILD_NUMBER%
-  if ERRORLEVEL == 1 (echo exit 1)
+)
+
+find "[1] FALSE" output.txt
+
+IF ERRORLEVEL 1 (
+  del output.txt
+  exit /B 0
+)
+
+IF ERRORLEVEL 0 (
+  del output.txt
+  exit /B 1
 )
