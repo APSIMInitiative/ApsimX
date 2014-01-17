9df706689e1c49fd2cf0c5f55505c1adc71d2faf
Neville <her123@csiro.au>
2014-01-17 14:40:22 +1100

    Added Memo component

---
+++ Models/Memo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using Models.Core;
+
+namespace Models
+{
+    /// <summary>
+    /// This is a memo/text component that stores user entered text information.
+    /// </summary>
+    [Serializable]
+    [ViewName("UserInterface.Views.MemoView")]
+    [PresenterName("UserInterface.Presenters.MemoPresenter")]
+    public class Memo : Model
+    {
+        public Memo()
+        {
+
+        }
+
+        [Description("Text of the memo")]
+        public string MemoText { get; set; }
+    }
+}
