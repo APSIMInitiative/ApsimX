29c72a3fbb7b444ce7246aedb8f7e3a11331f393
Neville <her123@csiro.au>
2014-05-02 10:06:38 +1000

    Improve Operations importing

--- Models/Importer/APSIMImporter.cs
+++ Models/Importer/APSIMImporter.cs
@@ -31,6 +31,9 @@ namespace Importer
         private bool SoilWaterExists = false;
         private bool MicroClimateExists = false;
 
+        // fertiliser type conversion lookups
+        Dictionary<string, string> Fertilisers;
+
         /// <summary>
         /// Original path that is substituted for %apsim%
         /// </summary>
@@ -38,6 +41,23 @@ namespace Importer
 
         public APSIMImporter()
         {
+            // fertiliser type strings that are mapped to Fertiliser.Types
+            Fertilisers = new Dictionary<string, string>();
+            Fertilisers.Add("calcite_ca", "CalciteCA");
+            Fertilisers.Add("calcite_fine", "CalciteFine");
+            Fertilisers.Add("dolomite", "Dolomite");
+            Fertilisers.Add("NO3_N", "NO3N");
+            Fertilisers.Add("NH4_N", "NH4N");
+            Fertilisers.Add("NH4NO3", "NH4NO3N");
+            Fertilisers.Add("DAP", "DAP");
+            Fertilisers.Add("MAP", "MAP");
+            Fertilisers.Add("urea_N", "UreaN");
+            Fertilisers.Add("urea_no3", "UreaNO3");
+            Fertilisers.Add("urea", "Urea");
+            Fertilisers.Add("nh4so4_n", "NH4SO4N");
+            Fertilisers.Add("rock_p", "RockP");
+            Fertilisers.Add("banded_p", "BandedP");
+            Fertilisers.Add("broadcast_p", "BroadcastP");
 
         }
 
@@ -885,6 +905,7 @@ namespace Importer
             List<string> startofdayScripts = new List<string>();
             List<string> endofdayScripts = new List<string>();
             List<string> initScripts = new List<string>();
+            List<string> unknownHandlerScripts = new List<string>();
 
             List<XmlNode> nodes = new List<XmlNode>();
             Utility.Xml.FindAllRecursivelyByType(compNode, "script", ref nodes);
@@ -912,7 +933,7 @@ namespace Importer
                     else
                     {
                         // use the StartOfDay as a default when the event name is unknown
-                        startofdayScripts.Add("// ----- " + eventNode.InnerText + " ----- \n" + textNode.InnerText);
+                        unknownHandlerScripts.Add("// ----- " + eventNode.InnerText + " ----- \n" + textNode.InnerText);
                     }
                 }
             }
@@ -955,6 +976,19 @@ namespace Importer
                 }
                 code.Append("\t\t}\n");
             }
+            if (unknownHandlerScripts.Count > 0)
+            {
+                code.Append("\t\t//[EventSubscribe(\"unknown\")]\n");
+                code.Append("\t\t//private void OnUnknown(object sender, EventArgs e)\n");
+                code.Append("\t\t//{\n");
+                foreach (string scripttext in unknownHandlerScripts)
+                {
+                    code.Append("\t\t\t/*\n");
+                    code.Append("\t\t\t\t" + scripttext + "\n");
+                    code.Append("\t\t\t*/\n");
+                }
+                code.Append("\t\t//}\n");
+            }
             code.Append("\t}\n}\n");
            
             mymanager.Code = code.ToString();   
@@ -969,21 +1003,6 @@ namespace Importer
             destParent = ImportManagerMemos(compNode, destParent);
             
             return newNode;
-        }
-
-        private string AddScriptToCode(string attr, string declar, string scriptText)
-        {
-            StringBuilder code = new StringBuilder();
-            if (attr.Length > 0)
-                code.Append("\t\t" + attr + "\n");
-            code.Append("\t\t" + declar + "\n");
-            code.Append("\t\t{\n");
-            code.Append("\t\t\t/*\n");
-            code.Append("\t\t\t\t" + scriptText + "\n");
-            code.Append("\t\t\t*/\n");
-            code.Append("\t\t}\n");
-
-            return code.ToString();
         }
 
         /// <summary>
@@ -1092,14 +1111,89 @@ namespace Importer
                 
                 XmlNode actionNode = operationNode.AppendChild(destParent.OwnerDocument.CreateElement("Action"));
 
-                childText = "";
+                childText = " ";
                 childNode = Utility.Xml.Find(oper, "action");
                 if (childNode != null)
-                    childText = "// " + childNode.InnerText;    //comment out the operations code for now
+                {
+                    childText = childNode.InnerText;    
+
+                    // parse the operation and determine if this can be converted to an apsimx function call
+                    // ** This code makes a BIG assumption that the fertiliser component has that name. Also the irrigation component!
+                    // if this name is not found then the conversion is not done as expected.
+                    childText = childText.Trim();
+                    int index = childText.IndexOf(" ");
+                    if (index > 0)
+                    {
+                        string operationOn = childText.Substring(0, index);
+                        if ((String.Compare(operationOn, "fertiliser", true) == 0) && ((childText.IndexOf(" apply") > 0) || (childText.IndexOf(" Apply") > 0)))
+                        {
+                            string amount = "0", type = "", depth = "0";
+
+                            FindTokenValue("amount", childText, ref amount);
+                            FindTokenValue("type", childText, ref type);
+
+                            string newtype;
+                            Fertilisers.TryGetValue(type, out newtype);
+                            type = "Fertiliser.Types." + newtype;
+                            FindTokenValue("depth", childText, ref depth);
+
+                            childText = String.Format("Fertiliser.Apply({0}, {1}, {2});", amount, type, depth);
+                        }
+                        else
+                        {
+                            if ((String.Compare(operationOn, "irrigation", true) == 0) && ((childText.IndexOf(" apply") > 0) || (childText.IndexOf(" Apply") > 0)))
+                            {
+                                string amount = "0";
+
+                                FindTokenValue("amount", childText, ref amount);
+
+                                childText = String.Format("Irrigation.Apply({0});", amount);
+                            }
+                            else
+                            {
+                                childText = " // " + childText; // for default comment out the operations code for now
+                            }
+                        }
+                    }
+                }
                 actionNode.InnerText = childText;
             } // next operation
 
             return newNode;
+        }
+
+        /// <summary>
+        /// Rough method for parsing an old manager script function call to obtain
+        /// the parameter values.
+        /// </summary>
+        /// <param name="name">Name of parameter</param>
+        /// <param name="line">Input function call line text</param>
+        /// <param name="value">The value found for the parameter. Contains the original value if not found.</param>
+        /// <returns>Index of the parameter name found or -1 if not found</returns>
+        private int FindTokenValue(string name, string line, ref string value)
+        {
+            int index = -1; // not found
+            index = line.IndexOf(name);
+            if (index > 0)
+            {
+                value = "";
+                int i = index + name.Length;
+                while ((i < line.Length) && (line[i] == ' '))
+                    i++;
+                // find =
+                while ((i < line.Length) && (line[i] != '='))   
+                    i++;
+                i++;
+                while ((i < line.Length) && (line[i] == ' '))
+                    i++;
+                // now build the value parameter
+                while ((i < line.Length) && (line[i] != ' ') && (line[i] != '(') && (line[i] != ','))
+                {
+                    value = value + line[i];
+                    i++;
+                }
+            }
+            return index;
         }
 
         /// <summary>
