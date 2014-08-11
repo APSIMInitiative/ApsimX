8a9ea01b9caf1702f5219df6c81c1f93c583535d
Dean Holzworth <Dean.Holzworth@csiro.au>
2014-08-11 11:27:29 +1000

    Added better cultivar handling

--- Tests/UnitTests/Test References/ModelTest.cs
+++ Tests/UnitTests/Test References/ModelTest.cs
@@ -291,7 +291,7 @@ namespace ModelTests
 
             Simulations testrunSimulations = Simulations.Read("Continuous_Wheat.apsimx");
 
-            Assert.AreEqual(189, testrunSimulations.Children.AllRecursively.Count, "Wrong number of objects in the simulation");
+            Assert.AreEqual(190, testrunSimulations.Children.AllRecursively.Count, "Wrong number of objects in the simulation");
             Assert.IsNotNull(testrunSimulations.Scope.Find("wheat"));
             Assert.IsNotNull(testrunSimulations.Scope.Find("clock"));
             Assert.IsNotNull(testrunSimulations.Scope.Find("SoilNitrogen"));
