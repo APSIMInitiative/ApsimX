201cdc1dae54dbebb49b35fc8fb1f9acc85fb5dc
Dean Holzworth <Dean.Holzworth@csiro.au>
2014-10-16 11:26:45 +1000

    Updated R version for testing

--- Tests/RTestSuite/RunTest.bat
+++ Tests/RTestSuite/RunTest.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\R\R-3.0.1\bin\x64\RScript" Tests\RTestSuite\RunTest.R %OS% %BUILD_NUMBER%
+"C:\Program Files\R\R-3.1.1\bin\x64\RScript" Tests\RTestSuite\RunTest.R %OS% %BUILD_NUMBER%
