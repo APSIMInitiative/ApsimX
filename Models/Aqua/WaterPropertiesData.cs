426935d44fb7c6d75daa57a056b1a7074ba66d88
Shaun Verrall <shaun.verrall@csiro.au>
2015-04-17 15:15:53 +1000

    Add the Pond Module and some Tests for the new Pond Module

---
+++ Models/Aqua/WaterPropertiesData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models.Aqua
+    {
+
+    /// <summary>
+    /// Stores the information about the water but not the volume of it.
+    /// </summary>
+    [Serializable]
+    public class WaterProperties
+        {
+
+        public double Temperature; 
+        public double Salinity; 
+        public double PH;
+        public double N;
+        public double P;
+        public double TSS;
+
+
+        public WaterProperties(double Temperature, double Salinity, double PH, double N, double P, double TSS)
+            {
+            this.Temperature = Temperature;
+            this.Salinity = Salinity;
+            this.PH = PH;
+            this.N = N;
+            this.P = P;
+            this.TSS = TSS;
+            }
+
+        }
+
+
+
+    }
