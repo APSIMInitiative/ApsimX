62d74a0fce9818f4ea6f200970ac6d206cd63063
Justin Fainges <Justin.Fainges@csiro.au>
2015-04-23 14:02:02 +1000

    Added Forestry model GUI

--- Models/Core/Model.cs
+++ Models/Core/Model.cs
@@ -56,6 +56,7 @@ namespace Models.Core
         [XmlElement(typeof(Clock))]
         [XmlElement(typeof(DataStore))]
         [XmlElement(typeof(Fertiliser))]
+        [XmlElement(typeof(Forestry))]
         [XmlElement(typeof(Models.PostSimulationTools.Input))]
         [XmlElement(typeof(Models.PostSimulationTools.PredictedObserved))]
         [XmlElement(typeof(Models.PostSimulationTools.TimeSeriesStats))]
---
+++ Models/Forestry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Models.Core;
+
+namespace Models
+{
+    /// <summary>
+    /// A simple agroforestry model
+    /// </summary>
+    [Serializable]
+    [ViewName("UserInterface.Views.ForestryView")]
+    [PresenterName("UserInterface.Presenters.ForestryPresenter")]
+    public class Forestry : Model
+    {
+        /// <summary>Gets or sets the ft.</summary>
+        /// <value>The ft.</value>
+        [Summary]
+        [Description("BD")]
+        public List<List<string>> Table { get; set; }
+    }
+}
