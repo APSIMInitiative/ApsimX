8eb36fa1d2f0ec163e0ea5cee44541d7eae585e1
unknown <uqadoher@QAAFI-5S3TN32.uq.edu.au>
2015-05-19 16:27:35 +1200

    UI Changes

--- UserInterface/Views/FunctionView.cs
+++ UserInterface/Views/FunctionView.cs
@@ -17,7 +17,8 @@ namespace UserInterface.Views
     {
 
         IGridView gridView { get; }
-        IGraphView graphView { get; }
+        IGridView gridViewXProp { get; }
+        //IGraphView graphView { get; }
     }
 
     public partial class FunctionView : UserControl, IFunctionView
@@ -30,31 +31,57 @@ namespace UserInterface.Views
             }
         }
 
-        public IGraphView graphView
+        public IGridView gridViewXProp
         {
             get
             {
-                return graph;
+                return gridXProp;
             }
         }
 
+        //public IGraphView graphView
+        //{
+        //    get
+        //    {
+        //        return graph;
+        //    }
+        //}
+        /// <summary>
+        /// 
+        /// </summary>
         public FunctionView()
         {
             InitializeComponent();
 
             grid.CellsChanged += new EventHandler<GridCellsChangedArgs>(gridViewCellsChanged);
-        }
 
+           // grid.
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void gridViewCellsChanged(object sender, GridCellsChangedArgs e)
         {
-            //UpdateChart();
+            UpdateChart();
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        void UpdateChart()
+        {
+            chart1.Series[0].Points.Clear();
 
-        //void UpdateChart()
-        //{
-           
+            DataTable dt = grid.DataSource;
 
-        //}
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+
+                chart1.Series[0].Points.AddXY(double.Parse(dt.Rows[i][0].ToString()), double.Parse(dt.Rows[i][1].ToString()));
+            }
+
+        }
 
         
     }
