0e81a90acae5a73939c049dcce7025985eb4f3d3
Dean Holzworth <Dean.Holzworth@csiro.au>
2015-10-01 16:05:45 +1000

    Trap exception when querying data and return null DataTable.

--- Models/DataStore.cs
+++ Models/DataStore.cs
@@ -507,7 +507,14 @@ namespace Models
         {
             Open(forWriting: false);
 
-            return Connection.ExecuteQuery(sql);
+            try
+            {
+                return Connection.ExecuteQuery(sql);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         /// <summary>Return all data from the specified simulation and table name.</summary>
