20280f46a8fe7e2b72f659dfba5492d16fa38efe
Dean Holzworth <Dean.Holzworth@csiro.au>
2016-03-02 15:45:10 +1000

    Fixed path in CreateDocumentation

--- Documentation/GenerateDocumentation.bat
+++ Documentation/GenerateDocumentation.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Generate model validation and configuration documentation
-for /D %%D in (D:\ApsimX\Tests\Validation\*) do (
+for /D %%D in (..\Tests\Validation\*) do (
     echo Generating PDF for model %%~nD
     set ModelName=%%~nD%%~xD
     ..\Bin\UserInterface.exe CreateModelDocumentation.cs
