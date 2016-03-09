ff4e66b0ff32c5f71f0428d2cbbb2d4b138840ce
unknown <ver078@JINX-SL.nexus.csiro.au>
2016-03-09 10:01:22 +1000

    Revert "Model changed to Models:"
    This reverts commit d86541f3694a9028244f5bab1ca5c2d84b26adbc.

--- Models/Core/Runner.cs
+++ Models/Core/Runner.cs
@@ -214,7 +214,7 @@
                 // For each .apsimx file - read it in and create a job for each simulation it contains.
                 string workingDirectory = Directory.GetCurrentDirectory();
                 string binDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                string apsimExe = Path.Combine(binDirectory, "Models.exe");
+                string apsimExe = Path.Combine(binDirectory, "Model.exe");
                 List<JobManager.IRunnable> jobs = new List<JobManager.IRunnable>();
                 foreach (string apsimxFileName in files)
                 {
