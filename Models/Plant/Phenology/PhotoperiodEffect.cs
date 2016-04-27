f296b647cce906f08849634681bfd79aece99786
Hamish <hamish.brown@plantandfoodco.nz>
2016-04-28 09:45:17 +1200

    minor change

--- Models/Plant/Phenology/PhotoperiodEffect.cs
+++ Models/Plant/Phenology/PhotoperiodEffect.cs
@@ -17,6 +17,10 @@ namespace Models.PMF.Phen
     [PresenterName("UserInterface.Presenters.PropertyPresenter")]
     public class PhotoPeriodEffect : Model, IFunction
     {
+        /// <summary>The phenology</summary>
+        [Link]
+        Phenology Phenology = null;
+
         /// <summary>The photoperiod</summary>
         [Link]
         IFunction Photoperiod = null;
@@ -24,7 +28,14 @@ namespace Models.PMF.Phen
         /// <summary>The Photoperiod sensitivity factor</summary>
         [Link]
         IFunction PhotoSens = null;
-  
+        
+        /// <summary>The start stage for effects</summary>
+        [Description("StartStageForEffects")]
+        public string StartStageForEffects { get; set; }
+        /// <summary>The end stage for effects</summary>
+        [Description("EndStageForEffects")]
+        public string EndStageForEffects { get; set; }
+
         /// <summary>Gets the value.</summary>
         /// <value>The value.</value>
         [Units("0-1")]
@@ -34,11 +45,13 @@ namespace Models.PMF.Phen
             {
                 double photop_eff = 1.0;
 
-                double photop_sen_factor = PhotoSens.Value * 0.002;
-                photop_eff = 1.0 - photop_sen_factor * Math.Pow(20.0 - Photoperiod.Value, 2);
-                photop_eff = Math.Max(photop_eff, 0.0);
-                photop_eff = Math.Min(photop_eff, 1.0);
-
+                if (Phenology.Between(StartStageForEffects, EndStageForEffects))
+                {
+                    double photop_sen_factor = PhotoSens.Value * 0.002;
+                    photop_eff = 1.0 - photop_sen_factor * Math.Pow(20.0 - Photoperiod.Value, 2);
+                    photop_eff = Math.Max(photop_eff, 0.0);
+                    photop_eff = Math.Min(photop_eff, 1.0);
+                }
                 return photop_eff;
             }
         }
