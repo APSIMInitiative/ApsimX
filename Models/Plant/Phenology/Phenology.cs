6468da104240276c4b899c3b9e6f9961e2a82b6e
Hamish <hamish.brown@plantandfoodco.nz>
2016-04-29 07:51:00 +1200

    Missing exception handling

--- Models/Plant/Phenology/Phenology.cs
+++ Models/Plant/Phenology/Phenology.cs
@@ -431,9 +431,10 @@ namespace Models.PMF.Phen
 
                 if (Emerged)
                     AccumulatedEmergedTT += CurrentPhase.TTForToday;
-               
-                if (Plant.IsAlive && PostPhenology != null)
-                    PostPhenology.Invoke(this, new EventArgs());
+
+                if (Plant != null)
+                    if (Plant.IsAlive && PostPhenology != null)
+                        PostPhenology.Invoke(this, new EventArgs());
 
                 Util.Debug("Phenology.CurrentPhaseName=%s", CurrentPhase.Name.ToLower());
                 Util.Debug("Phenology.CurrentStage=%f", Stage);
