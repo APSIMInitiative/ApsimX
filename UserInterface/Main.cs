62892a772ab422eec3563b8d65c6bdc50b0b5e70
DeanH <DeanH@DESKTOP-QQKB7NG>
2016-07-28 08:53:14 +1000

    Fifth iteration

--- UserInterface/Main.cs
+++ UserInterface/Main.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
-using System.IO;
-using UserInterface.Views;
-using UserInterface.Presenters;
-
-namespace UserInterface
+﻿namespace UserInterface
 {
+    using Models;
+    using Presenters;
+    using System;
+    using System.IO;
+    using System.Windows.Forms;
+    using Views;
     static class UserInterface
     {
         /// <summary>
@@ -16,10 +14,15 @@ namespace UserInterface
         [STAThread]
         static int Main(string[] args)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(Manager.ResolveManagerAssembliesEventHandler);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             MainView mainForm = new MainView();
             MainPresenter mainPresenter = new MainPresenter();
+
+            // Clean up temporary manager assemblies.
+            Models.Manager.CleanupTempAssemblies();
 
             try
             {
