8e7399bfb67c10e60e1e88ef897d292587a71983
Hamish <hamish.brown@plantandfoodco.nz>
2016-09-15 10:56:29 +1200

    Add a class to handle sub hourly water inputs

---
+++ Models/Soils/MultiPoreWater/SubHourlyData.cs
@@ -0,0 +1,26 @@
+﻿using Models.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models.Soils
+{
+    /// <summary>
+    /// Data structure that holds parameters and variables specific to each pore component in the soil horizion
+    /// </summary>
+    [Serializable]
+    public class SubHourlyData : HourlyData
+    {
+        /// <summary>
+        /// Initialise arays on construction
+        /// </summary>
+        public SubHourlyData()
+        {
+            Irrigation = new double[10];
+            Rainfall = new double[10];
+            Drainage = new double[10];
+            Infiltration = new double[10];
+        }
+    }
+}
