b33d03e06c1a6e95f7ec530726c1a2c260cfe190
DeanH <DeanH@DESKTOP-QQKB7NG>
2016-10-26 12:44:28 +1000

    Fix some graph issues

--- Models/DataStore.cs
+++ Models/DataStore.cs
@@ -425,6 +425,14 @@ namespace Models
             }
         }
 
+        /// <summary>Get a list of column names for table.</summary>
+        public string[] ColumnNames(string tableName)
+        {
+            string sql = "SELECT * FROM " + tableName + " LIMIT 1";
+            DataTable data = RunQuery(sql);
+            return DataTableUtilities.GetColumnNames(data);
+        }
+
         /// <summary>
         /// Return all data from the specified simulation and table name. If simulationName = "*"
         /// the all simulation data will be returned.
