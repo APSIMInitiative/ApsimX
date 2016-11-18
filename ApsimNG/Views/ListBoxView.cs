f9571fb0ee56810580e647d43545ed3668841673
DeanH <DeanH@DESKTOP-QQKB7NG>
2016-11-18 14:55:25 +1000

    Added italics text

--- ApsimNG/Views/ListBoxView.cs
+++ ApsimNG/Views/ListBoxView.cs
@@ -52,7 +52,7 @@ namespace UserInterface.Views
         public event EventHandler DoubleClicked;
 
         public IkonView listview;
-        private ListStore listmodel = new ListStore(typeof(string), typeof(Gdk.Pixbuf), typeof(string));
+        private ListStore listmodel = new ListStore(typeof(string), typeof(Gdk.Pixbuf), typeof(string), typeof(string));
 
         /// <summary>Constructor</summary>
         public ListBoxView(ViewBase owner) : base(owner)
@@ -63,6 +63,7 @@ namespace UserInterface.Views
             listview.TextColumn = 0;
             listview.PixbufColumn = 1;
             listview.TooltipColumn = 2;
+            listview.MarkupColumn = 3;
             listview.SelectionMode = SelectionMode.Browse;
             listview.Orientation = Gtk.Orientation.Horizontal;
             listview.RowSpacing = 0;
@@ -107,7 +108,7 @@ namespace UserInterface.Views
                     {
                         text = AddFileNameListItem(val, ref image);
                     }
-                    listmodel.AppendValues(text, image, val);
+                    listmodel.AppendValues(text, image, val, text);
                 }
             }
         }
@@ -121,7 +122,7 @@ namespace UserInterface.Views
             List<string> resourceNames = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceNames().ToList();
             List<string> largeImageNames = resourceNames.FindAll(r => r.Contains(".LargeImages."));
 
-            string result = Path.GetFileName(fileName) + "\n" + Path.GetDirectoryName(fileName);
+            string result = Path.GetFileName(fileName) + "\n<i>" + Path.GetDirectoryName(fileName) + "</i>";
 
             listview.ItemPadding = 6; // Restore padding if we have images to display
 
