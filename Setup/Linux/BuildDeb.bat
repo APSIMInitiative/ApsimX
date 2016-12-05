dab4720989ed89bf8b93ca2ffdd38749d4001329
Eric Zurcher <Eric.Zurcher@csiro.au>
2016-12-05 16:34:38 +1100

    Add script for creation of Debian install package

---
+++ Setup/Linux/BuildDeb.bat
@@ -0,0 +1,87 @@
+@echo off
+rem Get the current version number
+if Exist Version.tmp Del Version.tmp
+C:\Jenkins\Utilities\sigcheck64 -n -nobanner ..\..\Bin\Models.exe > Version.tmp
+set /p APSIM_VERSION=<Version.tmp
+for /F "tokens=1,2 delims=." %%a in ("%APSIM_VERSION%") do (set SHORT_VERSION=%%a.%%b)
+del Version.tmp
+
+rem Create a clean set of output folders
+if Exist .\DebPackage rmdir /S /Q .\DebPackage
+mkdir .\DebPackage\DEBIAN
+mkdir .\DebPackage\data\usr\local\bin
+mkdir .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Bin
+mkdir .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Examples
+
+rem Create the main execution script; must have Unix newline
+echo 2.0>.\DebPackage\debian-binary
+C:\Jenkins\Utilities\dos2unix -q .\DebPackage\debian-binary
+
+(
+echo #!/bin/sh
+echo exec /usr/bin/mono /usr/local/lib/apsim/%APSIM_VERSION%/Bin/ApsimNG.exe "$@"
+)> .\DebPackage\data\usr\local\bin\apsim
+C:\Jenkins\Utilities\dos2unix -q .\DebPackage\data\usr\local\bin\apsim
+
+rem Copy the binaries and examples to their destinations
+xcopy /S /I /Y /Q ..\..\Examples .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Examples
+xcopy /I /Y /Q ..\..\Bin\*.dll .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Bin
+xcopy /I /Y /Q ..\..\Bin\*.exe .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Bin
+xcopy /I /Y /Q ..\..\ApsimNG\Assemblies\Mono.TextEditor.dll.config .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Bin
+xcopy /I /Y /Q ..\..\ApsimNG\Assemblies\webkit-sharp.dll.config .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Bin
+xcopy /I /Y /Q ..\..\ApsimNG\Assemblies\MonoMac.dll .\DebPackage\data\usr\local\lib\apsim\%APSIM_VERSION%\Bin
+
+rem Determine the approximate size of the package
+C:\Jenkins\Utilities\du.exe -k -s DebPackage > size.temp
+for /F "tokens=1" %%i in (size.temp) do set INSTALL_SIZE=%%i
+del size.temp
+
+rem Create the control file
+(
+echo Package: apsim
+echo Version: %APSIM_VERSION%
+echo Section: science
+echo Architecture: all
+echo Priority: optional
+echo Maintainer: APSIM Initiative ^<apsim@daf.qld.gov.au^>
+echo Homepage: www.apsim.info
+echo Installed-Size: %INSTALL_SIZE%
+echo Depends: gtk-sharp2 ^(^>=2.12^), libwebkit1.1-cil, mono-runtime ^(^>=4.4^), sqlite3
+echo Description: The Agricultural Production Systems Simulator.
+echo ^ The Agricultural Production Systems sIMulator ^(APSIM^) is internationally recognised 
+echo ^ as a highly advanced simulator of agricultural systems. It contains a suite of modules 
+echo ^ which enable the simulation of systems that cover a range of plant, animal, soil, 
+echo ^ climate and management interactions. 
+)> .\DebPackage\DEBIAN\control
+
+rem Calculate the md5sums
+setlocal enabledelayedexpansion
+cd DebPackage/data
+C:\Jenkins\Utilities\fciv usr/local/bin/apsim | findstr /r /v "^//" >..\DEBIAN\md5sums
+for %%a in (usr/local/lib/apsim/%APSIM_VERSION%/Bin/*) do (
+C:\Jenkins\Utilities\fciv "usr/local/lib/apsim/%APSIM_VERSION%/Bin/%%a" | findstr /r /v "^//" > Temp.out
+set /p CHECKSUM_TMP=<Temp.out
+for /F "tokens=1" %%i in ("!CHECKSUM_TMP!") do echo %%i usr/local/lib/apsim/%APSIM_VERSION%/Bin/%%a >> ..\DEBIAN\md5sums
+del Temp.out
+)
+
+for %%a in (usr/local/lib/apsim/%APSIM_VERSION%/Examples/*) do (
+C:\Jenkins\Utilities\fciv "usr/local/lib/apsim/%APSIM_VERSION%/Examples/%%a" | findstr /r /v "^//" > Temp.out
+set /p CHECKSUM_TMP=<Temp.out
+for /F "tokens=1" %%i in ("!CHECKSUM_TMP!") do echo %%i usr/local/lib/apsim/%APSIM_VERSION%/Examples/%%a >> ..\DEBIAN\md5sums
+del Temp.out
+)
+
+for %%a in (usr/local/lib/apsim/%APSIM_VERSION%/Examples/WeatherFiles/*) do (
+C:\Jenkins\Utilities\fciv "usr/local/lib/apsim/%APSIM_VERSION%/Examples/WeatherFiles/%%a" | findstr /r /v "^//" > Temp.out
+set /p CHECKSUM_TMP=<Temp.out
+for /F "tokens=1" %%i in ("!CHECKSUM_TMP!") do echo %%i usr/local/lib/apsim/%APSIM_VERSION%/Examples/WeatherFiles/%%a >> ..\DEBIAN\md5sums
+del Temp.out
+)
+
+rem Create the tarballs and ar them together
+C:\Jenkins\Utilities\tar zcf ..\data.tar.gz .
+cd ..\DEBIAN
+C:\Jenkins\Utilities\tar zcf ..\control.tar.gz .
+cd ..
+C:\Jenkins\Utilities\ar r Apsim.deb debian-binary control.tar.gz data.tar.gz
