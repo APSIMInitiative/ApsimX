bb1cb60a3e84992013f552800d11aa0ab1017cd2
NEXUS\zur003 <Eric.Zurcher@csiro.au>
2016-12-16 13:02:44 +1100

    Add first pass of support for doing Updates on Linux

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -25,7 +25,22 @@ namespace Updater
 
                 string uninstallDirectory = args[0];
                 string newInstallDirectory = args[1];
-                DoUpate(uninstallDirectory, newInstallDirectory);
+                uint platformID = (uint)System.Environment.OSVersion.Platform;
+                if (platformID <= 3)
+                    DoWindowsUpdate(uninstallDirectory, newInstallDirectory);
+                else if (platformID == 4 || platformID == 6 || platformID == 128)
+                // Distinguishing Unix from OS X is a bit harder.
+                // We have code for doing that in APSIM.Shared, but we don't want this
+                // application to depend on any of our DLLs.
+                {
+                    int exitCode;
+                    if (ReadProcessOutput("uname", null, out exitCode).Contains("Darwin"))
+                        DoMacUpdate(uninstallDirectory, newInstallDirectory);
+                    else
+                        DoLinuxUpdate(uninstallDirectory, newInstallDirectory);
+                }
+                else
+                    throw new Exception("Could not determine execution platform!");
             }
             catch (Exception err)
             {
@@ -38,7 +53,7 @@ namespace Updater
         /// </summary>
         /// <param name="uninstallDirectory"></param>
         /// <param name="newInstallDirectory"></param>
-        private static void DoUpate(string uninstallDirectory, string newInstallDirectory)
+        private static void DoWindowsUpdate(string uninstallDirectory, string newInstallDirectory)
         {
             WaitForProcess("ApsimNG");
 
@@ -62,6 +77,82 @@ namespace Updater
             if (!File.Exists(userInterface))
                 throw new Exception("Cannot find user interface: " + userInterface);
             Process.Start(userInterface);
+        }
+
+        private static void DoMacUpdate(string uninstallDirectory, string newInstallDirectory)
+        {
+        }
+
+        private static void DoLinuxUpdate(string uninstallDirectory, string newInstallDirectory)
+        {
+            // Write the updater script to a temporary file.
+            // We assume that we are in the same folder as the .deb file, and that
+            // we have write-access to that folder.
+
+            using (StreamWriter outputFile = new StreamWriter("updater.sh", false))
+            {
+                outputFile.WriteLine("#!/bin/sh");
+                outputFile.WriteLine("sudo -v");
+                outputFile.WriteLine("if [ $? -eq 0 ]");
+                outputFile.WriteLine("then");
+                outputFile.WriteLine("  if dpkg-query -Wf'${db:Status-abbrev}' apsim 2>/dev/null | grep -q '^i'; then");
+                outputFile.WriteLine("    sudo dpkg -r apsim");
+                outputFile.WriteLine("  fi");
+                outputFile.WriteLine("  sudo dpkg -i $1");
+                outputFile.WriteLine("  if [ $? -ne 0 ]");
+                outputFile.WriteLine("  then");
+                outputFile.WriteLine("    sudo apt install -f");
+                outputFile.WriteLine("    sudo dpkg -i $1");
+                outputFile.WriteLine("  fi");
+                outputFile.WriteLine("else");
+                outputFile.WriteLine("  echo \"This update script requires superuser privileges.\"");
+                outputFile.WriteLine("  exit 1");
+                outputFile.WriteLine("fi");
+            }
+
+            int exitCode;
+            string output = ReadProcessOutput("/bin/sh", "./updater.sh ApsimSetup.deb", out exitCode);
+            if (exitCode == 0)
+            {
+                File.Delete("updater.sh");
+                // Run the user interface.
+                string apsimCmd = "/usr/local/bin/apsim";
+                if (!File.Exists(apsimCmd))
+                    throw new Exception("Cannot find apsim at: " + apsimCmd);
+                Process.Start(apsimCmd);
+            }
+            else
+                MessageBox.Show("Update failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private static string ReadProcessOutput(string name, string args, out int exitCode)
+        {
+            exitCode = -1;
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                if (args != null && args != "") p.StartInfo.Arguments = " " + args;
+                p.StartInfo.FileName = name;
+                p.Start();
+                // Do not wait for the child process to exit before
+                // reading to the end of its redirected stream.
+                // p.WaitForExit();
+                // Read the output stream first and then wait.
+                string output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                if (p.HasExited)
+                    exitCode = p.ExitCode;
+                if (output == null) output = "";
+                output = output.Trim();
+                return output;
+            }
+            catch
+            {
+                return "";
+            }
         }
 
         private static int WaitForProcess(string exeName)
