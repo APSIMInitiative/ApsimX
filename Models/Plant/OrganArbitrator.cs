dcd18fe0e3c451265caf583b30b6d5965dfbcaba
NEXUS\hut104 <neil.huth@csiro.au>
2017-01-27 17:00:23 +1000

    removed unnecessary method

--- Models/Plant/OrganArbitrator.cs
+++ Models/Plant/OrganArbitrator.cs
@@ -714,8 +714,7 @@ namespace Models.PMF
             // Calculate the total water supply across all zones.
             double waterSupply = 0;
             foreach (ZoneWaterAndN Z in zones)
-                if (Plant.Root.HaveRootsInZone(Z.Name))
-                    waterSupply += MathUtilities.Sum(Z.Water);
+               waterSupply += MathUtilities.Sum(Z.Water);
             
             // Calculate total plant water demand.
             waterDemand = 0.0;
@@ -809,17 +808,14 @@ namespace Models.PMF
                 double[] nh4Supply = null;
                 foreach (ZoneWaterAndN Z in zones)
                 {
-                    if (Plant.Root.HaveRootsInZone(Z.Name))
-                    {
-                        if (no3Supply == null)
-                            no3Supply = Z.NO3N;
-                        else
-                            no3Supply = MathUtilities.Add(no3Supply, Z.NO3N);
-                        if (nh4Supply == null)
-                            nh4Supply = Z.NH4N;
-                        else
-                            nh4Supply = MathUtilities.Add(nh4Supply, Z.NH4N);
-                    }
+                    if (no3Supply == null)
+                        no3Supply = Z.NO3N;
+                    else
+                        no3Supply = MathUtilities.Add(no3Supply, Z.NO3N);
+                    if (nh4Supply == null)
+                        nh4Supply = Z.NH4N;
+                    else
+                        nh4Supply = MathUtilities.Add(nh4Supply, Z.NH4N);
                 }
 
                 if (no3Supply != null && nh4Supply != null)
