8b4312bd13d7ac9fe85a2481d441de27fed6363a
NEXUS\zur003 <Eric.Zurcher@csiro.au>
2017-02-24 15:25:35 +1100

    Make test results grid readonly

--- ApsimNG/Presenters/PropertyPresenter.cs
+++ ApsimNG/Presenters/PropertyPresenter.cs
@@ -71,15 +71,19 @@ namespace UserInterface.Presenters
         /// <param name="explorerPresenter">The parent explorer presenter</param>
         public void Attach(object model, object view, ExplorerPresenter explorerPresenter)
         {
-            // if the model is Testable, run the test method.
-            ITestable testModel = model as ITestable;
-            if (testModel != null)
-                testModel.Test(false, true);
-
-            string[] split;
             this.grid = view as IGridView;
             this.model = model as Model;
             this.explorerPresenter = explorerPresenter;
+
+            // if the model is Testable, run the test method.
+            ITestable testModel = model as ITestable;
+            if (testModel != null)
+            {
+                testModel.Test(false, true);
+                this.grid.ReadOnly = true;
+            }
+
+            string[] split;
 
             this.FindAllProperties(this.model);
             if (this.grid.DataSource == null)
