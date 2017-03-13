be3c29b0b05ceb85cd873b80fff034ba2ed92e2b
Justin Fainges <Justin.Fainges@csiro.au>
2017-03-13 12:04:48 +1000

    Beginning SWIM integration

---
+++ Models/Soils/SWIM4/ISink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models.Soils.SWIM4
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISoil
+    {
+        int nex { get; set; }
+        void SetSinks(int n, int ns, int idrip, int idrn, double driprate, double[] dripsol, double dcond);
+        void Wsinks(double t, int[] isat, double[] var, double[] he, ref double[,] qwex, ref double[,] qwexd);
+        void Ssinks(double t, double ti, double tf, int isol, double[,] dwex, double[] c, ref double[,] qsex, ref double[,] qsexd);
+    }
+}
