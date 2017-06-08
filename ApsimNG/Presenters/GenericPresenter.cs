573f90a75e3a848cb3a5c1b56fda8e2df8930fe4
zur003 <Eric.Zurcher@csiro.au>
2017-06-08 16:02:35 +1000

    Create a "generic" presenter for those components that don't have any values to be entered or displayed. It just displays a brief description of the component.

---
+++ ApsimNG/Presenters/GenericPresenter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Core;
+using UserInterface.Views;
+
+namespace UserInterface.Presenters
+{
+    class GenericPresenter : IPresenter
+    {
+        private Model model;
+        private IHTMLView genericView;
+        private ExplorerPresenter ExplorerPresenter;
+
+        public void Attach(object model, object view, ExplorerPresenter explorerPresenter)
+        {
+            this.model = model as Model;
+            this.genericView = view as IHTMLView;
+            this.ExplorerPresenter = explorerPresenter;
+
+
+            // Just how much documentation do we want to generate?
+            // For now, let's just use the component name and a basic description.
+
+            // It's slightly simpler to generate Markdown for this, but it
+            // would be pretty easy to build this directly as HTML
+            List<AutoDocumentation.ITag> tags = new List<AutoDocumentation.ITag>();
+            AutoDocumentation.GetClassDescription(this.model, tags, 0);
+
+            StringBuilder contents = new StringBuilder("## " + this.model.Name + "\r\n");
+            foreach (AutoDocumentation.ITag tag in tags)
+            {
+                if (tag is AutoDocumentation.Heading)
+                {
+                    contents.Append("\r\n### ");
+                    contents.Append((tag as AutoDocumentation.Heading).text);
+                }
+                else if (tag is AutoDocumentation.Paragraph)
+                {
+                    contents.Append("\r\n#### ");
+                    contents.Append((tag as AutoDocumentation.Paragraph).text.Replace("\r\n", " "));
+                }
+            }
+
+            MarkdownDeep.Markdown markDown = new MarkdownDeep.Markdown();
+            markDown.ExtraMode = true;
+
+            string html = markDown.Transform(contents.ToString());
+            genericView.SetContents(html, false, false);
+        }
+
+        public void Detach()
+        {
+        }
+    }
+}
