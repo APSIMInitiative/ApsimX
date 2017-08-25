949205a6f3e1ed31119215dc0e3de16906ae705a
Neville <neville.herrmann@csiro.au>
2017-08-25 16:28:33 +1000

    Fixed more style issues

--- ApsimNG/Presenters/ExperimentPresenter.cs
+++ ApsimNG/Presenters/ExperimentPresenter.cs
@@ -1,63 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Models.Factorial;
-using UserInterface.Views;
-using Models.Core;
-using APSIM.Shared.Utilities;
-using Models.Core.Runners;
+﻿// -----------------------------------------------------------------------
+// <copyright file="ExperimentPresenter.cs"  company="APSIM Initiative">
+//     Copyright (c) APSIM Initiative
+// </copyright>
+// -----------------------------------------------------------------------
 
 namespace UserInterface.Presenters
 {
+    using System;
+    using APSIM.Shared.Utilities;
+    using Models.Core;
+    using Models.Factorial;
+    using Views;
+
     /// <summary>
     /// Connects a Experiment model with an readonly memo view.
     /// </summary>
     public class ExperimentPresenter : IPresenter
     {
-        private Experiment Experiment;
-        private IMemoView ListView;
-        private ExplorerPresenter ExplorerPresenter;
+        /// <summary>
+        /// Experiment object
+        /// </summary>
+        private Experiment experiment;
 
-        public void Attach(object Model, object View, ExplorerPresenter explorerPresenter)
+        /// <summary>
+        /// The item list
+        /// </summary>
+        private IMemoView listView;
+
+        /// <summary>
+        /// The presenter
+        /// </summary>
+        private ExplorerPresenter explorerPresenter;
+
+        /// <summary>
+        /// Attach the view
+        /// </summary>
+        /// <param name="model">The model</param>
+        /// <param name="view">The view</param>
+        /// <param name="explorerPresenter">The presenter</param>
+        public void Attach(object model, object view, ExplorerPresenter explorerPresenter)
         {
-            Experiment = Model as Experiment;
-            ListView = View as IMemoView;
-            ExplorerPresenter = explorerPresenter;
+            this.experiment = model as Experiment;
+            this.listView = view as IMemoView;
+            this.explorerPresenter = explorerPresenter;
 
-            string[] allNames = Experiment.Names();
-            ListView.MemoLines = allNames;
-            ListView.LabelText = "Listed below are names of the " + allNames.Length.ToString() + " simulations that this experiment will create";
-            ListView.ReadOnly = true;
-            ListView.AddContextAction("Run APSIM", OnRunApsimClick);
+            string[] allNames = this.experiment.Names();
+            this.listView.MemoLines = allNames;
+            this.listView.LabelText = "Listed below are names of the " + allNames.Length.ToString() + " simulations that this experiment will create";
+            this.listView.ReadOnly = true;
+            this.listView.AddContextAction("Run APSIM", this.OnRunApsimClick);
         }
 
+        /// <summary>
+        /// Detach the presenter
+        /// </summary>
         public void Detach()
         {
-
         }
 
         /// <summary>
         /// User has clicked RunAPSIM
         /// </summary>
+        /// <param name="sender">Sender button</param>
+        /// <param name="e">Event details</param>
         private void OnRunApsimClick(object sender, EventArgs e)
         {
             try
             {
-                Simulation simulation = Experiment.CreateSpecificSimulation(ListView.MemoLines[ListView.CurrentPosition.Y]);
-                JobManager.IRunnable job = Runner.ForSimulations(ExplorerPresenter.ApsimXFile, simulation, false);
+                Simulation simulation = this.experiment.CreateSpecificSimulation(this.listView.MemoLines[this.listView.CurrentPosition.Y]);
+                JobManager.IRunnable job = Runner.ForSimulations(this.explorerPresenter.ApsimXFile, simulation, false);
 
-                Commands.RunCommand run = new Commands.RunCommand(job, 
+                Commands.RunCommand run = new Commands.RunCommand(
+                                                                  job, 
                                                                   simulation.Name,
-                                                                  ExplorerPresenter,
+                                                                  this.explorerPresenter,
                                                                   false);
                 run.Do(null);
             }
             catch (Exception err)
             {
-                ExplorerPresenter.MainPresenter.ShowMessage(err.Message, Models.DataStore.ErrorLevel.Error);
+                this.explorerPresenter.MainPresenter.ShowMessage(err.Message, Models.DataStore.ErrorLevel.Error);
             }
         }
-
     }
 }
