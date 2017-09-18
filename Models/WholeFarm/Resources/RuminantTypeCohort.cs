9d7aa5849f6bb49b7508b92b1518c0b0b8e92274
lie112 <lie112@CODER-DA>
2017-09-18 15:13:53 +0930

    WF Shifted RunimantInitialCohorts model to own file

--- Models/WholeFarm/Resources/RuminantTypeCohort.cs
+++ Models/WholeFarm/Resources/RuminantTypeCohort.cs
@@ -9,34 +9,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Models.WholeFarm.Resources
 {
-	/// <summary>
-	/// Holder for all initial ruminant cohorts
-	/// </summary>
-	[Serializable]
-	[ViewName("UserInterface.Views.GridView")]
-	[PresenterName("UserInterface.Presenters.PropertyPresenter")]
-	[ValidParent(ParentType = typeof(RuminantType))]
-	public class RuminantInitialCohorts : Model
-	{
-		/// <summary>
-		/// Create the individual ruminant animals for this Ruminant Type (Breed)
-		/// </summary>
-		/// <returns></returns>
-		public List<Ruminant> CreateIndividuals()
-		{
-			List<Ruminant> Individuals = new List<Ruminant>();
-			foreach (RuminantTypeCohort cohort in Apsim.Children(this, typeof(RuminantTypeCohort)))
-			{
-				Individuals.AddRange(cohort.CreateIndividuals());
-			}
-			return Individuals;
-		}
-	}
 
-	/// <summary>
-	/// This stores the initialisation parameters for a Cohort of a specific Ruminant Type.
-	/// </summary>
-	[Serializable]
+    /// <summary>
+    /// This stores the initialisation parameters for a Cohort of a specific Ruminant Type.
+    /// </summary>
+    [Serializable]
 	[ViewName("UserInterface.Views.GridView")]
 	[PresenterName("UserInterface.Presenters.PropertyPresenter")]
 	[ValidParent(ParentType = typeof(RuminantInitialCohorts))]
