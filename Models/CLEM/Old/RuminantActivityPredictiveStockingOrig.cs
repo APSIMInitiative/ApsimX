10a7b07a7a3b024dbe79b0aa27b101966b4a49a4
lie112 <lie112@CODER-DA>
2018-01-18 15:32:39 +0930

    CLEM  updated predictive stocking

--- Models/CLEM/Old/RuminantActivityPredictiveStockingOrig.cs
+++ Models/CLEM/Old/RuminantActivityPredictiveStockingOrig.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
-using Models.CLEM.Groupings;
 
 namespace Models.CLEM.Activities
 {
@@ -22,17 +21,17 @@ namespace Models.CLEM.Activities
     [ValidParent(ParentType = typeof(ActivitiesHolder))]
     [ValidParent(ParentType = typeof(ActivityFolder))]
     [Description("This activity manages ruminant stocking during the dry season based upon wet season pasture biomass. It requires a RuminantActivityBuySell to undertake the sales and removal of individuals.")]
-    public class RuminantActivityPredictiveStocking: CLEMRuminantActivityBase, IValidatableObject
+    public class RuminantActivityPredictiveStockingOrig: CLEMRuminantActivityBase
     {
         [Link]
         Clock Clock = null;
 
-        ///// <summary>
-        ///// Herd to manage for dry season pasture availability
-        ///// </summary>
-        //[Description("Name of herd to manage")]
-        //[Required]
-        //public string HerdName { get; set; }
+        /// <summary>
+        /// Herd to manage for dry season pasture availability
+        /// </summary>
+        [Description("Name of herd to manage")]
+        [Required]
+        public string HerdName { get; set; }
 
         /// <summary>
         /// Month for assessing dry season feed requirements
@@ -55,78 +54,40 @@ namespace Models.CLEM.Activities
         [Required, Range(0, double.MaxValue, ErrorMessage = "Value must be a greter than or equal to 0")]
         public double FeedLowLimit { get; set; }
 
-        // minimum no that can be sold off... now controlled by sale and transport activity 
+        /// minimum no that can be sold off... now controlled by sale and transport activity 
 
-        ///// <summary>
-        ///// Minimum breedeer age allowed to be sold
-        ///// </summary>
-        //[Description("Minimum breedeer age allowed to be sold")]
-        //[Required, Range(0, double.MaxValue, ErrorMessage = "Value must be a greter than or equal to 0")]
-        //public double MinimumBreederAgeLimit { get; set; }
+        /// <summary>
+        /// Minimum breedeer age allowed to be sold
+        /// </summary>
+        [Description("Minimum breedeer age allowed to be sold")]
+        [Required, Range(0, double.MaxValue, ErrorMessage = "Value must be a greter than or equal to 0")]
+        public double MinimumBreederAgeLimit { get; set; }
 
         // restock proportion. I don't understand this.
         // Maximum % restock breeders/age group
 
-        ///// <summary>
-        ///// Allow dry cows to be sold if feed shortage
-        ///// </summary>
-        //[Description("Allow dry cows to be sold if feed shortage")]
-        //[Required]
-        //public bool SellDryCows { get; set; }
-
-        ///// <summary>
-        ///// Allow wet cows to be sold if feed shortage
-        ///// </summary>
-        //[Description("Allow wet cows to be sold if feed shortage")]
-        //[Required]
-        //public bool SellWetCows { get; set; }
-
-        ///// <summary>
-        ///// Allow steers to be sold if feed shortage
-        ///// </summary>
-        //[Description("Allow steers to be sold if feed shortage")]
-        //[Required]
-        //public bool SellSteers { get; set; }
+        /// <summary>
+        /// Allow dry cows to be sold if feed shortage
+        /// </summary>
+        [Description("Allow dry cows to be sold if feed shortage")]
+        [Required]
+        public bool SellDryCows { get; set; }
 
         /// <summary>
-        /// Validate this model
+        /// Allow wet cows to be sold if feed shortage
         /// </summary>
-        /// <param name="validationContext"></param>
-        /// <returns></returns>
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            // check that this model contains children RuminantDestockGroups with filters
-            var results = new List<ValidationResult>();
-            // check that this activity contains at least one RuminantDestockGroups group with filters
-            bool destockGroupFound = false;
-            foreach (RuminantDestockGroup item in this.Children.Where(a => a.GetType() == typeof(RuminantDestockGroup)))
-            {
-                foreach (RuminantFilter filter in item.Children.Where(a => a.GetType() == typeof(RuminantFilter)))
-                {
-                    destockGroupFound = true;
-                    break;
-                }
-                if (destockGroupFound) break;
-            }
+        [Description("Allow wet cows to be sold if feed shortage")]
+        [Required]
+        public bool SellWetCows { get; set; }
 
-            if (!destockGroupFound)
-            {
-                string[] memberNames = new string[] { "Ruminant destocking group" };
-                results.Add(new ValidationResult("At least one RuminantDestockGroup with RuminantFilter must be present under this RuminantActivityPredictiveStocking activity", memberNames));
-            }
-            return results;
-        }
+        /// <summary>
+        /// Allow steers to be sold if feed shortage
+        /// </summary>
+        [Description("Allow steers to be sold if feed shortage")]
+        [Required]
+        public bool SellSteers { get; set; }
 
-        /// <summary>An event handler to allow us to initialise ourselves.</summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        [EventSubscribe("CLEMInitialiseActivity")]
-        private void OnCLEMInitialiseActivity(object sender, EventArgs e)
-        {
-            this.InitialiseHerd(false, true);
-        }
-
-        /// <summary>An event handler to call for changing stocking based on prediced pasture biomass</summary>
+        /// <summary>An event handler to call for all resources other than food for feeding activity</summary>
         /// <param name="sender">The sender.</param>
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         [EventSubscribe("CLEMAnimalStock")]
@@ -137,16 +98,13 @@ namespace Models.CLEM.Activities
             {
                 // calculate dry season pasture available for each managed paddock holding stock not flagged for sale
                 RuminantHerd ruminantHerd = Resources.RuminantHerd();
-                foreach (var paddockGroup in ruminantHerd.Herd.Where(a => a.Location != "").GroupBy(a => a.Location))
+                foreach (var paddockGroup in ruminantHerd.Herd.Where(a => a.Location != "" & a.SaleFlag == HerdChangeReason.None).GroupBy(a => a.Location))
                 {
-                    // multiple breeds are currently not supported as we need to work out what to do with diferent AEs
-                    if(paddockGroup.GroupBy(a => a.Breed).Count() > 1)
-                    {
-                        throw new ApsimXException(this, "Dry season destocking paddocks with multiple breeds is currently not supported\nActivity:"+this.Name+", Paddock: "+paddockGroup.Key);
-                    }
-
-                    // total adult equivalents not marked for sale of all breeds on pasture for utilisation
-                    double AETotal = paddockGroup.Where(a => a.SaleFlag == HerdChangeReason.None).Sum(a => a.AdultEquivalent);
+                    // total adult equivalents of all breeds on pasture for utilisation
+                    double AETotal = paddockGroup.Sum(a => a.AdultEquivalent);
+                    // determine AE marked for sale and purchase of managed herd
+                    double AEmarkedForSale = paddockGroup.Where(a => a.ReadyForSale & a.HerdName == HerdName).Sum(a => a.AdultEquivalent);
+                    double AEPurchase = ruminantHerd.PurchaseIndividuals.Where(a => a.Location == paddockGroup.Key & a.HerdName == HerdName).Sum(a => a.AdultEquivalent);
 
                     double ShortfallAE = 0;
                     // Determine total feed requirements for dry season for all ruminants on the pasture
@@ -166,7 +124,7 @@ namespace Models.CLEM.Activities
                     // Shortfall in Fodder in kg per hectare
                     double pastureShortFallKgHa = pastureBiomass / pasture.Area;
                     pastureShortFallKgHa = Math.Max(0, pastureShortFallKgHa - FeedLowLimit);
-                    // Shortfall in Fodder in kg for paddock
+                    // Shortfalll in Fodder in kg for paddock
                     double pastureShortFallKg = pastureShortFallKgHa * pasture.Area;
 
                     if (pastureShortFallKg == 0) return;
@@ -187,40 +145,83 @@ namespace Models.CLEM.Activities
             // move to underutilised paddocks
             // TODO: This can be added later as an activity including spelling
 
-            // remove all potential purchases from list as they can't be supported.
-            // This does not change the shortfall AE as they were not counted in TotalAE pressure.
+            // remove potential purchases from list
             RuminantHerd ruminantHerd = Resources.RuminantHerd();
-            ruminantHerd.PurchaseIndividuals.RemoveAll(a => a.Location == PaddockName);
-
-            // remove individuals to sale as specified by destock groups
-            foreach (RuminantDestockGroup item in this.Children.Where(a => a.GetType() == typeof(RuminantDestockGroup)))
+            List<Ruminant> purchases = ruminantHerd.PurchaseIndividuals.Where(a => a.Location == PaddockName & a.HerdName == HerdName).ToList();
+            while (purchases.Count() > 0 & AEforSale > 0)
             {
-                // works with current filtered herd to obey filtering.
-                List<Ruminant> herd = this.CurrentHerd(false).Where(a => a.Location == PaddockName & !a.ReadyForSale).ToList();
-                herd = herd.Filter(item);
-                int cnt = 0;
-                while (cnt < herd.Count() & AEforSale > 0)
+                AEforSale -= purchases[0].AdultEquivalent;
+                purchases.RemoveAt(0);
+                if (AEforSale < purchases.Min(a => a.AdultEquivalent))
                 {
-                    AEforSale -= herd[cnt].AdultEquivalent;
-                    herd[cnt].SaleFlag = HerdChangeReason.DestockSale;
-                    if (AEforSale < herd.Min(a => a.AdultEquivalent))
+                    AEforSale = 0;
+                }
+            }
+            if (AEforSale <= 0) return;
+
+            // adjust remaining herd
+            // remove steers
+            if (this.SellSteers)
+            {
+                List<RuminantMale> steers = ruminantHerd.Herd.Where(a => a.Location == PaddockName & a.HerdName == HerdName & a.Gender == Sex.Male).Cast<RuminantMale>().Where(a => a.BreedingSire == false).ToList();
+                int cnt = 0;
+                while (cnt < steers.Count() & AEforSale > 0)
+                {
+                    AEforSale -= steers[cnt].AdultEquivalent;
+                    steers[cnt].SaleFlag = HerdChangeReason.DestockSale;
+                    if (AEforSale < steers.Min(a => a.AdultEquivalent))
                     {
                         AEforSale = 0;
                     }
                     cnt++;
                 }
-                if (AEforSale <= 0) return;
+            }
+            if (AEforSale <= 0) return;
+
+            // remove additional dry breeders
+            if (this.SellDryCows)
+            {
+                // find dry cows not already marked for sale
+                List<RuminantFemale> drybreeders = ruminantHerd.Herd.Where(a => a.Location == PaddockName & a.HerdName == HerdName & a.Gender == Sex.Female & a.SaleFlag == HerdChangeReason.None).Cast<RuminantFemale>().Where(a => a.DryBreeder == true).ToList();
+                int cnt = 0;
+                while (cnt < drybreeders.Count() & AEforSale > 0)
+                {
+                    AEforSale -= drybreeders[cnt].AdultEquivalent;
+                    drybreeders[cnt].SaleFlag = HerdChangeReason.DestockSale;
+                    if (AEforSale < drybreeders.Min(a => a.AdultEquivalent))
+                    {
+                        AEforSale = 0;
+                    }
+                    cnt++;
+                }
+            }
+            if (AEforSale <= 0) return;
+
+            // remove wet breeders with no calf
+            // currently ignore pregant
+            // is lactating with no calves are sold.
+
+            // TODO manage calves from sold wet breeders. eg move to yards
+            if (this.SellWetCows)
+            {
+                // remove wet cows
+                // find wet cows not already marked for sale
+                List<RuminantFemale> wetbreeders = ruminantHerd.Herd.Where(a => a.Location == PaddockName & a.HerdName == HerdName & a.Gender == Sex.Female & a.SaleFlag == HerdChangeReason.None).Cast<RuminantFemale>().Where(a => a.IsLactating == true & a.SucklingOffspring.Count() == 0).ToList();
+                int cnt = 0;
+                while (cnt < wetbreeders.Count() & AEforSale > 0)
+                {
+                    AEforSale -= wetbreeders[cnt].AdultEquivalent;
+                    wetbreeders[cnt].SaleFlag = HerdChangeReason.DestockSale;
+                    if (AEforSale < wetbreeders.Min(a => a.AdultEquivalent))
+                    {
+                        AEforSale = 0;
+                    }
+                    cnt++;
+                }
             }
 
-            // Possible destock groups
-            // Steers, Male, Not BreedingSire, > Age
-            // Dry Cows, IsDryBreeder
-            // Breeders, IsBreeder, !IsPregnant, > Age
-            // Underweight ProportionOfMaxWeight < 0.6
-            
-            // handling of sucklings with sold female is in RuminantActivityBuySell
-
             // buy or sell is handled by the buy sell activity
+
         }
 
         /// <summary>
@@ -237,7 +238,7 @@ namespace Models.CLEM.Activities
         /// </summary>
         public override void DoActivity()
         {
-            return;
+            return; ;
         }
 
         /// <summary>
@@ -278,6 +279,7 @@ namespace Models.CLEM.Activities
             if (ActivityPerformed != null)
                 ActivityPerformed(this, e);
         }
+
 
     }
 }
