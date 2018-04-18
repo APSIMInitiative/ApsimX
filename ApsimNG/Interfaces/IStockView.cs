15ca0529e1cef1c0e222d1d490db9a2b3e8a2a14
her123 <neville.herrmann@csiro.au>
2018-04-18 17:07:55 +1000

    Added view and presenter

---
+++ ApsimNG/Interfaces/IStockView.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IStockView.cs" company="APSIM Initiative">
+//     Copyright (c) APSIM Initiative
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace UserInterface.Interfaces
+{
+    using System;
+    using Models.GrazPlan;
+
+
+    /// <summary>
+    /// Interface for a supplement view.
+    /// </summary>
+    public interface IStockView
+    {
+
+    }
+}
---
+++ ApsimNG/Views/StockView.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="StockView.cs" company="APSIM Initiative">
+//     Copyright (c) APSIM Initiative
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace UserInterface.Views
+{
+    using System;
+    using System.Collections.Generic;
+    using Gtk;
+    using Interfaces;
+    using Models.GrazPlan;   // For access to the TSuppAttribute enumeration
+
+    public class StockView : ViewBase, IStockView
+    {
+
+        public StockView(ViewBase owner) : base(owner)
+        {
+
+        }
+    }
+}
