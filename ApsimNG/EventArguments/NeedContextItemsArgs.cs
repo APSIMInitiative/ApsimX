bd5ab75340c0366994f3eacbb5361db9d948d929
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-06-12 12:35:14 +1000

    Allow for spaces and underscores in node names when generating intellisense.

--- ApsimNG/EventArguments/NeedContextItemsArgs.cs
+++ ApsimNG/EventArguments/NeedContextItemsArgs.cs
@@ -282,7 +282,7 @@ namespace UserInterface.EventArguments
         private static object GetNodeFromPath(Model relativeTo, string objectName)
         {
             
-            string modelNamePattern = @"\[([A-Za-z]+[A-Za-z0-9]*)\]";
+            string modelNamePattern = @"\[([A-Za-z]+[A-Za-z0-9\s_]*)\]";
             var matches = System.Text.RegularExpressions.Regex.Matches(objectName, modelNamePattern);
             if (matches.Count <= 0)
                 return null;
