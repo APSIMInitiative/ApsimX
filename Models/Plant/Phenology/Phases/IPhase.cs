5350efdc08412d5a0ce4b240cd6e25db610a7dfa
Hamish Brown <hamish.brown@plantandfood.co.nz>
2018-07-24 19:19:43 +1200

    Put phase classes into folder and create IPhase class

---
+++ Models/Plant/Phenology/Phases/IPhase.cs
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------
+// <copyright file="IFunction.cs" company="APSIM Initiative">
+//     Copyright (c) APSIM Initiative
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Models.PMF.Phen
+{
+    using System.IO;
+    
+    /// <summary>Interface for a function</summary>
+    public interface IPhase
+    {
+        /// <summary>  /// Gets or sets the name of the model  /// </summary>
+        string Name { get; set; }
+
+        /// <summary>The start</summary>
+        string Start { get; set; }
+
+        /// <summary>The end</summary>
+        string End { get; set; }
+       
+        /// <summary> Fraction of progress through the phase  /// </summary>
+        double FractionComplete { get; set; }
+
+        /// <summary>Resets the phase.</summary>
+        void ResetPhase();
+
+        /// <summary> Write summary to file each time a phase completes  /// </summary>
+        void WriteSummary(TextWriter writer);
+
+    }
+}
