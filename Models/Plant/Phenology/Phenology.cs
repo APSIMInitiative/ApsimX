ef2b1ff4c2e9c32a8d0f6f04be6e31909f1f9391
Hamish Brown <hamish.brown@plantandfood.co.nz>
2018-07-26 10:32:11 +1200

    tidying up phenology class

--- Models/Plant/Phenology/Phenology.cs
+++ Models/Plant/Phenology/Phenology.cs
@@ -29,87 +29,10 @@ namespace Models.PMF.Phen
     /// <summary>
     /// This model simulates the development of the crop through successive developmental <i>phases</i>. Each phase is bound by distinct growth <i>stages</i>. Phases often require a target to be reached to signal movement to the next phase. Differences between cultivars are specified by changing the values of the default parameters shown below.
     /// </summary>
-    /// \warning An \ref Models.PMF.Phen.EndPhase "EndPhase" model 
-    /// should be included as the last child of 
-    /// \ref Models.PMF.Phen.Phenology "Phenology" model
-    /// \pre A Models.Clock "Clock" model has to exist.
-    /// \retval CurrentPhaseName The current phase name.
-    /// \retval CurrentStageName The current stage name. 
-    /// Return name of parameter \p Start of 
-    /// \ref Models.PMF.Phen.Phase "Phase" model 
-    /// on the day of phase starting, or ? in other days.
-    /// \retval Stage A one based stage number (start from 1).
-    /// \retval DaysAfterSowing The day after sowing. 
-    /// \retval FractionInCurrentPhase The fraction in current phase (0-1).
-    /// <remarks>
-    /// Generally, the plant phenology is divided into \f$N_{p}\f$ 
-    /// phases (i.e. \ref Models.PMF.Phen.Phase "Phase" functions) and stages 
-    /// (children number of Phenology model, only count 
-    /// \ref Models.PMF.Phen.Phase "Phase" models).
-    /// 
-    /// Any models inherited from \ref Models.PMF.Phen.Phase "Phase" models 
-    /// could be used as children of Phenology model, except the last phase,
-    /// which should be the \ref Models.PMF.Phen.EndPhase "EndPhase" model.
-    /// The stage names are defined by the \p Start and \p End parameters 
-    /// in each \ref Models.PMF.Phen.Phase "Phase" model. Consequently,
-    /// The parameter value of \p Start should equal to the parameter value
-    /// of \p End of previous \ref Models.PMF.Phen.Phase "Phase" model,
-    /// except for the first phase. The parameter value of \p End 
-    /// should equal to the parameter value of \p Start of next 
-    /// \ref Models.PMF.Phen.Phase "Phase" function, except for the last phase. 
-    /// The equality of stage and phase number raise from the last phase,
-    /// which is \ref Models.PMF.Phen.EndPhase "EndPhase" model and the 
-    /// \p End stage is unused. 
-    /// 
-    /// For example, the wheat phenology model defines 9 stages from sowing to maturity.
-    /// See the diagram below for definitions of stages and phases, 
-    /// and models used for each phase.
-    /// 
-    /// \startuml
-    /// Sowing -> Germination : Germinating
-    /// Germination -> Emergence : Emerging
-    /// Emergence ->  TerminalSpikelet : Vegetative
-    /// TerminalSpikelet-> FlagLeaf : FloralInitiation\nToFlagLeaf
-    /// FlagLeaf -> Flowering : Spike\nDevelopment
-    /// Flowering -> StartGrainFill: Grain\nDevelopment
-    /// StartGrainFill -> EndGrainFill : GrainFilling
-    /// EndGrainFill -> Maturity : Maturing
-    /// Maturity -> Unused: ReadyFor\nHarvesting
-    /// 
-    /// note over Sowing, Germination : GerminatingPhase
-    /// note over Germination, Emergence : EmergingPhase
-    /// note over Emergence, TerminalSpikelet : GenericPhase
-    /// note over TerminalSpikelet, FlagLeaf : LeafAppearancePhase
-    /// note over FlagLeaf, Flowering : GenericPhase
-    /// note over Flowering, StartGrainFill: GenericPhase
-    /// note over StartGrainFill, EndGrainFill : GenericPhase
-    /// note over EndGrainFill, Maturity : GenericPhase
-    /// note over Maturity, Unused: EndPhase
-    /// \enduml
-    /// 
-    /// On commencing simulation and sowing
-    /// ----------------
-    /// On commencing simulation and sowing
-    /// \ref Models.PMF.Phen.Phenology "Phenology" function and all related 
-    /// \ref Models.PMF.Phen.Phase "Phase" functions will be reset.
-    /// On harvest
-    /// -----------------
-    /// On harvest, \ref Models.PMF.Phen.Phenology "Phenology" will 
-    /// jump to the last phase function i.e. \ref Models.PMF.Phen.EndPhase 
-    /// "EndPhase" function.
-    /// On daily
-    /// -----------------
-    /// Phenology function performs a daily time step function, get the current 
-    /// phase to do its development for the day. If thermal time is leftover after 
-    /// Phase is progressed, and the time step for the subsequent phase is calculated 
-    /// using leftover thermal time.
-    /// 
-    /// </remarks>
     [Serializable]
     [ValidParent(ParentType = typeof(Plant))]
     public class Phenology : Model, ICustomDocumentation
     {
-        #region Links
         [Link]
         private Plant Plant = null;
 
@@ -117,17 +40,14 @@ namespace Models.PMF.Phen
         [Link]
         private Clock Clock = null;
 
-        /// <summary>The rewind due to biomass removed</summary>
-        [ChildLinkByName(IsOptional = true)]
-        private IFunction RewindDueToBiomassRemoved = null;
-
         /// <summary>The summary</summary>
         [Link]
         ISummary Summary = null;
 
-        #endregion
+        /// <summary>The rewind due to biomass removed</summary>
+        [ChildLinkByName(IsOptional = true)]
+        private IFunction RewindDueToBiomassRemoved = null;
 
-        #region Events
         /// <summary>Occurs when [phase changed].</summary>
         public event EventHandler<PhaseChangedType> PhaseChanged;
 
@@ -139,9 +59,7 @@ namespace Models.PMF.Phen
 
         /// <summary>Occurs when daily phenology timestep completed</summary>
         public event EventHandler PostPhenology;
-        #endregion
 
-        #region Parameters
         /// <summary>The thermal time</summary>
         [Link]
         public IFunction ThermalTime = null;
@@ -150,10 +68,10 @@ namespace Models.PMF.Phen
         [XmlIgnore]
         //public IPhase[] Phases { get; private set; }
         private List<IPhase> Phases = new List<IPhase>();
-        #endregion
 
-        #region States
+
         /// <summary>The current phase index</summary>
+        [XmlIgnore]
         private int CurrentPhaseIndex;
         /// <summary>The Thermal time accumulated tt</summary>
         [XmlIgnore]
@@ -162,14 +80,19 @@ namespace Models.PMF.Phen
         [XmlIgnore]
         public double AccumulatedEmergedTT { get; set; }
         /// <summary>The currently on first day of phase.  This is an array that lists all the stages that are pased on this day</summary>
+        [XmlIgnore]
         private string[] CurrentlyOnFirstDayOfPhase = new string[] {"","","","","",""};
         /// <summary>The number of stages that have been passed today</summary>
+        [XmlIgnore]
         private int StagesPassedToday = 0;
         /// <summary>The just initialised</summary>
+        [XmlIgnore]
         private bool JustInitialised = true;
         /// <summary>The fraction biomass removed</summary>
+        [XmlIgnore]
         public double FractionBiomassRemoved { get; set; }
         /// <summary>The sow date</summary>
+        [XmlIgnore]
         private DateTime SowDate = DateTime.MinValue;
         /// <summary>The emerged</summary>
         [XmlIgnore]
@@ -177,46 +100,17 @@ namespace Models.PMF.Phen
         /// <summary>Germinated test</summary>
         [XmlIgnore]
         public bool Germinated = false;
-        /// <summary>
-        /// This is the difference in phase index between the current phase and the phas it runs parallel with.
-        /// It is 0 for phases that are not run parallel
-        /// </summary>
-        private int PhaseIndexOffset = 0;
 
-        /// <summary>
-        /// Phases that can't be rewound (e.g. due to grazing)
-        /// </summary>
+        //Fixme.  This currently wont be working because the phases are all IPhase types now.  Each phase needs a property to say if it can be rewound
+        /// <summary> Phases that can't be rewound (e.g. due to grazing)   /// </summary>
+        [XmlIgnore]
         private static Type[] phasesThatWontRewind = new Type[] { typeof(NodeNumberPhase), typeof(LeafDeathPhase), typeof(LeafAppearancePhase), typeof(GotoPhase), typeof(GerminatingPhase), typeof(EndPhase) };
 
         /// <summary>A one based stage number.</summary>
         [XmlIgnore]
         public double Stage { get; set; }
 
-        /// <summary>Clears this instance.</summary>
-        public void Clear()
-        {
-            DaysAfterSowing = 0;
-            Stage = 1;
-            AccumulatedTT = 0;
-            AccumulatedEmergedTT = 0;
-            JustInitialised = true;
-            Emerged = false;
-            Germinated = false;
-            SowDate = Clock.Today;
-            CurrentlyOnFirstDayOfPhase = new string[] { "", "", "", "", "", "" };
-            CurrentPhaseIndex = 0;
-            FractionBiomassRemoved = 0;
-            foreach (IPhase phase in Phases)
-                phase.ResetPhase();
-        }
-
-        #endregion
-
-        #region Outputs
         /// <summary>This property is used to retrieve or set the current phase name.</summary>
-        /// <value>The name of the current phase.</value>
-        /// <exception cref="System.Exception">Cannot jump to phenology phase:  + value + . Phase not found.</exception>
-        
         [XmlIgnore]
         public string CurrentPhaseName
         {
@@ -238,8 +132,7 @@ namespace Models.PMF.Phen
         }
 
         /// <summary>Return current stage name.</summary>
-        /// <value>The name of the current stage.</value>
-        
+        [XmlIgnore]
         public string CurrentStageName
         {
             get
@@ -253,7 +146,7 @@ namespace Models.PMF.Phen
 
 
         /// <summary>Gets the fraction in current phase.</summary>
-        /// <value>The fraction in current phase.</value>
+        [XmlIgnore]
         public double FractionInCurrentPhase
         {
             get
@@ -263,13 +156,25 @@ namespace Models.PMF.Phen
         }
 
         /// <summary>Gets the days after sowing.</summary>
-        /// <value>The days after sowing.</value>
+        [XmlIgnore]
         public int DaysAfterSowing { get; set; }
-       
-        #endregion
 
-        /// <summary>Constructor</summary>
-        public Phenology() { }
+
+
+        /// <summary>Gets the tt in above ground phase.</summary>
+        /// <value>The tt in above ground phase.</value>
+        /// <exception cref="System.Exception">Cannot find Phenology.AboveGroundPeriod function in xml file</exception>
+        private double TTInAboveGroundPhase
+        {
+            get
+            {
+                double TTInPhase = 0.0;
+                for (int i = IndexOfFirstAboveGroundPhase(); i < Phases.Count; i++)
+                    TTInPhase += Phases[i].TTinPhase;
+                return TTInPhase;
+            }
+        }
+
         /// <summary>Initialize the phase list of phenology.</summary>
         [EventSubscribe("Loaded")]
         private void OnLoaded(object sender, LoadedEventArgs args)
@@ -426,12 +331,12 @@ namespace Models.PMF.Phen
                        // run the next phase with the left over time step from the phase we have just completed
                         FractionOfDayLeftOver = CurrentPhase.DoTimeStep(FractionOfDayLeftOver);
                        
-                        Stage = (CurrentPhaseIndex + 1) + CurrentPhase.FractionComplete - PhaseIndexOffset;
+                        Stage = (CurrentPhaseIndex + 1) + CurrentPhase.FractionComplete;
                     }
                 }
                 else
                 {
-                    Stage = (CurrentPhaseIndex + 1) + CurrentPhase.FractionComplete - PhaseIndexOffset;
+                    Stage = (CurrentPhaseIndex + 1) + CurrentPhase.FractionComplete;
                 }
 
                 AccumulatedTT += CurrentPhase.TTForToday;
@@ -473,7 +378,6 @@ namespace Models.PMF.Phen
                 bool HarvestCall = false;
                 if (CurrentPhaseIndex == Phases.Count - 1)
                     HarvestCall = true;
-                PhaseIndexOffset = 0;
                 if (CurrentPhaseIndex == -1)
                     throw new Exception("Cannot jump to phenology phase: " + value + ". Phase not found.");
 
@@ -543,6 +447,33 @@ namespace Models.PMF.Phen
             PhaseRewind.Invoke(this, new EventArgs());
         }
 
+        /// <summary> /// Find the first phase that is beyond germination i.e. plant is above ground.  /// </summary>
+        private int IndexOfFirstAboveGroundPhase()
+        {
+            int index = Phases.FindIndex(p => p.End == "Germination");
+            if (index == -1)
+                return 0;
+            else
+                return index + 1;
+        }
+
+        /// <summary>Clears this instance.</summary>
+        public void Clear()
+        {
+            DaysAfterSowing = 0;
+            Stage = 1;
+            AccumulatedTT = 0;
+            AccumulatedEmergedTT = 0;
+            JustInitialised = true;
+            Emerged = false;
+            Germinated = false;
+            SowDate = Clock.Today;
+            CurrentlyOnFirstDayOfPhase = new string[] { "", "", "", "", "", "" };
+            CurrentPhaseIndex = 0;
+            FractionBiomassRemoved = 0;
+            foreach (IPhase phase in Phases)
+                phase.ResetPhase();
+        }
 
         /// <summary>
         /// A utility function to return true if the simulation is on the first day of the
@@ -756,7 +687,7 @@ namespace Models.PMF.Phen
                     { // phase is empty - not interested in it
                     }
                 }
-                Stage = (CurrentPhaseIndex + 1) + CurrentPhase.FractionComplete - PhaseIndexOffset;
+                Stage = (CurrentPhaseIndex + 1) + CurrentPhase.FractionComplete;
 
                 if (existingPhaseIndex != CurrentPhaseIndex)
                 {
@@ -769,32 +700,11 @@ namespace Models.PMF.Phen
             }
         }
 
-        /// <summary>
-        /// Find the first phase that is beyond germination i.e. plant is above ground.
-        /// </summary>
-        /// <returns></returns>
-        private int IndexOfFirstAboveGroundPhase()
-        {
-            int index = Phases.FindIndex(p => p.End == "Germination");
-            if (index == -1)
-                return 0;
-            else
-                return index + 1;
-        }
 
-        /// <summary>Gets the tt in above ground phase.</summary>
-        /// <value>The tt in above ground phase.</value>
-        /// <exception cref="System.Exception">Cannot find Phenology.AboveGroundPeriod function in xml file</exception>
-        private double TTInAboveGroundPhase
-        {
-            get
-            {
-                double TTInPhase = 0.0;
-                for (int i = IndexOfFirstAboveGroundPhase(); i < Phases.Count; i++)
-                    TTInPhase += Phases[i].TTinPhase;
-                return TTInPhase;
-            }
-        }
+
+        /// <summary>Constructor</summary>
+        public Phenology() { }
+
         /// <summary>Writes documentation for this function by adding to the list of documentation tags.</summary>
         /// <param name="tags">The list of tags to add to.</param>
         /// <param name="headingLevel">The level (e.g. H2) of the headings.</param>
