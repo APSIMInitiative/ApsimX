2561192340ac7e39c3597df765e0d282f882da29
Hamish Brown <hamish.brown@plantandfood.co.nz>
2018-07-25 14:05:44 +1200

    Make End phase use IPhase

--- Models/Plant/Phenology/Phases/EndPhase.cs
+++ Models/Plant/Phenology/Phases/EndPhase.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Models.Core;
 using System.Xml.Serialization;
+using System.IO;
+using Models.Functions;
 
 namespace Models.PMF.Phen
 {
@@ -11,11 +11,30 @@ namespace Models.PMF.Phen
     [Serializable]
     [ViewName("UserInterface.Views.GridView")]
     [PresenterName("UserInterface.Presenters.PropertyPresenter")]
-    public class EndPhase : Phase
+    public class EndPhase : Model, IPhase
     {
         /// <summary>The _ cumulative value</summary>
         private double _CumulativeValue;
 
+        /// <summary>The start</summary>
+        [Description("Start")]
+        public string Start { get; set; }
+
+        /// <summary>The end</summary>
+        [Models.Core.Description("End")]
+        public string End { get; set; }
+
+        /// <summary>The phenology</summary>
+        [Link]
+        protected Phenology phenology = null;
+
+        /// <summary>The thermal time</summary>
+        [Link(IsOptional = true)]
+        public IFunction ThermalTime = null;  //FIXME this should be called something to represent rate of progress as it is sometimes used to represent other things that are not thermal time.
+
+        /// <summary>The stress</summary>
+        [Link(IsOptional = true)]
+        public IFunction Stress = null;
 
         /// <summary>Gets the cumulative value.</summary>
         /// <value>The cumulative value.</value>
@@ -24,30 +43,68 @@ namespace Models.PMF.Phen
         /// <summary>Do our timestep development</summary>
         /// <param name="PropOfDayToUse">The property of day to use.</param>
         /// <returns></returns>
-        public override double DoTimeStep(double PropOfDayToUse)
+        public double DoTimeStep(double PropOfDayToUse)
         {
             _CumulativeValue += ThermalTime.Value();
             TTinPhase += ThermalTime.Value();
             return 0;
         }
 
+        /// <summary>Gets the t tin phase.</summary>
+        /// <value>The t tin phase.</value>
+        [XmlIgnore]
+        public double TTinPhase { get; set; }
+
         /// <summary>Return a fraction of phase complete.</summary>
         /// <value>The fraction complete.</value>
         [XmlIgnore]
-        public override double FractionComplete
+        public double FractionComplete
         {
             get { return 0.0; }
             set
             {
-                if (Phenology != null)
+                if (phenology != null)
                 throw new Exception("Not possible to set phenology into " + this + " phase (at least not at the moment because there is no code to do it");
             }
         }
 
-        /// <summary>Resets the phase.</summary>
-        public override void ResetPhase()
+        /// <summary>The property of day unused</summary>
+        protected double PropOfDayUnused = 0;
+        /// <summary>The _ tt for today</summary>
+        protected double _TTForToday = 0;
+
+        /// <summary>Gets the tt for today.</summary>
+        /// <value>The tt for today.</value>
+        public double TTForToday
         {
-            base.ResetPhase();
+            get
+            {
+                if (ThermalTime == null)
+                    return 0;
+                return ThermalTime.Value();
+            }
+        }
+
+        /// <summary>Writes the summary.</summary>
+        /// <param name="writer">The writer.</param>
+        public void WriteSummary(TextWriter writer)
+        {
+            writer.WriteLine("      " + Name);
+        }
+
+        /// <summary>Called when [simulation commencing].</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        [EventSubscribe("Commencing")]
+        private void OnSimulationCommencing(object sender, EventArgs e)
+        { ResetPhase(); }
+
+        /// <summary>Resets the phase.</summary>
+        public void ResetPhase()
+        {
+            _TTForToday = 0;
+            TTinPhase = 0;
+            PropOfDayUnused = 0;
             _CumulativeValue = 0;
         }
     }
--- Models/Plant/Phenology/Phases/Phase.cs
+++ Models/Plant/Phenology/Phases/Phase.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Models.Core;
-//using System.ComponentModel;
 using Models.Functions;
 using System.IO;
 using System.Xml.Serialization;
