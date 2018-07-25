3331c683d2b5656b5cfaf3997f62674a93e3c1e0
Hamish Brown <hamish.brown@plantandfood.co.nz>
2018-07-25 14:38:39 +1200

    Put Leaf Death phase onto IPhase

--- Models/Plant/Phenology/Phases/GotoPhase.cs
+++ Models/Plant/Phenology/Phases/GotoPhase.cs
@@ -112,10 +112,5 @@ namespace Models.PMF.Phen
         {
             writer.WriteLine("      " + Name);
         }
-
-        /// <summary>Writes documentation for this function by adding to the list of documentation tags.</summary>
-        /// <param name="tags">The list of tags to add to.</param>
-        /// <param name="headingLevel">The level (e.g. H2) of the headings.</param>
-        /// <param name="indent">The level of indentation 1, 2, 3 etc.</param>
     }
 }
--- Models/Plant/Phenology/Phases/LeafDeathPhase.cs
+++ Models/Plant/Phenology/Phases/LeafDeathPhase.cs
@@ -1,10 +1,11 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Models.Core;
 using Models.PMF.Organs;
 using System.Xml.Serialization;
 using Models.PMF.Struct;
+using System.IO;
+using Models.Functions;
+
 
 namespace Models.PMF.Phen
 {
@@ -14,7 +15,7 @@ namespace Models.PMF.Phen
     [Serializable]
     [ViewName("UserInterface.Views.GridView")]
     [PresenterName("UserInterface.Presenters.PropertyPresenter")]
-    public class LeafDeathPhase : Phase
+    public class LeafDeathPhase : Model, IPhase
     {
         /// <summary>The leaf</summary>
         [Link]
@@ -30,18 +31,29 @@ namespace Models.PMF.Phen
         private bool First = true;
 
         /// <summary>Resets the phase.</summary>
-        public override void ResetPhase()
+        public void ResetPhase()
         {
-            base.ResetPhase();
+            _TTForToday = 0;
+            TTinPhase = 0;
+            PropOfDayUnused = 0;
             DeadNodeNoAtStart = 0;
             First = true;
         }
         /// <summary>Do our timestep development</summary>
         /// <param name="PropOfDayToUse">The property of day to use.</param>
         /// <returns></returns>
-        public override double DoTimeStep(double PropOfDayToUse)
+        public double DoTimeStep(double PropOfDayToUse)
         {
-            base.DoTimeStep(PropOfDayToUse);
+            // Calculate the TT for today and Accumulate.      
+            if (ThermalTime != null)
+            {
+                _TTForToday = ThermalTime.Value() * PropOfDayToUse;
+                if (Stress != null)
+                {
+                    _TTForToday *= Stress.Value();
+                }
+                TTinPhase += _TTForToday;
+            }
 
             if (First)
             {
@@ -58,7 +70,7 @@ namespace Models.PMF.Phen
         /// <summary>Return a fraction of phase complete.</summary>
         /// <value>The fraction complete.</value>
         [XmlIgnore]
-        public override double FractionComplete
+        public double FractionComplete
         {
             get
             {
@@ -71,6 +83,77 @@ namespace Models.PMF.Phen
             {
                 throw new Exception("Not possible to set phenology into "+ this + " phase (at least not at the moment because there is no code to do it");
             }
+        }
+
+        /// <summary>The start</summary>
+        [Description("Start")]
+        public string Start { get; set; }
+
+        /// <summary>The end</summary>
+        [Models.Core.Description("End")]
+        public string End { get; set; }
+
+        /// <summary>The phenology</summary>
+        [Link]
+        protected Phenology Phenology = null;
+
+        /// <summary>The thermal time</summary>
+        [Link(IsOptional = true)]
+        public IFunction ThermalTime = null;  //FIXME this should be called something to represent rate of progress as it is sometimes used to represent other things that are not thermal time.
+
+        /// <summary>The stress</summary>
+        [Link(IsOptional = true)]
+        public IFunction Stress = null;
+
+        /// <summary>The property of day unused</summary>
+        protected double PropOfDayUnused = 0;
+        /// <summary>The _ tt for today</summary>
+        protected double _TTForToday = 0;
+
+        /// <summary>Gets the tt for today.</summary>
+        /// <value>The tt for today.</value>
+        public double TTForToday
+        {
+            get
+            {
+                if (ThermalTime == null)
+                    return 0;
+                return ThermalTime.Value();
+            }
+        }
+
+        /// <summary>Gets the t tin phase.</summary>
+        /// <value>The t tin phase.</value>
+        [XmlIgnore]
+        public double TTinPhase { get; set; }
+
+        // Return proportion of TT unused
+        /// <summary>Adds the tt.</summary>
+        /// <param name="PropOfDayToUse">The property of day to use.</param>
+        /// <returns></returns>
+        virtual public double AddTT(double PropOfDayToUse)
+        {
+            TTinPhase += ThermalTime.Value() * PropOfDayToUse;
+            return 0;
+        }
+        /// <summary>Adds the specified DLT_TT.</summary>
+        /// <param name="dlt_tt">The DLT_TT.</param>
+        virtual public void Add(double dlt_tt) { TTinPhase += dlt_tt; }
+
+        /// <summary>Called when [simulation commencing].</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        [EventSubscribe("Commencing")]
+        private void OnSimulationCommencing(object sender, EventArgs e)
+        { ResetPhase(); }
+
+
+
+        /// <summary>Writes the summary.</summary>
+        /// <param name="writer">The writer.</param>
+        public void WriteSummary(TextWriter writer)
+        {
+            writer.WriteLine("      " + Name);
         }
 
     }
