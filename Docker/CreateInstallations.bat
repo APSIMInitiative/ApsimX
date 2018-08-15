2ed9b45454b35a352477c67a6326a42a8908e963
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-08-15 15:02:08 +1000

    More work on create installation

--- Docker/CreateInstallations.bat
+++ Docker/CreateInstallations.bat
@@ -13,7 +13,7 @@ if "%1"=="macos" (
 	echo.
 	echo.
 	docker build -t createosxinstallation ApsimX\Docker\osx
-	docker run -e APSIM_SITE_CREDS -e ISSUE_NUMBER -v %cd%\ApsimX:/ApsimX createosxinstallation
+	docker run -e APSIM_SITE_CREDS -e ISSUE_NUMBER -v "%cd%\ApsimX":/ApsimX createosxinstallation
 ) else (
 	docker build -m 16g -t createinstallation ApsimX\Docker\CreateInstallation
 	docker run -m 16g --cpu-count %NUMBER_OF_PROCESSORS% --cpu-percent 100 -e APSIM_SITE_CREDS -e NUMBER_OF_PROCESSORS -e ISSUE_NUMBER -v %cd%\ApsimX:C:\ApsimX createinstallation %1
