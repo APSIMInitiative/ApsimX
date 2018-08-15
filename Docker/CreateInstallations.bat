df1f3722dec1f3f70c71296238005d268c86a9f1
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-08-15 14:21:34 +1000

    Fix Linux docker container.

--- Docker/CreateInstallations.bat
+++ Docker/CreateInstallations.bat
@@ -1,13 +1,8 @@
 @echo off
-if not exist APSIM.Shared (
-	git clone https://github.com/APSIMInitiative/APSIM.Shared APSIM.Shared
-)
-git -C APSIM.Shared pull origin master
-
 if "%1"=="macos" (
 	docker build -t createosxinstallation ApsimX\Docker\osx\CreateInstallation
-	docker run -e APSIM_SITE_CREDS -e ISSUE_NUMBER -v %cd%\ApsimX:C:\ApsimX -v %cd%\APSIM.Shared:C:\APSIM.Shared createosxinstallation %1
+	docker run -e APSIM_SITE_CREDS -e ISSUE_NUMBER -v %cd%\ApsimX:/ApsimX -v createosxinstallation
 ) else (
 	docker build -m 16g -t createinstallation ApsimX\Docker\CreateInstallation
-	docker run -m 16g --cpu-count %NUMBER_OF_PROCESSORS% --cpu-percent 100 -e APSIM_SITE_CREDS -e NUMBER_OF_PROCESSORS -e ISSUE_NUMBER -v %cd%\ApsimX:C:\ApsimX -v %cd%\APSIM.Shared:C:\APSIM.Shared createinstallation %1
+	docker run -m 16g --cpu-count %NUMBER_OF_PROCESSORS% --cpu-percent 100 -e APSIM_SITE_CREDS -e NUMBER_OF_PROCESSORS -e ISSUE_NUMBER -v %cd%\ApsimX:C:\ApsimX createinstallation %1
 )
