544ab85e1b3d9fcde95eac6f6890c2bd9e62c0cd
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-08-24 14:18:01 +1000

    Add some temporary diagnostics to create installation job.

--- Docker/Deploy/deploy.bat
+++ Docker/Deploy/deploy.bat
@@ -1,5 +1,7 @@
 @echo off
 echo ########### Adding a green build to the builds database...
+echo %PULL_ID%
+echo curl "https://www.apsim.info/APSIM.Builds.Service/Builds.svc/AddBuild?pullRequestNumber=%PULL_ID%&ChangeDBPassword="
 @curl "https://www.apsim.info/APSIM.Builds.Service/Builds.svc/AddBuild?pullRequestNumber=%PULL_ID%&ChangeDBPassword=%PASSWORD%"
 if errorlevel 1 (
 	echo Errors encountered!
