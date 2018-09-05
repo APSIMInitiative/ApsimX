9b0af637027c68f6638a7907d718893b0a50a29f
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-09-05 12:01:49 +1000

    Fix small bug with path of documentation script.

--- Docker/CreateDocumentation.bat
+++ Docker/CreateDocumentation.bat
@@ -1,8 +1,5 @@
 @echo off
 set apsimx=%~dp0..
-if not exist %apsimx% (
-	echo %apsimx% does not exist. Aborting...
-)
 
 if exist %apsimx%\bin.zip (
 	echo Unzipping %apsimx%\bin.zip...
@@ -12,10 +9,10 @@ if exist %apsimx%\bin.zip (
 		exit %errorlevel%
 	)
 )
-cd %apsimx%
+
 if exist %apsimx%\results.7z (
 	echo Unzipping %apsimx%\results.7z...
-	7z x -y results.7z
+	7z x -y %apsimx%\results.7z
 	if errorlevel 1 (
 		echo Error unzipping %apsimx%\results.7z
 	)
@@ -25,7 +22,7 @@ if not exist %apsimx%\lib (
 	robocopy /e /NJS /np %apsimx%\DeploymentSupport\Windows\lib %apsimx%\lib
 )
 
-call GenerateDocumentation.bat
+%apsimx%\Documentation\GenerateDocumentation.bat
 
 docker build -t documentation %apsimx%\Docker\Documentation
 docker run -m 12g --cpu-count %NUMBER_OF_PROCESSORS% --cpu-percent 100 -e NUMBER_OF_PROCESSORS -e ISSUE_NUMBER -e APSIM_SITE_CREDS -v %cd%\ApsimX:C:\ApsimX -v %cd%\APSIM.Shared:C:\APSIM.Shared documentation
