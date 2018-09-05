8fc872362e94bcce094e60726df8e88da32be323
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-09-05 11:52:18 +1000

    Allow GenerateDocumentation.bat to be run from any directory.

--- Documentation/GenerateDocumentation.bat
+++ Documentation/GenerateDocumentation.bat
@@ -1,16 +1,18 @@
 @echo off
-
+setlocal
 :: Generate model validation and configuration documentation
-del ..\Bin\errors.txt >nul 2>nul
+set apsimx=%~dp0..
+del %apsimx%\Bin\errors.txt >nul 2>nul
 rmdir /S /Q PDF >nul 2>nul
-for /D %%D in (..\Tests\Validation\*) do (
+for /D %%D in (%apsimx%\Tests\Validation\*) do (
     echo Generating PDF for model %%~nD
     set ModelName=%%~nD%%~xD
-    ..\Bin\ApsimNG.exe CreateModelDocumentation.cs
+    %apsimx%\Bin\ApsimNG.exe %~dp0\CreateModelDocumentation.cs
 	if ERRORLEVEL 1 goto error
 )
 exit /B 0
 
 :error
-type ..\Bin\errors.txt
+type %apsimx%\Bin\errors.txt
 exit /B 1
+endlocal
