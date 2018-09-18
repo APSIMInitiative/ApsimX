4f96cb2dd0a7b28b61d79e0fbd9b84ecabce36c2
Hamish <hamish.brown@plantandfoodco.nz>
2018-09-18 16:01:49 +1200

    Create function to calculate storage N demand

---
+++ Models/Functions/DemandFunctions/StorageNDemandFunction.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Models.Core;
+using Models.PMF.Interfaces;
+using APSIM.Shared.Utilities;
+
+namespace Models.Functions.DemandFunctions
+{
+    /// <summary>
+    /// # [Name]
+    /// Calculate partitioning of daily growth based upon allometric relationship
+    /// </summary>
+    [Serializable]
+    [Description("This function calculated dry matter demand using plant allometry which is described using a simple power function (y=kX^p).")]
+    [ViewName("UserInterface.Views.GridView")]
+    [PresenterName("UserInterface.Presenters.PropertyPresenter")]
+    public class StorageNDemandFunction : Model, IFunction
+    {
+        /// <summary>The maximimum N concentration of the organ</summary>
+        [Description("The maximum N concenctration of the organ")]
+        [Link]
+        private IFunction maxNConc = null;
+
+        /// <summary>Switch to modulate N demand</summary>
+        [Description("Switch to modulate N demand")]
+        [Link]
+        private IFunction nitrogenDemandSwitch = null;
+
+        private IArbitration parentOrgan = null;
+
+        /// <summary>Called when [simulation commencing].</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        [EventSubscribe("Commencing")]
+        private void OnSimulationCommencing(object sender, EventArgs e)
+        {
+            bool ParentOrganIdentified = false;
+            IModel ParentClass = this.Parent;
+            while (!ParentOrganIdentified)
+            {
+                if (ParentClass is IArbitration)
+                {
+                    parentOrgan = ParentClass as IArbitration;
+                    ParentOrganIdentified = true;
+                    if (ParentClass is IPlant)
+                        throw new Exception(Name + "cannot find parent organ to get Structural and storage DM status");
+                }
+                ParentClass = ParentClass.Parent;
+            }
+        }
+
+        /// <summary>Gets the value.</summary>
+        public double Value(int arrayIndex = -1)
+        {
+            double potentialAllocation = parentOrgan.potentialDMAllocation.Structural + parentOrgan.potentialDMAllocation.Metabolic;
+            double NDeficit = Math.Max(0.0, maxNConc.Value() * (parentOrgan.Live.Wt + potentialAllocation) - parentOrgan.Live.N);
+            NDeficit *= nitrogenDemandSwitch.Value();
+
+            return Math.Max(0, NDeficit - parentOrgan.NDemand.Structural - parentOrgan.NDemand.Metabolic);
+        }
+
+    }
+}
