b697c3d4f832bee392d54b338645bd645dd4f408
behnamxa <39573622+behnamxa@users.noreply.github.com>
2018-09-27 18:50:47 +1000

    Accounting for 'fraction' of water demand that is available in soil profile
    Accounting for 'fraction' of water demand that is available in soil profile

--- Models/Plant/Arbitrator/OrganArbitrator.cs
+++ Models/Plant/Arbitrator/OrganArbitrator.cs
@@ -213,8 +213,15 @@ namespace Models.PMF
             // Give the water uptake for each zone to Root so that it can perform the uptake
             // i.e. Root will do pass the uptake to the soil water balance.
             if (doUptake)
+            {
+                fraction = 1;
+                if (waterSupply > 0) fraction = Math.Max(0.0, Math.Min(1.0, WDemand / waterSupply));
                 foreach (ZoneWaterAndN Z in zones)
-                    Plant.Root.DoWaterUptake(Z.Water, Z.Zone.Name);
+                {
+                    double[] uptake = MathUtilities.Multiply_Value(Z.Water, fraction);
+                    Plant.Root.DoWaterUptake(uptake, Z.Zone.Name);
+                }
+            }
         }
 
         /// <summary>
