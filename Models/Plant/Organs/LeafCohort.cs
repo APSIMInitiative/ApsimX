30ec383c1aeca421f4b23a94d3ddfe9ea3b8d96f
Bangyou Zheng <zheng.bangyou@gmail.com>
2018-10-08 16:02:11 +1000

    Check whether initial leaf area is more than max leaf area

--- Models/Plant/Organs/LeafCohort.cs
+++ Models/Plant/Organs/LeafCohort.cs
@@ -756,7 +756,11 @@ namespace Models.PMF.Organs
             StorageFraction = leafCohortParameters.StorageFraction.Value();
             InitialNConc = leafCohortParameters.InitialNConc.Value();
             if (Area > 0) //Only set age for cohorts that have an area specified in the xml.
+            {
+                if (Area > MaxArea)
+                    throw new Exception("Initial area is more than max area for cohort " + Rank.ToString() + ".");
                 Age = Area / MaxArea * GrowthDuration;
+            }
             //FIXME.  The size function is not linear so this does not give an exact starting age.  Should re-arange the the size function to return age for a given area to initialise age on appearance.
             LiveArea = Area * CohortPopulation;
             Live.StructuralWt = LiveArea / ((SpecificLeafAreaMax + SpecificLeafAreaMin) / 2) * StructuralFraction;
