43be6321e228713649fd1af0be910c26f2656fdf
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-11-01 13:45:57 +1000

    Reduce curl verbosity when running performance tests.

--- Jenkins/performanceTests.bat
+++ Jenkins/performanceTests.bat
@@ -8,7 +8,7 @@ if "%apsimx%"=="" (
 )
 set COMMIT_AUTHOR=%ghprbActualCommitAuthor%
 set PULL_ID=%ghprbPullId%
-curl -k https://www.apsim.info/APSIM.Builds.Service/Builds.svc/GetPullRequestDetails?pullRequestID=%PULL_ID% > temp.txt
+curl -ks https://www.apsim.info/APSIM.Builds.Service/Builds.svc/GetPullRequestDetails?pullRequestID=%PULL_ID% > temp.txt
 for /F "tokens=1-6 delims==><" %%I IN (temp.txt) DO SET FULLRESPONSE=%%K
 del temp.txt
 for /F "tokens=1-6 delims=," %%I IN ("%FULLRESPONSE%") DO SET DATETIMESTAMP=%%I
