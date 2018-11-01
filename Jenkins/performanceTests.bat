8377281910acbfb61d65ff2aae16498ef432ea5c
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-11-01 13:47:47 +1000

    Further reduce the verbosity of the performance tests stage on Jenkins.

--- Jenkins/performanceTests.bat
+++ Jenkins/performanceTests.bat
@@ -13,14 +13,14 @@ for /F "tokens=1-6 delims==><" %%I IN (temp.txt) DO SET FULLRESPONSE=%%K
 del temp.txt
 for /F "tokens=1-6 delims=," %%I IN ("%FULLRESPONSE%") DO SET DATETIMESTAMP=%%I
 
-echo Pull request ID: 	"%PULL_ID%"
-echo DateTime stamp: 	"%DATETIMESTAMP%"
-echo Commit author:		"%COMMIT_AUTHOR%"
 echo Running performance tests collector...
 %apsimx%\Docker\runtests\APSIM.PerformanceTests.Collector\APSIM.PerformanceTests.Collector.exe AddToDatabase %PULL_ID% %DATETIMESTAMP% %COMMIT_AUTHOR%
 set err=%errorlevel%
 if errorlevel 1 (
 	echo APSIM.PerformanceTests.Collector did not run succecssfully!
+	echo Pull request ID: 	"%PULL_ID%"
+	echo DateTime stamp: 	"%DATETIMESTAMP%"
+	echo Commit author:		"%COMMIT_AUTHOR%"
 	echo Log file:
 	type %apsimx%\Docker\runtests\APSIM.PerformanceTests.Collector\PerformanceCollector.txt
 ) else (
