d5406d9cfdf123594db275b3cd2f76f6255aae0c
NEXUS\hut104 <neil.huth@csiro.au>
2018-11-06 15:40:27 +1000

    Added code with only minor changes

--- Models/Soils/Nutrients/Solute.cs
+++ Models/Soils/Nutrients/Solute.cs
@@ -13,7 +13,12 @@ namespace Models.Soils.Nutrients
 
     /// <summary>
     /// # [Name]
-    /// Encapsulates a solute class.
+    /// [DocumentType Memo]
+    /// 
+    /// This class used for this nutrient encapsulates the nitrogen within a mineral N pool.  Child functions provide information on flows of N from it to other mineral N pools, or losses from the system.
+    /// 
+    /// ## Mineral N Flows
+    /// [DocumentType NFlow]
     /// </summary>
     [Serializable]
     [ValidParent(ParentType = typeof(Nutrient))]
