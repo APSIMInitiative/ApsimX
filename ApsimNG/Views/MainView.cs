27029469be1773c785c003ab349bcbeff8e07a64
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-11-15 14:23:15 +1000

    Improved colour of home screen json warning under the dark theme.

--- ApsimNG/Views/MainView.cs
+++ ApsimNG/Views/MainView.cs
@@ -147,7 +147,8 @@
             EventBox labelBox = new EventBox();
             Label label = new Label("NOTE: This version of APSIM writes .apsimx files as JSON, not XML. These files cannot be opened with older versions of APSIM.");
             labelBox.Add(label);
-            labelBox.ModifyBg(StateType.Normal, new Gdk.Color(0xff, 0xff, 0x00)); // yellow
+            if (!Utility.Configuration.Settings.DarkTheme)
+                labelBox.ModifyBg(StateType.Normal, new Gdk.Color(0xff, 0xff, 0x00)); // yellow
             vbox1.PackStart(labelBox, false, true, 0);
             vbox1.PackEnd(listButtonView1.MainWidget, true, true, 0);
             listButtonView2 = new ListButtonView(this);
