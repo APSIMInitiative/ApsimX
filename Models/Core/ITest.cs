8661df1dd33058467f6322d0d79101eb3a0ca00d
NEXUS\hol430 <drew.holzworth@csiro.au>
2018-11-16 15:00:29 +1000

    Added an ITest interface which will allow for more flexible testing.

---
+++ Models/Core/ITest.cs
@@ -0,0 +1,14 @@
+﻿namespace Models.Core
+{
+    /// <summary>
+    /// An interface for a model which is a test.
+    /// </summary>
+    public interface ITest : IModel
+    {
+        /// <summary>
+        /// Runs the test. Returns true iff successful.
+        /// </summary>
+        /// <returns>True iff successful.</returns>
+        bool Run();
+    }
+}
--- Models/Core/Runners/RunOrganiser.cs
+++ Models/Core/Runners/RunOrganiser.cs
@@ -112,6 +112,25 @@
             {
                 foreach (Tests test in Apsim.ChildrenRecursively(simulations, typeof(Tests)))
                     test.Test();
+                foreach (ITest test in Apsim.ChildrenRecursively(simulations, typeof(ITest)))
+                {
+                    // If we run into problems, we will want to include the name of the test in the 
+                    // exception's message. However, tests may be manager scripts, which always have
+                    // a name of 'Script'. Therefore, if the test's parent is a Manager, we use the
+                    // manager's name instead.
+                    string testName = test.Parent is Manager ? test.Parent.Name : test.Name;
+                    bool successful = false;
+                    try
+                    {
+                        successful = test.Run();
+                    }
+                    catch (Exception err)
+                    {
+                        throw new Exception("Encountered an error while running test " + testName, err);
+                    }
+                    if (!successful)
+                        throw new ApsimXException(test, "Failed test " + testName);
+                }
             }
         }
     }
