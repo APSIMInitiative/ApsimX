cfcc783c1a076b5adc2a43ef05151812f528200f
Neil Huth <neil.huth@csiro.au>
2019-01-04 10:11:00 +1000

    Fixed incorrect case in method name

--- Models/MicroClimate/MicroClimate.cs
+++ Models/MicroClimate/MicroClimate.cs
@@ -172,7 +172,7 @@ namespace Models
             foreach (ZoneMicroClimate ZoneMC in zoneMicroClimates)
             {
                 ZoneMC.DoCanopyCompartments();
-                CalculateIncomingShortwaveRadiation(ZoneMC);
+                CalculateIncomingShortWaveRadiation(ZoneMC);
                 CalculateLayeredShortWaveRadiation(ZoneMC);
                 CalculateEnergyTerms(ZoneMC);
                 CalculateLongWaveRadiation(ZoneMC);
