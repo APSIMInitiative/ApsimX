75012b0aab91a32489fa4502129a54249e9a399d
NEXUS\hol430 <drew.holzworth@csiro.au>
2019-02-06 16:08:54 +1000

    Factors should attempt to resolve model replacements with an incorrect name if there is only one model of that type being replaced by the factor.

--- Models/Factorial/Factor.cs
+++ Models/Factorial/Factor.cs
@@ -244,7 +244,27 @@ namespace Models.Factorial
                     // name as the model which it is replacing.
                     IModel replacementModel = Apsim.Child(this, modelToReplace.Name);
                     if (replacementModel == null)
-                        throw new ApsimXException(this, "Unable to find a replacement model for model " + modelToReplace.Name + " in factor " + Name);
+                    {
+                        // We couldn't find a replacement model with the correct name.
+                        int numMatches = Apsim.Children(this, modelToReplace.GetType()).Count;
+                        if (numMatches == 1)
+                        {
+                            // If there is only one child of the correct type, we will
+                            // use it iff there are no other specifications replacing
+                            // a model of that type.
+                            foreach (string spec in Specifications.Where(s => s != specification))
+                            {
+                                IModel candidate = Apsim.Get(experiment.BaseSimulation, spec) as IModel;
+                                if (modelToReplace.GetType() == candidate.GetType())
+                                    throw new ApsimXException(this, $"Unable to find replacement model for model {modelToReplace.Name} in factor {Name}. {Apsim.Child(this, modelToReplace.GetType()).Name} is of the correct type, but is already being used to replace model {spec}.");
+                            }
+                            replacementModel = Apsim.Child(this, modelToReplace.GetType());
+                        }
+                        else if (numMatches > 1)
+                            throw new ApsimXException(this, $"Error in factor {Name}: too many replacement candidates for model {modelToReplace.Name}. You have {numMatches} replacement models of type {modelToReplace.GetType().Name}, but none have the correct name ({modelToReplace.Name}).");
+                        else
+                            throw new ApsimXException(this, $"Unable to find a replacement model for model {modelToReplace.Name} in factor {Name}");
+                    }
                     pairs.Add(new PathValuesPair() { path = specification, value = replacementModel });
                 }
             }
