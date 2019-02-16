ae1efc83a3023950f6b5635da251d26ea8630679
Neil Huth <neil.huth@csiro.au>
2019-02-16 19:24:18 +1000

    adding CERES water factor

---
+++ Models/Functions/SoilFunctions/CERESMineralisationWaterFactor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using Models.Core;
+using Models.Soils.Nutrients;
+using APSIM.Shared.Utilities;
+using Models.Soils;
+
+namespace Models.Functions
+{
+    /// <summary>Fraction of NH4 which nitrifies today</summary>
+    /// \pre All children have to contain a public function "Value"
+    /// \retval fraction of NH4 nitrified.
+    [Serializable]
+    [Description("Mineralisation Temperature Factor from CERES-Maize")]
+    public class CERESMineralisationWaterFactor : Model, IFunction, ICustomDocumentation
+    {
+
+        [Link]
+        Soil soil = null;
+
+   
+        /// <summary>Gets the value.</summary>
+        /// <value>The value.</value>
+        public double Value(int arrayIndex = -1)
+        {
+            if (arrayIndex == -1)
+                throw new Exception("Layer number must be provided to CERES mineralisation water factor Model");
+
+            double WF = 0;
+            //double LL15 = soil.
+            return WF;
+        }
+
+        /// <summary>Writes documentation for this function by adding to the list of documentation tags.</summary>
+        /// <param name="tags">The list of tags to add to.</param>
+        /// <param name="headingLevel">The level (e.g. H2) of the headings.</param>
+        /// <param name="indent">The level of indentation 1, 2, 3 etc.</param>
+        public void Document(List<AutoDocumentation.ITag> tags, int headingLevel, int indent)
+        {
+
+            // add a heading.
+            tags.Add(new AutoDocumentation.Heading(Name, headingLevel));
+
+
+            // write memos.
+            foreach (IModel memo in Apsim.Children(this, typeof(Memo)))
+                AutoDocumentation.DocumentModel(memo, tags, headingLevel + 1, indent);
+
+
+        }
+    }
+}
