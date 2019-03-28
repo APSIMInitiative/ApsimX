d7ff89b800cf9b7d0569553df2eca9dae289f421
hol430 <drew.holzworth@csiro.au>
2019-03-29 09:09:01 +1000

    Added a new unit test for IDataStore
    This will reproduce a bug where saving the .apsimx file under a
    new filename will not update the filename of the data store.

---
+++ Tests/UnitTests/Storage/DataStoreTests.cs
@@ -0,0 +1,42 @@
+﻿using Models.Core;
+using Models.Storage;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Storage
+{
+    [TestFixture]
+    public class DataStoreTests
+    {
+        [Test]
+        public void TestFileNameChange()
+        {
+            Simulations sims = Utilities.GetRunnableSim();
+            IDataStore storage = Apsim.Find(sims, typeof(IDataStore)) as IDataStore;
+
+            // Write the simulations to disk.
+            storage.Close();
+            sims.Write(sims.FileName);
+            storage.Open();
+
+            // Record the .db file's filename.
+            string oldDbFileName = storage.FileName;
+
+            // Write the simulations to disk under a new filename.
+            storage.Close();
+            sims.Write(Path.ChangeExtension(Path.GetTempFileName(), ".apsimx"));
+            storage.Open();
+
+            // Record the .db file's new filename.
+            string newDbFileName = storage.FileName;
+
+            // The new .db file name should not be the same as the old one.
+            Assert.AreNotEqual(oldDbFileName, newDbFileName);
+        }
+    }
+}
