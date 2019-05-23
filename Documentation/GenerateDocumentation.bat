07b98db098b5a2b1f9d9b33489460d7116fa9169
hol430 <drew.holzworth@csiro.au>
2019-05-23 15:01:00 +1000

    Jenkins should generate autodocs for under-review models.

--- Documentation/GenerateDocumentation.bat
+++ Documentation/GenerateDocumentation.bat
@@ -1,7 +1,10 @@
 @echo off
-setlocal
+setlocal enableDelayedExpansion
 :: Generate model validation and configuration documentation
 set apsimx=%~dp0..
+if "%documentation%"=="" (
+	set "documentation=%~dp0"
+)
 del %apsimx%\Bin\errors.txt >nul 2>nul
 for /D %%D in (%apsimx%\Tests\Validation\*) do (
     echo Generating PDF for model %%~nD
@@ -9,9 +12,23 @@ for /D %%D in (%apsimx%\Tests\Validation\*) do (
     %apsimx%\Bin\ApsimNG.exe %~dp0\CreateModelDocumentation.cs
 	if ERRORLEVEL 1 goto error
 )
+
+call :document "%apsimx%\Tests\UnderReview"
+
 exit /B 0
 
+:document
+for /r %1 %%f in (*.apsimx) do (
+	set "FileToDocument=%%f"
+	echo Generating documentation for %%~nxf
+	"%bin%\ApsimNG.exe" "%documentation%\DocumentFile.cs"
+	if errorlevel 1 goto error
+)
+
+exit /b
+
 :error
+echo 1 or more errors encountered:
 type %apsimx%\Bin\errors.txt
 exit /B 1
 endlocal
