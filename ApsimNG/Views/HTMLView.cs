e6cb5b37397e87cc98251271b2fa6a5e8904e54b
hol430 <drew.holzworth@csiro.au>
2019-05-27 10:04:55 +1000

    Added font control for macOS html view.

--- ApsimNG/Views/HTMLView.cs
+++ ApsimNG/Views/HTMLView.cs
@@ -425,7 +425,15 @@ namespace UserInterface.Views
             }
         }
 
-        public Pango.FontDescription Font { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Pango.FontDescription Font
+        {
+            get => throw new NotImplementedException();
+            set
+            {
+                Browser.StringByEvaluatingJavaScriptFromString($"document.body.style.fontFamily = \"{value.Family}\";");
+                Browser.StringByEvaluatingJavaScriptFromString($"document.body.style.fontSize = {1.5 * value.Size / Pango.Scale.PangoScale}");
+            }
+        }
 
         /// <summary>
         /// The find form
