d51065cc1bbb519d8518e8e65eaea580e134c7e7
uqpdevo1 <peter.devoil@uq.edu.au>
2019-06-04 13:04:00 +1000

    Add remove nodes

---
+++ ApsimNG/EventArguments/RotBubbleChartArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ApsimNG.Classes.DirectedGraph;
+
+namespace UserInterface.EventArguments
+{
+    public class AddNodeEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+        public System.Drawing.Color Background { get; set; }
+        public System.Drawing.Color Outline { get; set; }
+    }
+    public class DupNodeEventArgs : EventArgs
+    {
+        public string nodeNameToDuplicate;
+    }
+    public class DelNodeEventArgs : EventArgs
+    {
+        public string nodeNameToDelete;
+    }
+}
---
+++ ApsimNG/Views/ContextMenuHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Gtk;
+using Gdk;
+using GLib;
+
+namespace UserInterface.Views
+{
+    // https://daveaglick.com/posts/right-click-context-menus-in-gtksharp
+
+    public class ContextMenuEventArgs : EventArgs
+    {
+        private Widget widget;
+        public Widget Widget { get { return widget; } }
+
+        private bool rightClick;
+        public bool RightClick { get { return rightClick; } }
+
+        public ContextMenuEventArgs(Widget widget, bool rightClick)
+        {
+            this.widget = widget;
+            this.rightClick = rightClick;
+        }
+    }
+
+    public class ContextMenuHelper
+    {
+        public event EventHandler<ContextMenuEventArgs> ContextMenu;
+
+        public ContextMenuHelper()
+        { }
+
+        public ContextMenuHelper(Widget widget)
+        {
+            AttachToWidget(widget);
+        }
+
+        public ContextMenuHelper(Widget widget, EventHandler<ContextMenuEventArgs> handler)
+        {
+            AttachToWidget(widget);
+            ContextMenu += handler;
+        }
+
+        public void AttachToWidget(Widget widget)
+        {
+            widget.PopupMenu += Widget_PopupMenu;
+            widget.ButtonPressEvent += Widget_ButtonPressEvent;
+        }
+
+        public void DetachFromWidget(Widget widget)
+        {
+            widget.PopupMenu -= Widget_PopupMenu;
+            widget.ButtonPressEvent -= Widget_ButtonPressEvent;
+        }
+
+        [GLib.ConnectBefore]
+        private void Widget_PopupMenu(object o, PopupMenuArgs args)
+        {
+            args.RetVal = true;
+            RaiseContextMenuEvent(args, (Widget)o, false);
+        }
+
+        [GLib.ConnectBefore]
+        private void Widget_ButtonPressEvent(object o, ButtonPressEventArgs args)
+        {
+            if (args.Event.Button == 3 && args.Event.Type == EventType.ButtonPress)
+            {
+                args.RetVal = true;
+                RaiseContextMenuEvent(args, (Widget)o, true);
+            }
+        }
+
+        private bool propagating = false;   //Prevent reentry
+
+        private void RaiseContextMenuEvent(GLib.SignalArgs signalArgs, Widget widget, bool rightClick)
+        {
+            if (!propagating)
+            {
+                //Propagate the event
+                Event evnt = Gtk.Global.CurrentEvent;
+                propagating = true;
+                Gtk.Global.PropagateEvent(widget, evnt);
+                propagating = false;
+                signalArgs.RetVal = true;     //The widget already processed the event in the propagation
+
+                //Raise the context menu event
+                ContextMenuEventArgs args = new ContextMenuEventArgs(widget, rightClick);
+                if (ContextMenu != null)
+                {
+                    ContextMenu.Invoke(this, args);
+                }
+            }
+        }
+    }
+}
