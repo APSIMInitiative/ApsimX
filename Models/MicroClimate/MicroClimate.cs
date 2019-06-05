7eff91f5e1855e2cb989268420983c98e668f84f
sno036 <val.snow@agresearch.co.nz>
2019-06-05 13:31:29 +1200

    Fix typo

--- Models/MicroClimate/MicroClimate.cs
+++ Models/MicroClimate/MicroClimate.cs
@@ -301,7 +301,7 @@ namespace Models
         {
             dayLengthLight = MathUtilities.DayLength(Clock.Today.DayOfYear, SunSetAngle, weather.Latitude);
             dayLengthEvap = MathUtilities.DayLength(Clock.Today.DayOfYear, SunAngleNetPositiveRadiation, weather.Latitude);
-            // VOS - a temporary kludge to get this running for high latitudes. MicroMet is due for a clean up so reconsider then.
+            // VOS - a temporary kludge to get this running for high latitudes. MicroMet is due for a clean up soon so reconsider then.
             dayLengthEvap = Math.Max(dayLengthEvap, (dayLengthLight * 2.0 / 3.0)); 
 
             if (zoneMicroClimates.Count == 2 && zoneMicroClimates[0].zone is Zones.RectangularZone && zoneMicroClimates[1].zone is Zones.RectangularZone)
