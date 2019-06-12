367f1f8982f20ee862e3527fb8025db0c603df90
lie112 <lie112@CODER-DA>
2019-06-12 11:39:56 +0930

    CLEM Added ConvertTo functionality for all ResourceTypes.

--- Models/CLEM/ZoneCLEM.cs
+++ Models/CLEM/ZoneCLEM.cs
@@ -24,6 +24,7 @@ namespace Models.CLEM
     [ValidParent(ParentType = typeof(Zone))]
     [Description("This manages all CLEM resources and activities in the simulation.")]
     [HelpUri(@"Content/Features/CLEMComponent.htm")]
+    [Version(1, 0, 2, "New <a href=\"http://www.apsim.info/CLEM/Content/Features/Resources/ResourceUnitsConverter.htm\">ResourceUnitConverter</a> functionality added that changes some reporting.\nThis change will cause errors for all previous custom resource ledger reports created using the APSIM Report component.\nTo fix errors add \".Name\" to all LastTransaction.ResourceType and LastTransaction.Activity entries in custom ledgers (i.e. LastTransaction.ResourceType.Name as Resource). The CLEM ReportResourceLedger component has been updated to automatically handle the changes.")]
     [Version(1,0,1,"")]
     [ScopedModel]
     public class ZoneCLEM: Zone, IValidatableObject, ICLEMUI
