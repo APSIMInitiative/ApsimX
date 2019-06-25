ebabf5c3f6ae2c416a0f4d680c34d37d01141448
lie112 <lie112@CODER-DA>
2019-06-25 17:10:44 +0930

    CLEM Moved timer reporting to ActivityFolder to simplify Timers and remove multiple records being produced in output.

--- Models/CLEM/Activities/ActivityTimerMonthRange.cs
+++ Models/CLEM/Activities/ActivityTimerMonthRange.cs
@@ -66,22 +66,7 @@ namespace Models.CLEM.Activities
         {
             get
             {
-                bool due = IsMonthInRange(Clock.Today);
-                if (due)
-                {
-                    // report activity performed.
-                    ActivityPerformedEventArgs activitye = new ActivityPerformedEventArgs
-                    {
-                        Activity = new BlankActivity()
-                        {
-                            Status = ActivityStatus.Timer,
-                            Name = this.Name
-                        }
-                    };
-                    activitye.Activity.SetGuID(this.UniqueID);
-                    this.OnActivityPerformed(activitye);
-                }
-                return due;
+                return IsMonthInRange(Clock.Today);
             }
         }
 
@@ -118,7 +103,7 @@ namespace Models.CLEM.Activities
         /// Activity has occurred 
         /// </summary>
         /// <param name="e"></param>
-        protected virtual void OnActivityPerformed(EventArgs e)
+        public virtual void OnActivityPerformed(EventArgs e)
         {
             ActivityPerformed?.Invoke(this, e);
         }
