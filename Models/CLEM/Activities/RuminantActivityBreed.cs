1cadd0b7f664fb2e3c321c6f110640e58660421a
lie112 <lie112@CODER-DA>
2019-07-31 09:04:39 +0930

    CLEM Fixed condition filter to apply prenatal mortality

--- Models/CLEM/Activities/RuminantActivityBreed.cs
+++ Models/CLEM/Activities/RuminantActivityBreed.cs
@@ -246,12 +246,12 @@ namespace Models.CLEM.Activities
             foreach (var location in breeders)
             {
                 // determine all foetus and newborn mortality of all pregnant females.
-                foreach (RuminantFemale female in location.Where(a => a.Gender == Sex.Female).Cast<RuminantFemale>().Where(a => a.IsPregnant | a.BirthDue).ToList())
+                foreach (RuminantFemale female in location.Where(a => a.Gender == Sex.Female).Cast<RuminantFemale>().Where(a => a.IsPregnant).ToList())
                 {
                     // calculate foetus and newborn mortality 
                     // total mortality / (gestation months + 1) to get monthly mortality
                     // done here before births to account for post birth motality as well..
-                    // IsPregnant or BirthDue month
+                    // IsPregnant status does not change until births occur in next section so will include mortality in month of birth
                     // needs to be caclulated for each offspring carried.
                     for (int i = 0; i < female.CarryingCount; i++)
                     {
