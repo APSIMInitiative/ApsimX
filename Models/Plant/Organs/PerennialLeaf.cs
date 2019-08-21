5712602875684f063e65a5a85584079335c10c0a
Neil Huth <neil.huth@csiro.au>
2019-08-21 12:30:21 +1000

    Removed MicroClimatePresent and other useless bits

--- Models/Plant/Organs/PerennialLeaf.cs
+++ Models/Plant/Organs/PerennialLeaf.cs
@@ -20,7 +20,7 @@ namespace Models.PMF.Organs
     [Serializable]
     [ViewName("UserInterface.Views.GridView")]
     [PresenterName("UserInterface.Presenters.PropertyPresenter")]
-    public class PerennialLeaf : Model, IOrgan, ICanopy, ILeaf, IArbitration, IHasWaterDemand, IRemovableBiomass
+    public class PerennialLeaf : Model, IOrgan, ICanopy, IArbitration, IHasWaterDemand, IRemovableBiomass
     {
         /// <summary>The met data</summary>
         [Link]
@@ -105,34 +105,6 @@ namespace Models.PMF.Organs
         [XmlIgnore]
         public Biomass Detached { get; set; }
 
-        #region Leaf Interface
-        /// <summary>
-        /// Number of initiated cohorts that have not appeared yet
-        /// </summary>
-        public int ApicalCohortNo { get; set; }
-        /// <summary>
-        /// reset leaf numbers
-        /// </summary>
-        public void Reset() { }
-        /// <summary></summary>
-        public bool CohortsInitialised { get; set; }
-        /// <summary></summary>
-        public int TipsAtEmergence { get; set; }
-        /// <summary></summary>
-        public int CohortsAtInitialisation { get; set; }
-        /// <summary></summary>
-        public int AppearedCohortNo { get; set; }
-        /// <summary></summary>
-        public double PlantAppearedLeafNo { get; set; }
-        /// <summary></summary>
-        /// <param name="proprtionRemoved"></param>
-        public void DoThin(double proprtionRemoved) { }
-        /// <summary></summary>
-        public int InitialisedCohortNo { get; set; }
-        /// <summary></summary>
-        public void RemoveHighestLeaf() { }
-        #endregion
-
         #region Canopy interface
 
         /// <summary>Gets the canopy. Should return null if no canopy present.</summary>
@@ -159,8 +131,6 @@ namespace Models.PMF.Organs
         /// <summary>Gets or sets the R50.</summary>
         [Description("R50")]
         public double R50 { get; set; }
-
-
 
         /// <summary>Gets the LAI</summary>
         [Units("m^2/m^2")]
@@ -216,28 +186,20 @@ namespace Models.PMF.Organs
         /// <summary>Gets or sets the FRGR.</summary>
         [Units("mm")]
         public double FRGR { get; set; }
-
+         
         private double _PotentialEP = 0;
         /// <summary>Sets the potential evapotranspiration. Set by MICROCLIMATE.</summary>
         [Units("mm")]
         public double PotentialEP
         {
             get { return _PotentialEP; }
-            set
-            {
-                _PotentialEP = value;
-                MicroClimatePresent = true;
-            }
+            set { _PotentialEP = value;}
         }
 
         /// <summary>Sets the actual water demand.</summary>
         [Units("mm")]
         public double WaterDemand { get; set; }
 
-        /// <summary>
-        /// Flag to test if Microclimate is present
-        /// </summary>
-        public bool MicroClimatePresent { get; set; }
 
         /// <summary>Sets the light profile. Set by MICROCLIMATE.</summary>
         public CanopyEnergyBalanceInterceptionlayerType[] LightProfile { get; set; }
@@ -373,15 +335,10 @@ namespace Models.PMF.Organs
         {
             get
             {
-                if (MicroClimatePresent)
-                {
-                    double TotalRadn = 0;
-                    for (int i = 0; i < LightProfile.Length; i++)
-                        TotalRadn += LightProfile[i].amount;
-                    return TotalRadn;
-                }
-                else
-                   return CoverGreen * MetData.Radn;
+                 double TotalRadn = 0;
+                 for (int i = 0; i < LightProfile.Length; i++)
+                     TotalRadn += LightProfile[i].amount;
+                 return TotalRadn;
             }
         }
 
@@ -759,7 +716,6 @@ namespace Models.PMF.Organs
         [EventSubscribe("PlantSowing")]
         protected void OnPlantSowing(object sender, SowPlant2Type data)
         {
-                MicroClimatePresent = false;
                 Clear();
         }
 
@@ -779,9 +735,6 @@ namespace Models.PMF.Organs
         {
             if (Plant.IsEmerged)
             {
-                if (MicroClimatePresent == false)
-                    throw new Exception(this.Name + " is trying to calculate water demand but no MicroClimate module is present.  Include a microclimate node in your zone");
-
                 Detached.Clear();
                 FRGR = FRGRFunction.Value();
                 Height = HeightFunction.Value();
