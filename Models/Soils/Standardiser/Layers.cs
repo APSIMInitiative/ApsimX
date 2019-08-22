503f9967168fdf90a22c4c7c760303735c148205
Dean Holzworth <Dean.Holzworth@csiro.au>
2019-08-23 09:47:58 +1000

    Fix to get Weirdo working - doesnt have a water node

--- Models/Soils/Standardiser/Layers.cs
+++ Models/Soils/Standardiser/Layers.cs
@@ -67,12 +67,11 @@
                 soil.SAT = MapConcentration(soil.SAT, soil.Thickness, toThickness, MathUtilities.LastValue(soil.SAT));
                 soil.KS = MapConcentration(soil.KS, soil.Thickness, toThickness, MathUtilities.LastValue(soil.KS));
                 soil.Thickness = toThickness;
-                if (water.ParticleSizeClay != null && water.ParticleSizeClay.Length == water.Thickness.Length)
-                {
-                    water.ParticleSizeClay = MapConcentration(water.ParticleSizeClay, water.Thickness, toThickness, MathUtilities.LastValue(water.ParticleSizeClay));
-                }
-                else
-                    water.ParticleSizeClay = null;
+                if (water != null)
+                    if (water.ParticleSizeClay != null && water.ParticleSizeClay.Length == water.Thickness.Length)
+                        water.ParticleSizeClay = MapConcentration(water.ParticleSizeClay, water.Thickness, toThickness, MathUtilities.LastValue(water.ParticleSizeClay));
+                    else
+                        water.ParticleSizeClay = null;
                 
                 if (needToConstrainCropLL)
                 {
