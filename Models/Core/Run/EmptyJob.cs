296c5bec4793b8286257410688f5eaef7739e521
Jason <Brider>
2019-09-11 16:07:32 +1000

    JobRunnerMultiProcess was expecting something in the output for a job
    Added null check

--- Models/Core/Run/EmptyJob.cs
+++ Models/Core/Run/EmptyJob.cs
@@ -3,17 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Models.Core.Run
 {
+    [Serializable]
     class EmptyJob : IRunnable
     {
         /// <summary>Called to start the job. Can throw on error.</summary>
         /// <param name="cancelToken">Is cancellation pending?</param>
         public void Run(System.Threading.CancellationTokenSource cancelToken)
         {
-            //doing nothing
+            //do nothing
         }
 
     }
