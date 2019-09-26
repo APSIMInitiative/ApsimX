860e0faf41c6e1155d69f7b3b0ad5d991ff87014
NEXUS\zur003 <Eric.Zurcher@csiro.au>
2019-09-27 09:45:44 +1000

    Fix duplicate AssemblyInfo problem.

--- APSIM.Shared/Properties/AssemblyInfo.cs
+++ APSIM.Shared/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CSIRO")]
 [assembly: AssemblyProduct("APSIM.Shared")]
-[assembly: AssemblyCopyright("Copyright © APSIM Initiative 2016")]
+[assembly: AssemblyCopyright("Copyright © APSIM Initiative 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
