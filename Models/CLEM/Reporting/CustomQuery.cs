9bcc8b7876b51a563f99d5daff3e808f026c1cf3
lie112 <lie112@CODER-DA>
2019-10-07 16:53:55 +0930

    CLEM Fixed CustomQuery to remove link to summary error

--- Models/CLEM/Reporting/CustomQuery.cs
+++ Models/CLEM/Reporting/CustomQuery.cs
@@ -20,14 +20,11 @@ namespace Models.CLEM.Reporting
     [PresenterName("ApsimNG.Presenters.CLEM.CustomQueryPresenter")]
     [ValidParent(ParentType = typeof(ZoneCLEM))]
     [Description("Allows custom SQL queries to be applied to the DataStore.")]
-    [HelpUri(@"Content/Features/Reporting/CropDataReaderSQLite.htm")]
     [Version(1, 0, 1, "")]
     public class CustomQuery : Model, IPostSimulationTool
     {
         [Link]
         private IDataStore dataStore = null;
-        [Link]
-        private Summary Summary = null;
 
         /// <summary>
         /// Raw text of an SQL query
@@ -51,14 +48,11 @@ namespace Models.CLEM.Reporting
         {
             var data = dataStore.Reader.GetDataUsingSql(Sql);
 
-            if (data == null)
+            if (data != null)
             {
-                Summary.WriteWarning(this, String.Format("@warning:No results were found in SQL query for [r={0}]", this.Name));
-                return;
+                data.TableName = Tablename;
+                dataStore.Writer.WriteTable(data);
             }
-
-            data.TableName = Tablename;
-            dataStore.Writer.WriteTable(data);
         }
     }
 }
