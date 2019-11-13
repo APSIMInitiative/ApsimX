604a9526c3e9fe6071e26e266878b43c95734960
Dean Holzworth <Dean.Holzworth@csiro.au>
2019-11-13 16:37:45 +1300

    Fixed bug in stock - prpnToRemove doesn't work with 1 species

--- Models/Stock/stock_padd.cs
+++ Models/Stock/stock_padd.cs
@@ -915,7 +915,7 @@ namespace Models.GrazPlan
                     if (organ.IsAboveGround && (organ.Live.Wt + organ.Dead.Wt) > 0)
                     {
                         double propnOfPlantDM = (organ.Live.Wt + organ.Dead.Wt) / totalDM;
-                        double prpnToRemove = propnRemoved * propnOfPlantDM / (1.0 - propnOfPlantDM);
+                        double prpnToRemove = propnRemoved * propnOfPlantDM;// / (1.0 - propnOfPlantDM);
                         prpnToRemove = Math.Min(prpnToRemove, 1.0);
                         PMF.OrganBiomassRemovalType removal = new PMF.OrganBiomassRemovalType();
                         removal.FractionDeadToRemove = prpnToRemove;
