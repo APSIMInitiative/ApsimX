f8bc53eb997c8854f3699f0e850bffd836ac7c7c
Shaun <shaun.verrall@csiro.au>
2020-01-17 11:56:29 +1000

    set data location to dropbox shared folder

--- Prototypes/Cotton/munge-experiment-data/scripts/apsimclassic.r
+++ Prototypes/Cotton/munge-experiment-data/scripts/apsimclassic.r
@@ -1,9 +1,10 @@
+
+
+
 #! /bin/bash
 
 
 library(tidyverse)
-
-
 
 #writes .xlsx files. (you need to install the package from CRAN)
 #https://cran.r-project.org/web/packages/writexl/writexl.pdf
@@ -12,8 +13,15 @@ library(writexl)
 
 
 
-myall <- read_csv("./data/APSIMClassicValidation/MyallValeObservedData.csv")
-gac <- read_csv("./data/APSIMClassicValidation/GACObservedData.csv")
+dir_sourcedata <- file.path("C:","Users","ver078","Dropbox","CottonModel","OldData","APSIMClassicValidation")
+
+path_myall <- file.path(dir_sourcedata, "MyallValeObservedData.csv")
+path_gac <- file.path(dir_sourcedata, "GACObservedData.csv")
+
+
+
+myall <- read_csv(path_myall)
+gac <- read_csv(path_gac)
 
 typeof(myall)
 typeof(gac)
