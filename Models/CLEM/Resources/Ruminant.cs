cbeb63261d672119c54aba097d087f0ef4f32540
lie112 <lie112@CODER-DA>
2020-01-30 13:59:17 +0930

    CLEM Only update normalised weight and adult equivalent values on change of age and weight to reduce coalculation demand.

--- Models/CLEM/Resources/Ruminant.cs
+++ Models/CLEM/Resources/Ruminant.cs
@@ -15,6 +15,9 @@ namespace Models.CLEM.Resources
     {
         private RuminantFemale mother;
         private double weight;
+        private double age;
+        private double normalisedWeight;
+        private double adultEquivalent;
 
         /// <summary>
         /// Reference to the Breed Parameters.
@@ -73,7 +76,18 @@ namespace Models.CLEM.Resources
         /// Age (Months)
         /// </summary>
         /// <units>Months</units>
-        public double Age { get; private set; }
+        public double Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                age = value;
+                normalisedWeight = StandardReferenceWeight - ((1 - BreedParams.SRWBirth) * StandardReferenceWeight) * Math.Exp(-(BreedParams.AgeGrowthRateCoefficient * (Age * 30.4)) / (Math.Pow(StandardReferenceWeight, BreedParams.SRWGrowthScalar)));
+            }
+        }
 
         /// <summary>
         /// The age (months) this individual entered the simulation.
@@ -118,6 +132,8 @@ namespace Models.CLEM.Resources
             {
                 weight = value;
 
+                adultEquivalent = Math.Pow(this.Weight, 0.75) / Math.Pow(this.BreedParams.BaseAnimalEquivalent, 0.75);
+
                 // if highweight has not been defined set to initial weight
                 if (HighWeight == 0)
                 {
@@ -142,7 +158,7 @@ namespace Models.CLEM.Resources
         /// <summary>
         /// The adult equivalent of this individual
         /// </summary>
-        public double AdultEquivalent { get { return Math.Pow(this.Weight, 0.75) / Math.Pow(this.BreedParams.BaseAnimalEquivalent, 0.75); } }
+        public double AdultEquivalent { get { return adultEquivalent; } }
         // Needs to include ind.Number*weight if ever added to this model
 
         /// <summary>
@@ -386,7 +402,8 @@ namespace Models.CLEM.Resources
         {
             get
             {
-                return StandardReferenceWeight - ((1 - BreedParams.SRWBirth) * StandardReferenceWeight) * Math.Exp(-(BreedParams.AgeGrowthRateCoefficient * (Age * 30.4)) / (Math.Pow(StandardReferenceWeight, BreedParams.SRWGrowthScalar)));
+                return normalisedWeight;
+                //return StandardReferenceWeight - ((1 - BreedParams.SRWBirth) * StandardReferenceWeight) * Math.Exp(-(BreedParams.AgeGrowthRateCoefficient * (Age * 30.4)) / (Math.Pow(StandardReferenceWeight, BreedParams.SRWGrowthScalar)));
             }
         }
 
@@ -517,10 +534,10 @@ namespace Models.CLEM.Resources
         /// </summary>
         public Ruminant(double setAge, Sex setGender, double setWeight, RuminantType setParams)
         {
-            this.Age = setAge;
-            this.AgeEnteredSimulation = setAge;
             this.Gender = setGender;
             this.BreedParams = setParams;
+            this.Age = setAge;
+            this.AgeEnteredSimulation = setAge;
 
             if (setWeight <= 0)
             {
