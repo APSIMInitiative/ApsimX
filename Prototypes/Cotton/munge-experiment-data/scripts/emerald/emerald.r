f43ad5a42896b6c03a63fe5a2e0571446be16be4
Shaun <shaun.verrall@csiro.au>
2020-02-10 12:29:57 +1000

    add r code for phenology and fix bugs

--- Prototypes/Cotton/munge-experiment-data/scripts/emerald/emerald.r
+++ Prototypes/Cotton/munge-experiment-data/scripts/emerald/emerald.r
@@ -22,7 +22,16 @@ soilwater <- bind_rows(sw2015, sw2016)
 # WRITE TO SPREADSHEET
 # --------------------
 
-sheets <- list(Yield = yield, Fruit = fruit, SoilWater = soilwater)  
+sheets <- list(Phenology = phenology, Yield = yield, Fruit = fruit, SoilWater = soilwater)  
 write_xlsx(x = sheets, path = "./output/emerald.xlsx")
 
 
+
+# PLOT THE DATA 
+# -------------
+
+
+
+ggplot(data = fruit, mapping = aes(x=das, y=LAI, size=FruitNum)) + geom_point() + facet_grid(sowing ~ year) 
+
+ggplot(data = fruit, mapping = aes(x=das, y=LAI, size=FruitNum, color=sowing)) + geom_point() + geom_smooth() + facet_grid(sowing ~ year)
--- Prototypes/Cotton/munge-experiment-data/scripts/emerald/emerald_yield_fruit.r
+++ Prototypes/Cotton/munge-experiment-data/scripts/emerald/emerald_yield_fruit.r
@@ -19,27 +19,30 @@ path <- file.path(dir_sourcedata, "EmeraldObs.xlsx")
 
 
 heading <- read_xlsx(path, sheet = "EmeraldObs", range = "A1:Q1") 
-yield <- read_xlsx(path, sheet = "EmeraldObs", range = "A3:Q16", col_names = colnames(heading),  na = "*") 
+raw <- read_xlsx(path, sheet = "EmeraldObs", range = "A3:Q16", col_names = colnames(heading),  na = "*") 
 
 # remove the empty columns
-yield <- yield %>% select( -bolls_sc, -lai_max, -dw_total, -nuptake_total, -esw)
+raw <- raw %>% select( -bolls_sc, -lai_max, -dw_total, -nuptake_total, -esw)
 
 # split the title column to create a year and sowing column. 
-yield <- yield %>% separate(col= "title", into = c("year", "sowing"), sep = "_", remove = TRUE)
+raw <- raw %>% separate(col= "title", into = c("year", "sowing"), sep = "_", remove = TRUE)
 
 # clean up values in year to be just one year.
 # nb. year goes by the year of sowing (not the year of harvesting) 
 # eg. 2015-2016 year would be called 2015.  
 
-yield$year <- case_when( 
-                yield$year == "Emer1314" ~ "2013",
-                yield$year == "Emer1415" ~ "2014",
-                yield$year == "Emer1516" ~ "2015",
-                yield$year == "Emer1617" ~ "2016",
+raw$year <- case_when( 
+                raw$year == "Emer1314" ~ "2013",
+                raw$year == "Emer1415" ~ "2014",
+                raw$year == "Emer1516" ~ "2015",
+                raw$year == "Emer1617" ~ "2016"
                 )
 
+yield <- raw %>% select(-fs_DAS, -ff_DAS, -fob_DAS, -cutOut_DAS, -mat_DAS, -defol_DAS)
 
+phenology <- raw %>% select(year, sowing, Variety, first_square = fs_DAS, first_flower = ff_DAS, first_open_boll = fob_DAS, cut_out = cutOut_DAS, maturity = mat_DAS, defoliation = defol_DAS)
 
+phenology <- phenology %>% pivot_longer(cols = c(first_square, first_flower, first_open_boll, cut_out, maturity, defoliation), names_to = "stage_name", values_to = "das")
 
 
 
@@ -48,7 +51,7 @@ yield$year <- case_when(
 
 
 heading <- read_xlsx(path, sheet =  "Emerald_TimeSeries", range = "A1:E1") 
-fruit <- read_xlsx(path, sheet = "Emerald_TimeSeries", range = "A3:E72", col_names = colnames(heading))
+fruit <- read_xlsx(path, sheet = "Emerald_TimeSeries", range = "A3:E72", col_names = colnames(heading), na = "*")
 
 
 
@@ -63,8 +66,10 @@ fruit$year <- case_when(
   fruit$year == "Emer1314" ~ "2013",
   fruit$year == "Emer1415" ~ "2014",
   fruit$year == "Emer1516" ~ "2015",
-  fruit$year == "Emer1617" ~ "2016",
+  fruit$year == "Emer1617" ~ "2016"
 )
 
 
-
+                     
+                          
+                          
