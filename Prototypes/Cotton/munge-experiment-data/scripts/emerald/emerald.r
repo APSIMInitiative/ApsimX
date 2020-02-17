f9076150980d7f31b6dea3c7b9f728c64634ae6e
Shaun <shaun.verrall@csiro.au>
2020-02-17 12:58:07 +1000

    Add Clock.Today and SimulationName columns to spreadsheet

--- Prototypes/Cotton/munge-experiment-data/scripts/emerald/emerald.r
+++ Prototypes/Cotton/munge-experiment-data/scripts/emerald/emerald.r
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 
+library(lubridate)
 
 #writes .xlsx files. 
 #(you need to install the package from CRAN)
@@ -12,10 +13,35 @@ library(writexl)
 
 
 
-# Phenology 
-# ---------
+# JOIN DATASETS
 
 soilwater <- bind_rows(sw2015, sw2016)
+
+
+
+
+# ADD SimulationName Column and Clock.Today column 
+# ------------------------------------------------
+# nb. this is needed to do predicted vs observed in ApsimX
+
+yield <- yield %>% mutate("Clock.Today" = format(HarvestDate, "%d/%m/%Y"))
+yield <- yield %>% mutate(SimulationName = paste("Emerald_", year, "Sow", sowing, sep = ""))
+yield <- yield %>% select(Clock.Today, SimulationName, everything())
+
+#need to create a date column using sowdate and das.
+phenology <- phenology %>% mutate("Clock.Today" = format(HarvestDate, "%d/%m/%Y"))
+phenology <- phenology %>% mutate(SimulationName = paste("Emerald_", year, "Sow", sowing, sep = ""))
+phenology <- phenology %>% select(Clock.Today, SimulationName, everything())
+
+fruit <- fruit %>% mutate("Clock.Today" = format(Date, "%d/%m/%Y"))
+fruit <- fruit %>% mutate(SimulationName = paste("Emerald_", year, "Sow", sowing, sep = ""))
+fruit <- fruit %>% select(Clock.Today, SimulationName, everything())
+
+soilwater <- soilwater %>% mutate("Clock.Today" = format(date, "%d/%m/%Y"))
+soilwater <- soilwater %>% mutate(SimulationName = paste("Emerald_", year, "Sow", sowing, sep = ""))
+soilwater <- soilwater %>% select(Clock.Today, SimulationName, everything())
+
+
 
 
 
