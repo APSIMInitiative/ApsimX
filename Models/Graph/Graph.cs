570ac2c295cd4e1ab347d9de928bc48ac0cbccc1
hol430 <drew.holzworth@csiro.au>
2020-03-13 06:30:10 +1000

    Profile graph legend position should be rigt-middle, outside of graph area.

--- Models/Graph/Graph.cs
+++ Models/Graph/Graph.cs
@@ -37,45 +37,66 @@
         /// </remarks>
         public enum LegendPositionType
         {
-            /// <summary>
-            /// Top left corner of the graph
-            /// </summary>
-            TopLeft,
 
             /// <summary>
-            /// Top right corner of the graph
+            /// Place the legend box in the top-left corner.
             /// </summary>
-            TopRight,
+            TopLeft = 0,
 
             /// <summary>
-            /// Bottom left corner of the graph
+            ///     Place the legend box centered at the top.
             /// </summary>
-            BottomLeft,
+            TopCenter = 1,
 
             /// <summary>
-            /// Bottom right corner of the graph
+            /// Place the legend box in the top-right corner.
             /// </summary>
-            BottomRight,
+            TopRight = 2,
 
             /// <summary>
-            /// Left side of the graph, in the middle.
+            /// Place the legend box in the bottom-left corner.
             /// </summary>
-            LeftMiddle,
+            BottomLeft = 3,
 
             /// <summary>
-            /// Right side of the graph, in the middle.
+            /// Place the legend box centered at the bottom.
             /// </summary>
-            RightMiddle,
+            BottomCenter = 4,
 
             /// <summary>
-            /// Top of the graph, in the middle
+            /// Place the legend box in the bottom-right corner.
             /// </summary>
-            TopCenter,
+            BottomRight = 5,
 
             /// <summary>
-            /// Bottom of the graph, in the middle
+            /// Place the legend box in the left-top corner.
             /// </summary>
-            BottomCenter
+            LeftTop = 6,
+
+            /// <summary>
+            /// Place the legend box centered at the left.
+            /// </summary>
+            LeftMiddle = 7,
+
+            /// <summary>
+            /// Place the legend box in the left-bottom corner.
+            /// </summary>
+            LeftBottom = 8,
+
+            /// <summary>
+            /// Place the legend box in the right-top corner.
+            /// </summary>
+            RightTop = 9,
+
+            /// <summary>
+            /// Place the legend box centered at the right.
+            /// </summary>
+            RightMiddle = 10,
+
+            /// <summary>
+            /// Place the legend box in the right-bottom corner.
+            /// </summary>
+            RightBottom = 11
         }
 
         /// <summary>
