2ee6e0e639bceb6feddb87f9ce0965f6d846de64
hol430 <drew.holzworth@csiro.au>
2020-03-17 11:53:33 +1000

    Improved edit file's model replacement capability
    Also expanded the unit test for this feature.

--- Tests/UnitTests/Core/ApsimFile/EditFileTests.cs
+++ Tests/UnitTests/Core/ApsimFile/EditFileTests.cs
@@ -26,6 +26,8 @@ namespace UnitTests.Core.ApsimFile
         /// </summary>
         private string fileName;
 
+        private string extFile;
+
         [SetUp]
         public void Initialise()
         {
@@ -39,6 +41,24 @@ namespace UnitTests.Core.ApsimFile
             weather.FileName = "asdf.met";
             Structure.Add(weather, simulation);
 
+            // Add a second weather component.
+            Models.Weather weather2 = new Models.Weather();
+            weather2.FileName = "asdf.met";
+            weather2.Name = "Weather2";
+            Structure.Add(weather2, simulation);
+
+            // Add a third weather component.
+            Models.Weather weather3 = new Models.Weather();
+            weather3.FileName = "asdf.met";
+            weather3.Name = "Weather3";
+            Structure.Add(weather3, simulation);
+
+            // Add a third weather component.
+            Models.Weather weather4 = new Models.Weather();
+            weather4.FileName = "asdf.met";
+            weather4.Name = "Weather4";
+            Structure.Add(weather4, simulation);
+
             // Add a report.
             Models.Report report = new Models.Report();
             report.Name = "Report";
@@ -46,19 +66,46 @@ namespace UnitTests.Core.ApsimFile
             
             basicFile.Write(basicFile.FileName);
             fileName = basicFile.FileName;
+
+            // Create a new .apsimx file containing two weather nodes.
+            Simulations test = Utilities.GetRunnableSim();
+            IModel sim = Apsim.Find(test, typeof(Simulation));
+
+            Models.Weather w1 = new Models.Weather();
+            w1.FileName = "w1.met";
+            w1.Name = "w1";
+            Structure.Add(w1, sim);
+
+            Models.Weather w2 = new Models.Weather();
+            w2.Name = "w2";
+            w2.FileName = "w2.met";
+            Structure.Add(w2, sim);
+
+            extFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".apsimx");
+            test.Write(extFile);
         }
 
         [Test]
         public void TestEditing()
         {
+
             string configFile = Path.GetTempFileName();
             File.WriteAllLines(configFile, new[]
             {
                 // Modify an array
-                "[Report].VariableNames = x, y, z",
+                "[Report].VariableNames = x,y,z",
+
+                // Modify a date - try a few different formats.
+                "[Clock].StartDate = 2000-01-01",
+                "[Clock].EndDate = 2000-01-10T00:00:00",
 
                 // Modify a string
-                "[Weather].FileName = fdsa.met"
+                "[Weather].FileName = fdsa.met",
+                @"[Weather2].FullFileName = .\jkl.met",
+
+                // Replace a model with a model from another file.
+                $"[Weather3] = {extFile}",
+                $"[Weather4] = {extFile};[w2]",
             });
 
             string models = typeof(IModel).Assembly.Location;
@@ -68,15 +115,35 @@ namespace UnitTests.Core.ApsimFile
             proc.Start(models, args, Path.GetTempPath(), true, true);
             proc.WaitForExit();
 
+            Assert.AreEqual(null, proc.StdOut);
+            Assert.AreEqual(null, proc.StdErr);
+
             Simulations file = FileFormat.ReadFromFile<Simulations>(fileName, out List<Exception> errors);
             if (errors != null && errors.Count > 0)
                 throw errors[0];
 
             var report = Apsim.Find(file, typeof(Models.Report)) as Models.Report;
-            Assert.AreEqual(3, report.VariableNames.Length);
+            string[] variableNames = new[] { "x", "y", "z" };
+            Assert.AreEqual(variableNames, report.VariableNames);
 
-            var weather = Apsim.Find(file, typeof(Models.Weather)) as Models.Weather;
+            Clock clock = Apsim.Find(file, typeof(Clock)) as Clock;
+            Assert.AreEqual(new DateTime(2000, 1, 1), clock.StartDate);
+            Assert.AreEqual(new DateTime(2000, 1, 10), clock.EndDate);
+
+            var weather = Apsim.Find(file, "Weather") as Models.Weather;
             Assert.AreEqual("fdsa.met", weather.FileName);
+
+            var weather2 = Apsim.Find(file, "Weather2") as Models.Weather;
+            Assert.AreEqual(@".\jkl.met", weather2.FileName);
+
+            // Weather3 and Weather4 should have been renamed to w1 and w2, respectively.
+            var weather3 = Apsim.Find(file, "w1") as Models.Weather;
+            Assert.AreEqual("w1", weather3.Name);
+            Assert.AreEqual("w1.met", weather3.FileName);
+
+            var weather4 = Apsim.Find(file, "w2") as Models.Weather;
+            Assert.AreEqual("w2", weather4.Name);
+            Assert.AreEqual("w2.met", weather4.FileName);
         }
     }
 }
