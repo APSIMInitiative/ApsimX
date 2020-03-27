1465a1071a2d98780dc77d78a1c59585c1125a48
Dean Holzworth <Dean.Holzworth@csiro.au>
2020-03-27 13:10:30 +1000

    Added converter to change 'avg' to 'mean' in report variables. Fixed invalid char in Gliricidia validation file report var

--- Models/Core/ApsimFile/Converter.cs
+++ Models/Core/ApsimFile/Converter.cs
@@ -18,7 +18,7 @@
     public class Converter
     {
         /// <summary>Gets the latest .apsimx file format version.</summary>
-        public static int LatestVersion { get { return 88; } }
+        public static int LatestVersion { get { return 89; } }
 
         /// <summary>Converts a .apsimx string to the latest version.</summary>
         /// <param name="st">XML or JSON string to convert.</param>
@@ -1843,14 +1843,6 @@
             }
         }
 		
-
-
-
-
-
-
-
-
         /// <summary>
         /// Replace SoilWater model with WaterBalance model.
         /// </summary>
@@ -1913,6 +1905,17 @@
                 JsonUtilities.SearchReplaceReportVariableNames(report, "solute_flux_eff", "SoluteFluxEfficiency");
             }
 
+        }
+
+        /// <summary>
+        /// Replace 'avg' with 'mean' in report variables.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="fileName"></param>
+        private static void UpgradeToVersion89(JObject root, string fileName)
+        {
+            foreach (var report in JsonUtilities.ChildrenOfType(root, "Report"))
+                JsonUtilities.SearchReplaceReportVariableNames(report, "avg of ", "mean of ");
         }
 
         /// <summary>
