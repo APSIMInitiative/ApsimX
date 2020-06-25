4d64b772bd66d3bc3fe760b635347561b6f1f13b
Drew Holzworth <drew.holzworth@csiro.au>
2020-06-25 12:04:59 +1000

    Remove Sharp3UI directory.

--- Sharp3UI/UserInterface/Interfaces/ILabelView.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserInterface.Interfaces
-{
-    /// <summary>An interface for a label.</summary>
-    public interface ILabelView
-    {
-        /// <summary>Gets or sets the text of the label.</summary>
-        string Value { get; set; }
-    }
-}
