ed78677b384322fe5ed25c7af38298184c64abe1
Drew Holzworth <drew.holzworth@csiro.au>
2020-08-05 16:04:23 +1000

    Changed IRoot to inherit IWaterNitrogenUptake.

--- Models/PMF/Interfaces/IRoot.cs
+++ Models/PMF/Interfaces/IRoot.cs
@@ -6,21 +6,12 @@ namespace Models.PMF.Interfaces
     /// <summary>
     /// Root interface
     /// </summary>
-    public interface IRoot : IOrgan
+    public interface IRoot : IOrgan, IWaterNitrogenUptake
     {
         /// <summary>Root length density.</summary>
         double[] LengthDensity { get; }
 
         /// <summary>Root length density modifier due to damage.</summary>
         double RootLengthDensityModifierDueToDamage { get; set; }
-
-        /// <summary>Do the water uptake.</summary>
-        /// <param name="Amount">The amount.</param>
-        /// <param name="zoneName">Zone name to do water uptake in</param>
-        void DoWaterUptake(double[] Amount, string zoneName);
-
-        /// <summary>Do the Nitrogen uptake.</summary>
-        /// <param name="zonesFromSoilArbitrator">List of zones from soil arbitrator</param>
-        void DoNitrogenUptake(List<ZoneWaterAndN> zonesFromSoilArbitrator);
     }
 }
