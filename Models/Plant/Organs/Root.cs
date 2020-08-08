c3d3c1112dce23fe64330d7367122d1e17aea7f3
hut104 <neil.huth@csiro.au>
2020-08-08 14:44:35 +1000

    added exception

--- Models/Plant/Organs/Root.cs
+++ Models/Plant/Organs/Root.cs
@@ -556,15 +556,11 @@
                 if (dMCE > 0.0)
                 {
                     DMDemand.Structural = (dmDemands.Structural.Value() / dMCE + remobilisationCost.Value());
-                    DMDemand.Storage = Math.Max(0, dmDemands.Storage.Value() / dMCE) ;
+                    DMDemand.Storage = Math.Max(0, dmDemands.Storage.Value() / dMCE);
                     DMDemand.Metabolic = 0;
                 }
                 else
-                { // Conversion efficiency is zero!!!!
-                    DMDemand.Structural = 0;
-                    DMDemand.Storage = 0;
-                    DMDemand.Metabolic = 0;
-                }
+                    throw new Exception("dmConversionEfficiency should be greater than zero in " + Name);
 
                 if (dmDemandPriorityFactors != null)
                 {
