89969f6ab90e8efd74280b81c7be58e322f99134
Drew Holzworth <drew.holzworth@csiro.au>
2020-08-25 14:32:13 +1000

    Update jenkins to build API docs and publish to apsimdev on PR merges.

--- Jenkins/deploy.bat
+++ Jenkins/deploy.bat
@@ -8,6 +8,7 @@ if "%2"=="" (
 	echo %usage%
 	exit /b 1
 )
+if "%apsimx%"=="" set "apsimx=%~dp0.."
 
 rem Add build to builds database
 @curl -f "https://apsimdev.apsim.info/APSIM.Builds.Service/Builds.svc/AddBuild?pullRequestNumber=%1&ChangeDBPassword=%2"
@@ -24,3 +25,16 @@ if errorlevel 1 (
 	echo Error triggering netlify build
 	exit /b 1
 )
+
+rem Generate API docs
+cd "%apsimx%\Docs\docfx"
+docfx
+
+if errorlevel 1 (
+	echo Error generating API documentation
+	exit /b 1
+)
+
+rem Upload API docs to website
+cd "%apsimx%\Docs"
+rsync -arz apsimx-docs/ admin@apsimdev.apsim.info:/cygdrive/d/Websites/APSIM/apsimx-docs/
