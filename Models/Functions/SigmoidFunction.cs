f561a0f395690c183f32727c7803cd38d4fb73bf
Hamish <hamish.brown@plantandfoodco.nz>
2021-02-16 09:31:07 +1300

    complet merge

--- Models/Functions/SigmoidFunction.cs
+++ Models/Functions/SigmoidFunction.cs
@@ -8,7 +8,7 @@ using Newtonsoft.Json;
 namespace Models.Functions
 {
     /// <summary>
-    ///  [Name] is calcualted using a sigmoid function of the form y = Xmax * 1 / 1 + e<sup>-(Xvalue - Xo) / b</sup>. \n
+    ///  [Name] is calcualted using a sigmoid function of the form y = Xmax * 1 / 1 + e^-(Xvalue - Xo) / b^. \n
     ///  Ymax is calculated as 
     ///  [Document Ymax]
     ///  Xo is calculated as 
