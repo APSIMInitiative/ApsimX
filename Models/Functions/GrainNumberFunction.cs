dbdcec271e9ae7c6666d07f92ca59d341c20efe8
Drew Holzworth <drew.holzworth@csiro.au>
2021-02-18 15:55:34 +1000

    Move all sorghum grain temperature calculations into the grain number function.

--- Models/Functions/GrainNumberFunction.cs
+++ Models/Functions/GrainNumberFunction.cs
@@ -16,13 +16,6 @@ namespace Models.Functions
     public class GrainNumberFunction : Model, IFunction
     {
         /// <summary>
-        /// Temperature factor.
-        /// </summary>
-        [Units("0-1")]
-        [Link(Type = LinkType.Child, ByName = true)]
-        private IFunction temperatureFactor = null;
-
-        /// <summary>
         /// Number of degree days required for grain number to go from 0 (at start grain fill) to final grain number.
         /// </summary>
         [Link(Type = LinkType.Child, ByName = true)]
@@ -36,6 +29,28 @@ namespace Models.Functions
         [Link(Type = LinkType.Child, ByName = true)]
         [Units("?")]
         private IFunction dmPerSeed = null;
+
+        /// <summary>
+        /// Heat severity response.
+        /// </summary>
+        [Link(Type = LinkType.Child, ByName = true)]
+        private IFunction heatSeverity = null;
+
+        /// <summary>
+        /// Daily thermaltime delta.
+        /// </summary>
+        [Link(Type = LinkType.Path, Path = "[Phenology].DltTT")]
+        private IFunction dltTT = null;
+
+        /// <summary>
+        /// Flag leaf to flowering phenological phase.
+        /// </summary>
+        /// <remarks>
+        /// We need this because we need to know the TT target for this phase.
+        /// Could potentially refactor this out.
+        /// </remarks>
+        [Link(ByName = true)]
+        private GenericPhase flagLeafToFlowering = null;
 
         /// <summary>
         /// Link to phenology.
@@ -53,7 +68,19 @@ namespace Models.Functions
         /// Total accumulated thermal time from start grain fill to maturity.
         /// </summary>
         [NonSerialized]
-        private double accumulatedTT = 0;
+        private double ttGrainFillToMaturity = 0;
+
+        /// <summary>
+        /// Total accumulated thermaltime between flowering and maturity.
+        /// </summary>
+        [NonSerialized]
+        private double ttFloweringToMaturity = 0;
+
+        /// <summary>
+        /// Total accumulated thermal time between flag leaf and flowering.
+        /// </summary>
+        [NonSerialized]
+        private double ttFlagToFlowering = 0;
 
         /// <summary>
         /// The grain number, which gets updated at the end of the day, for consistency with old apsim.
@@ -80,6 +107,24 @@ namespace Models.Functions
         private double greenWtAtFloralInit = 0;
 
         /// <summary>
+        /// Effect of temperature on grain number.
+        /// </summary>
+        [NonSerialized]
+        private double temperatureFactor = 0;
+
+        /// <summary>
+        /// grainTempWindow[0] in old apsim.
+        /// </summary>
+        [NonSerialized]
+        private const double tempWindow0 = -50;
+        
+        /// <summary>
+        /// grainTempWindow[1] in old apsim.
+        /// </summary>
+        [NonSerialized]
+        private const double tempWindow1 = 100;
+
+        /// <summary>
         /// Get the grain number.
         /// </summary>
         /// <param name="arrayIndex">Array index (irrelevant for this function).</param>
@@ -90,22 +135,65 @@ namespace Models.Functions
         /// </summary>
         public void Clear()
         {
-            accumulatedTT = 0;
+            ttGrainFillToMaturity = 0;
+            ttFloweringToMaturity = 0;
+            ttFlagToFlowering = 0;
             value = 0;
             finalGrainNumber = 0;
             daysFIToStartGrainFill = 0;
             greenWtAtFloralInit = 0;
+            temperatureFactor = 1;
         }
 
         /// <summary>
         /// Calculate final grain number. Is called at start grain fill.
         /// </summary>
-        /// <returns></returns>
         private double CalculateFinalGrainNumber()
         {
             double plantGrowth = Math.Max(0, total.Wt - greenWtAtFloralInit);
             double growthRate = MathUtilities.Divide(plantGrowth, daysFIToStartGrainFill, 0);
             return MathUtilities.Divide(growthRate, dmPerSeed.Value(), 0);
+        }
+
+        /// <summary>
+        /// Calculate the daily temperature factor delta. Is called once per day, after flag leaf.
+        /// </summary>
+        private double CalculateTemperatureFactorDelta()
+        {
+            // calculate a daily contribution to stress on grain number
+            // if we are within the grain stress window (grainTempWindow)calculate stress factor
+            // from grainTempTable and this day's contribution to the total stress
+
+            // Calculate heat severity.
+            double heatStress = heatSeverity.Value();
+            // First see if it is a hot day.
+            if(heatStress < 0.001)
+                return 0.0;
+
+            // Check if we are in the pre-anthesis or post-anthesis window. If not return 0.
+            // Note: tempWindow0 is negative.
+            // todo: Could we change this to use one of the modern nextgen methods in phenology class?
+            double targetTT = flagLeafToFlowering.Target + tempWindow0;
+            if (ttFlagToFlowering < targetTT)
+                return 0.0;
+
+            // Check if in the post flag leaf window.
+            if (ttFloweringToMaturity > tempWindow1)
+                return 0.0;
+
+            double ttToday = dltTT.Value();
+            double ttContrib;
+
+            // Check window
+            if (ttFloweringToMaturity > 0.0)
+                // Post-anthesis
+                ttContrib = Math.Min(tempWindow1 - ttFloweringToMaturity, ttToday);
+            else
+                // Pre-flag leaf
+                ttContrib = Math.Min(ttFlagToFlowering - targetTT, ttToday);
+
+            double dayFract = MathUtilities.Divide(ttContrib, -1.0 * tempWindow0 + tempWindow1, 0);
+            return dayFract * heatStress;
         }
 
         /// <summary>
@@ -116,9 +204,7 @@ namespace Models.Functions
         [EventSubscribe("EndOfDay")]
         private void UpdateValue(object sender, EventArgs args)
         {
-            double tempFactor = temperatureFactor.Value();
-            double ttRamp = rampTT.Value();
-            value = tempFactor * Math.Min(finalGrainNumber, finalGrainNumber * MathUtilities.Divide(accumulatedTT, ttRamp, 0));
+            value = temperatureFactor * Math.Min(finalGrainNumber, finalGrainNumber * MathUtilities.Divide(ttGrainFillToMaturity, rampTT.Value(), 0));
         }
 
         /// <summary>Called by Plant.cs when phenology routines are complete.</summary>
@@ -129,12 +215,28 @@ namespace Models.Functions
         {
             if (phenology.Between("StartGrainFill", "Maturity"))
                 if (phenology.CurrentPhase is GenericPhase phase)
-                    accumulatedTT += phase.ProgressionForTimeStep;
+                    ttGrainFillToMaturity += phase.ProgressionForTimeStep;
+
+            if (phenology.Between("FlagLeaf", "Flowering"))
+                if (phenology.CurrentPhase is GenericPhase genPhase)
+                    ttFlagToFlowering += genPhase.ProgressionForTimeStep;
 
             if (phenology.Between("FloralInitiation", "StartGrainFill"))
                 daysFIToStartGrainFill++;
+
+            if (phenology.Between("Flowering", "Maturity"))
+                if (phenology.CurrentPhase is GenericPhase genericPhase)
+                    ttFloweringToMaturity += dltTT.Value();
+        
+            // Note: temperature factor calculations need to be BELOW the previous accumulator
+            // code, as it assumes that these TT values already include today's deltas.
+            if (phenology.Between("FlagLeaf", "Maturity"))
+            {
+                temperatureFactor -= CalculateTemperatureFactorDelta();
+                temperatureFactor = Math.Max(temperatureFactor, 0);
+            }
         }
-    
+
         /// <summary>Called when phenological phase changes. Updates grain number if the phase is start grainfill.</summary>
         /// <param name="phaseChange">The phase change.</param>
         /// <param name="sender">Sender plant.</param>
