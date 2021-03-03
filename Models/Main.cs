e88780f93f712b6278ce6b529ed6e5220eb1c052
Drew Holzworth <drew.holzworth@csiro.au>
2021-03-03 11:14:14 +1000

    Revert "Temp debugging change to get jenkins to run under invariant culture."
    This reverts commit f5bc9c701319bc1f81e4395d33ab1b317a1b24c6.

--- Models/Main.cs
+++ Models/Main.cs
@@ -9,7 +9,6 @@
     using Models.Factorial;
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Text;
@@ -28,7 +27,6 @@
         /// <returns> Program exit code (0 for success)</returns>
         public static int Main(string[] args)
         {
-            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 #if NETCOREAPP
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 #endif
