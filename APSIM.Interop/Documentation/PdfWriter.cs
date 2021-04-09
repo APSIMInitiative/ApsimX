91b609e646fddf48b6ced9dc844cf0da138da980
Drew Holzworth <drew.holzworth@csiro.au>
2021-04-09 12:15:36 +1000

    Added APSIM.Interop project
    This will contain I/O code for graphing, autodocs, etc.

---
+++ APSIM.Interop/Documentation/PdfWriter.cs
@@ -0,0 +1,14 @@
+using APSIM.Services.Documentation;
+using System;
+using System.Collections.Generic;
+
+namespace APSIM.Interop.Documentation
+{
+    public static class PdfWriter
+    {
+        public static void Write(string fileName, IEnumerable<ITag> tags)
+        {
+            
+        }
+    }
+}
