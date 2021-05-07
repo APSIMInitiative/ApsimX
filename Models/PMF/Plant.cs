c6dad0a99ef5bdb369173747043c9bcc6008fd80
Drew Holzworth <drew.holzworth@csiro.au>
2021-05-07 12:53:28 +1000

    Fixed several models which weren't zeroing themselves correctly at start of sim.

--- Models/PMF/Plant.cs
+++ Models/PMF/Plant.cs
@@ -207,7 +207,7 @@
 
 
         /// <summary>Amount of assimilate available to be damaged.</summary>
-        public double AssimilateAvailable => throw new NotImplementedException();
+        public double AssimilateAvailable => 0;
 
         /// <summary>Harvest the crop</summary>
         public void Harvest() { Harvest(null); }
