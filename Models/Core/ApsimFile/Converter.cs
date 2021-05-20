88a13744245883c29bcae13ba5bbd46d9511c82c
Drew Holzworth <drew.holzworth@csiro.au>
2021-05-20 14:20:54 +1000

    Replace all XmlIgnore with JsonIgnore in manager scripts

--- Models/Core/ApsimFile/Converter.cs
+++ Models/Core/ApsimFile/Converter.cs
@@ -23,7 +23,7 @@
     public class Converter
     {
         /// <summary>Gets the latest .apsimx file format version.</summary>
-        public static int LatestVersion { get { return 131; } }
+        public static int LatestVersion { get { return 132; } }
 
         /// <summary>Converts a .apsimx string to the latest version.</summary>
         /// <param name="st">XML or JSON string to convert.</param>
@@ -3443,6 +3443,25 @@
             {
                 JsonUtilities.RenameChildModel(Root, "DroughtInducedLagAcceleration", "LagAcceleration");
                 JsonUtilities.RenameChildModel(Root, "DroughtInducedSenAcceleration", "SenescenceAcceleration");
+            }
+        }
+
+        /// <summary>
+        /// Replace all XmlIgnore attributes with JsonIgnore attributes in manager scripts.
+        /// </summary>
+        /// <param name="root">Root node.</param>
+        /// <param name="fileName">Path to the .apsimx file.</param>
+        private static void UpgradeToVersion132(JObject root, string fileName)
+        {
+            foreach (ManagerConverter manager in JsonUtilities.ChildManagers(root))
+            {
+                bool changed = manager.Replace("[XmlIgnore]", "[JsonIgnore]");
+                changed |= manager.Replace("[System.Xml.Serialization.XmlIgnore]", "[JsonIgnore]");
+                if (changed)
+                {
+                    manager.AddUsingStatement("Newtonsoft.Json");
+                    manager.Save();
+                }
             }
         }
 
