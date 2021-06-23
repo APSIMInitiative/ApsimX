dc2b558df2aacd07bb7f16c0f2be4486e68d06d0
Dean Holzworth <Dean.Holzworth@csiro.au>
2021-06-23 15:10:48 +1000

    Fix for LIKE keyword in graph filters

--- Models/Graph/SeriesDefinition.cs
+++ Models/Graph/SeriesDefinition.cs
@@ -432,10 +432,25 @@
                 // Remove brackets.
                 localFilter = localFilter.Replace("(", "");
                 localFilter = localFilter.Replace(")", "");
+                localFilter = localFilter.Replace("[", "");
+                localFilter = localFilter.Replace("]", "");
 
                 // Look for individual filter clauses (e.g. A = B).
                 string clausePattern = @"\[?(?<FieldName>[^\s\]]+)\]?\s*(=|>|<|>=|<=)\s*(|'|\[|\w)";
                 match = Regex.Match(localFilter, clausePattern);
+                while (match.Success)
+                {
+                    if (!string.IsNullOrWhiteSpace(match.Groups["FieldName"].Value))
+                    {
+                        fieldNames.Add(match.Groups["FieldName"].Value);
+                        localFilter = localFilter.Remove(match.Index, match.Length);
+                    }
+                    match = Regex.Match(localFilter, clausePattern);
+                }
+
+                // Look for LIKE keyword
+                string likePattern = @"(?<FieldName>\S+)\s+LIKE";
+                match = Regex.Match(localFilter, likePattern);
                 while (match.Success)
                 {
                     if (!string.IsNullOrWhiteSpace(match.Groups["FieldName"].Value))
