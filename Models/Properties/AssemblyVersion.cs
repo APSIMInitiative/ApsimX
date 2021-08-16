2c9e487b6180601dad9e72440e739f117c6f49dc
Drew Holzworth <drew.holzworth@csiro.au>
2021-08-16 12:28:34 +1000

    Undo accidental changes to AssemblyVersion.cs.

--- Models/Properties/AssemblyVersion.cs
+++ Models/Properties/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.2.3.4")]
-[assembly: AssemblyFileVersion("1.2.3.4")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
 [assembly: AssemblyCopyright("Copyright © APSIM Initiative 2019")]
-[assembly: AssemblyTitle("APSIM 1.2.3.4")] 
+[assembly: AssemblyTitle("APSIM 0.0.0.0")] 
