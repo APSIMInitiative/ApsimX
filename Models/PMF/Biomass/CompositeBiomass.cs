b2cb39a7c1ddc4cd31ff134987e3979edab8c70d
Dean Holzworth <Dean.Holzworth@csiro.au>
2021-08-31 10:21:38 +1000

    Simplified CompositeBiomass

--- Models/PMF/Biomass/CompositeBiomass.cs
+++ Models/PMF/Biomass/CompositeBiomass.cs
@@ -3,54 +3,31 @@ using System.Collections;
 using Models.Core;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using Models.PMF.Interfaces;
+using System.Linq;
 
 namespace Models.PMF
 {
-    /// <summary>
-    /// This is a composite biomass class, representing the sum of 1 or more biomass objects.
-    /// </summary>
+    /// <summary>This is a composite biomass class, representing the sum of 1 or more biomass objects from one or more organs.</summary>
     [Serializable]
     [ViewName("UserInterface.Views.PropertyView")]
     [PresenterName("UserInterface.Presenters.PropertyPresenter")]
     [ValidParent(ParentType = typeof(Plant))]
-    public class CompositeBiomass : Biomass, ICustomDocumentation
+    public class CompositeBiomass : Model, ICustomDocumentation
     {
-        /// <summary>The propertys</summary>
-        [Description("List of organs to agregate into composite biomass")]
-        public string[] Propertys {get; set;}
+        private List<IOrganDamage> organs;
 
-        /// <summary>Update this biomass object.</summary>
-        public void Update()
-        {
-            base.Clear();
+        /// <summary>List of organs to agregate.</summary>
+        [Description("List of organs to agregate.")]
+        public string[] OrganNames {get; set;}
 
-            foreach (string PropertyName in Propertys)
-            {
-                object v = this.FindByPath(PropertyName)?.Value;
-                if (v == null)
-                    throw new Exception("Cannot find: " + PropertyName + " in composite biomass: " + this.Name);
+        /// <summary>Include live material?</summary>
+        [Description("Include live material?")]
+        public bool IncludeLive { get; set; }
 
-                if (v is IEnumerable)
-                {
-                    foreach (object i in v as IEnumerable)
-                    {
-                        if (i is CompositeBiomass)
-                            (i as CompositeBiomass).Update();
-                        if (!(i is Biomass))
-                            throw new Exception("Elements in the array: " + PropertyName + " are not Biomass objects in composite biomass: " + this.Name);
-                        Add(i as Biomass);
-                    }
-                }
-                else
-                {
-                    if (!(v is Biomass))
-                        throw new Exception("Property: " + PropertyName + " is not a Biomass object in composite biomass: " + this.Name);
-                    if (v is CompositeBiomass)
-                        (v as CompositeBiomass).Update();
-                    Add(v as Biomass);
-                }
-            }
-        }
+        /// <summary>Include dead material?</summary>
+        [Description("Include dead material?")]
+        public bool IncludeDead { get; set; }
 
         /// <summary>Clear ourselves.</summary>
         /// <param name="sender">The sender.</param>
@@ -58,99 +35,70 @@ namespace Models.PMF
         [EventSubscribe("Commencing")]
         private void OnSimulationCommencing(object sender, EventArgs e)
         {
-            Clear();
-        }
-
-        /// <summary>Gets or sets the non structural n.</summary>
-        /// <value>The non structural n.</value>
-        /// <exception cref="System.Exception">Cannot set StorageN in CompositeBiomass</exception>
-        [JsonIgnore]
-        [Units("g/m^2")]
-        override public double StorageN
-        {
-            get { Update(); return base.StorageN; }
-            set { throw new Exception("Cannot set StorageN in CompositeBiomass"); }
-        }
-
-        /// <summary>Gets or sets the structural n.</summary>
-        /// <value>The structural n.</value>
-        /// <exception cref="System.Exception">Cannot set StructuralN in CompositeBiomass</exception>
-        [JsonIgnore]
-        [Units("g/m^2")]
-        override public double StructuralN
-        {
-            get { Update(); return base.StructuralN; }
-            set { throw new Exception("Cannot set StructuralN in CompositeBiomass"); }
-        }
-
-        /// <summary>Gets or sets the non structural wt.</summary>
-        /// <value>The non structural wt.</value>
-        /// <exception cref="System.Exception">Cannot set StorageWt in CompositeBiomass</exception>
-        [JsonIgnore]
-        [Units("g/m^2")]
-        override public double StorageWt
-        {
-            get { Update(); return base.StorageWt; }
-            set { throw new Exception("Cannot set StorageWt in CompositeBiomass"); }
-        }
-
-        /// <summary>Gets or sets the structural wt.</summary>
-        /// <value>The structural wt.</value>
-        /// <exception cref="System.Exception">Cannot set StructuralWt in CompositeBiomass</exception>
-        [JsonIgnore]
-        [Units("g/m^2")]
-        override public double StructuralWt
-        {
-            get { Update(); return base.StructuralWt; }
-            set { throw new Exception("Cannot set StructuralWt in CompositeBiomass"); }
-        }
-
-        /// <summary>Gets or sets the metabolic n.</summary>
-        /// <value>The metabolic n.</value>
-        /// <exception cref="System.Exception">Cannot set MetabolicN in CompositeBiomass</exception>
-        [JsonIgnore]
-        [Units("g/m^2")]
-        override public double MetabolicN
-        {
-            get { Update(); return base.MetabolicN; }
-            set { throw new Exception("Cannot set MetabolicN in CompositeBiomass"); }
-        }
-
-        /// <summary>Gets or sets the metabolic wt.</summary>
-        /// <value>The metabolic wt.</value>
-        /// <exception cref="System.Exception">Cannot set MetabolicWt in CompositeBiomass</exception>
-        [JsonIgnore]
-        [Units("g/m^2")]
-        override public double MetabolicWt
-        {
-            get { Update(); return base.MetabolicWt; }
-            set { throw new Exception("Cannot set MetabolicWt in CompositeBiomass"); }
-        }
-
-        /// <summary>Gets the wt.</summary>
-        /// <value>The wt.</value>
-        [Units("g/m^2")]
-        override public double Wt
-        {
-            get
+            organs = new List<IOrganDamage>();
+            var parentPlant = Parent as Plant;
+            if (parentPlant == null)
+                throw new Exception("CompositeBiomass can only be dropped on a plant.");
+            foreach (var organName in OrganNames)
             {
-                Update();
-                return _StructuralWt + _StorageWt + _MetabolicWt;
+                var organ = parentPlant.Organs.FirstOrDefault(o => o.Name == organName);
+                if (organ == null && !(organ is IOrganDamage))
+                    throw new Exception($"In {Name}, cannot find a plant organ called {organName}");
+                organs.Add(organ as IOrganDamage);
             }
         }
 
-
-        /// <summary>Gets the n.</summary>
-        /// <value>The n.</value>
+        /// <summary>Gets the mass.</summary>
         [Units("g/m^2")]
-        override public double N
+        public double Wt
         {
             get
             {
-                Update();
-                return _StructuralN + _StorageN + _MetabolicN;
+                double wt = 0;
+                foreach (var organ in organs)
+                {
+                    if (IncludeLive)
+                        wt += organ.Live.Wt;
+                    if (IncludeDead)
+                        wt += organ.Dead.Wt;
+                }
+
+                return wt;
             }
         }
+
+        /// <summary>Gets the nitrogen content.</summary>
+        [Units("g/m^2")]
+        public double N
+        {
+            get 
+            {
+                double n = 0;
+                foreach (var organ in organs)
+                {
+                    if (IncludeLive)
+                        n += organ.Live.StorageN;
+                    if (IncludeDead)
+                        n += organ.Dead.StorageN;
+                }
+
+                return n; 
+            }
+        }
+
+        /// <summary>Gets the nitrogen concentration.</summary>
+        [Units("g/g")]
+        public double NConc
+        {
+            get
+            {
+                if (Wt > 0)
+                    return N / Wt;
+                else
+                    return 0.0;
+            }
+        }
+
 
         /// <summary>Writes documentation for this function by adding to the list of documentation tags.</summary>
         /// <param name="tags">The list of tags to add to.</param>
@@ -173,9 +121,9 @@ namespace Models.PMF
                 tags.Add(new AutoDocumentation.Paragraph(this.Name + " summarises the following biomass objects:", indent));
 
                 string st = string.Empty;
-                if (Propertys != null)
-                    foreach (string PropertyName in Propertys)
-                        st += Environment.NewLine + "* " + PropertyName;
+                if (OrganNames != null)
+                    foreach (string organName in OrganNames)
+                        st += Environment.NewLine + "* " + organName;
                 tags.Add(new AutoDocumentation.Paragraph(st, indent));
             }
         }
