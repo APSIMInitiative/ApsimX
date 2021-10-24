de4d712e6a815ca8f67cc5ce6a0e9662077a2a8d
Drew Holzworth <drew.holzworth@csiro.au>
2021-10-25 09:04:07 +1000

    Add missing files from previous commit.

--- ApsimNG/Presenters/MainPresenter.cs
+++ ApsimNG/Presenters/MainPresenter.cs
@@ -1150,29 +1150,6 @@
         }
 
         /// <summary>
-        /// Toggles between the default and dark themes.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnToggleTheme(object sender, EventArgs e)
-        {
-            try
-            {
-                Configuration.Settings.DarkTheme = !Configuration.Settings.DarkTheme;
-                view.ToggleTheme(sender, e);
-                // Might be better to restart automatically (after asking the user),
-                // but I haven't been able to figure out a reliable cross-platform 
-                // way of attaching the debugger to the new process. I leave this
-                // as an exercise to the reader.
-                view.ShowMsgDialog("Theme changes will be applied upon restarting Apsim.", "Theme Changes", Gtk.MessageType.Info, Gtk.ButtonsType.Ok);
-            }
-            catch (Exception err)
-            {
-                ShowError(err);
-            }
-        }
-
-        /// <summary>
         /// Opens the ApsimX online documentation.
         /// </summary>
         /// <param name="sender">Sender object.</param>
