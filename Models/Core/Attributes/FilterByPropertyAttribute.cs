3a6db7b5f2089d7c6bf97122d83e02b24e2a89ae
lie112 <lie112@csiro.au>
2021-11-25 09:15:11 +0930

    CLEM rename and move namespace filter attribute for filterable properties

--- Models/Core/Attributes/FilterByPropertyAttribute.cs
+++ Models/Core/Attributes/FilterByPropertyAttribute.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Models.Core
+namespace Models.CLEM
 {
     /// <summary>
     /// Identifies whether a Property or Method of an IFilterable model can be used for filtering
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
-    public class FilterAttribute : System.Attribute
+    public class FilterByPropertyAttribute : System.Attribute
     {
 
     }
