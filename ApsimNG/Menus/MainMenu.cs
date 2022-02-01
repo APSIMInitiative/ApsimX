12ea11e22f5bff2d9be12c209b2aab2ab4e97f83
Drew Holzworth <drew.holzworth@csiro.au>
2022-02-01 11:50:55 +1000

    Global hotkey to run file with f5 from anywhere

--- ApsimNG/Menus/MainMenu.cs
+++ ApsimNG/Menus/MainMenu.cs
@@ -1,9 +1,14 @@
 ﻿namespace UserInterface.Presenters
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Linq;
+    using APSIM.Shared.JobRunning;
     using APSIM.Shared.Utilities;
+    using global::UserInterface.Commands;
     using Models.Core;
+    using Models.Core.Run;
 
     /// <summary>
     /// This class contains methods for all main menu items that the ExplorerView exposes to the user.
@@ -14,6 +19,12 @@
         /// Reference to the main presenter.
         /// </summary>
         private MainPresenter presenter;
+
+        /// <summary>
+        /// The current run command. When this is null, the "run" menu item is
+        /// disabled.
+        /// </summary>
+        private RunCommand command;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MainMenu" /> class.
@@ -156,6 +167,52 @@
             {
                 presenter.ShowError(err);
             }
+        }
+
+        /// <summary>
+        /// User has clicked on Help
+        /// </summary>
+        /// <param name="sender">Sender of the event</param>
+        /// <param name="e">Event arguments</param>
+        [MainMenu("Run", "F5")]
+        public void OnRun(object sender, EventArgs e)
+        {
+            try
+            {
+                // If we're already running some simulations, don't do anything.
+                if (command != null && command.IsRunning)
+                    return;
+
+                // If no explorer presenter has focus, don't do anything.
+                ExplorerPresenter explorer = presenter.GetCurrentExplorerPresenter();
+                if (explorer == null)
+                    return;
+
+                IModel model = FindRunnable(explorer.CurrentNode);
+                if (model == null)
+                    throw new InvalidOperationException("Unable to find a model which may be run.");
+
+                Runner runner = new Runner(model, runType: Runner.RunTypeEnum.MultiThreaded, wait: false);
+                command = new RunCommand(model.Name, runner, explorer);
+                command.Do();
+            }
+            catch (Exception err)
+            {
+                presenter.ShowError(err);
+            }
+        }
+
+        private IModel FindRunnable(IModel currentNode)
+        {
+            IEnumerable<ISimulationDescriptionGenerator> runnableModels = currentNode.FindAllAncestors<ISimulationDescriptionGenerator>();
+            if (currentNode is ISimulationDescriptionGenerator runnable)
+                runnableModels = runnableModels.Prepend(runnable);
+            if (runnableModels.Any())
+                return runnableModels.LastOrDefault() as IModel;
+            Simulations topLevel = currentNode as Simulations;
+            if (topLevel != null)
+                return topLevel;
+            return currentNode.FindAncestor<Simulations>();
         }
     }
 }
