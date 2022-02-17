31727699b266af61c45074661181169d00ce9531
lie112 <lie112@csiro.au>
2022-02-17 17:48:58 +0930

    CLEM move hidden class

--- Models/CLEM/Activities/RuminantActivityManage.cs
+++ Models/CLEM/Activities/RuminantActivityManage.cs
@@ -1632,31 +1632,4 @@ namespace Models.CLEM.Activities
         #endregion
 
     }
-
-    /// <summary>
-    /// An individual purchase details provided by list of SpecifyRuminant components 
-    /// </summary>
-    public class SpecifiedRuminantListItem
-    {
-        /// <summary>
-        /// List index
-        /// </summary>
-        public int Index { get; set; }
-        /// <summary>
-        /// Ruminant example
-        /// </summary>
-        public Ruminant ExampleRuminant { get; set; }
-        ///// <summary>
-        ///// Label for ID
-        ///// </summary>
-        //public string Label { get; set; }
-        /// <summary>
-        /// The specify ruminant component 
-        /// </summary>
-        public SpecifyRuminant SpecifyRuminantComponent { get; set; }
-        /// <summary>
-        /// Cummulative probability of this one being picked
-        /// </summary>
-        public double CummulativeProbability { get; set; }
-    }
 }
