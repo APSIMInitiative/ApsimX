e02a56294d7b6948abb7066837235e78599e8b93
Dean Holzworth <Dean.Holzworth@csiro.au>
2022-03-09 10:57:54 +1000

    Renamed nutrient TotalN property to TotalOrganicN. Created a new TotalN that includes the N solutes. Updated nutrient validation to reflect this change

--- Models/Soils/Nutrients/Nutrient.cs
+++ Models/Soils/Nutrients/Nutrient.cs
@@ -367,10 +367,10 @@ namespace Models.Soils.Nutrients
         }
 
         /// <summary>
-        /// Total N in each soil layer, organic and mineral
+        /// Total organic N in each soil layer, organic and mineral
         /// </summary>
         [Units("kg/ha")]
-        public double[] TotalN
+        public double[] TotalOrganicN
         {
             get
             {
@@ -381,17 +381,30 @@ namespace Models.Soils.Nutrients
                     numLayers = FOMLignin.N.Length;
                 double[] values = new double[numLayers];
                 IEnumerable<NutrientPool> Pools = FindAllChildren<NutrientPool>();
-                IEnumerable<Solute> Solutes = FindAllChildren<Solute>();
 
                 foreach (NutrientPool P in Pools)
                     for (int i = 0; i < numLayers; i++)
                         values[i] += P.N[i];
-
-                foreach (Solute S in Solutes)
-                    for (int i = 0; i < numLayers; i++)
-                        values[i] += S.kgha[i];
-
                 return values;
+            }
+        }
+
+        /// <summary>
+        /// Total N in each soil layer, organic, mineral and nitrogen solutes
+        /// </summary>
+        [Units("kg/ha")]
+        public double[] TotalN
+        {
+            get
+            {
+                double[] totalN = TotalOrganicN;
+
+                // I don't like having hard coded solutes here but I can't think of another
+                // way to do this easily.
+                for (int i = 0; i < totalN.Length; i++)
+                    totalN[i] += Urea.kgha[i] + NO3.kgha[i] + NH4.kgha[i];
+                
+                return totalN;
             }
         }
 
