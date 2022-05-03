80680bd2b6930b3e0ed23a4e2154a3f9c0ca4403
Drew Holzworth <drew.holzworth@csiro.au>
2022-05-04 09:06:24 +1000

    Ensure that un-resolvable links result in a fatal error.

--- Models/Core/Simulation.cs
+++ Models/Core/Simulation.cs
@@ -259,7 +259,7 @@ namespace Models.Core
                 events.ConnectEvents();
 
                 // Resolve all links
-                links.Resolve(this, true);
+                links.Resolve(this, true, throwOnFail: true);
 
                 events.Publish("SubscribeToEvents", new object[] { this, EventArgs.Empty });
             }
