a5ec078280c077783111e75913cc5a562287e945
lie112 <lie112@csiro.au>
2022-05-03 17:52:00 +0930

    CLEM switch to remove inherent herd order before filtering

--- Models/CLEM/Groupings/FilterGroup.cs
+++ Models/CLEM/Groupings/FilterGroup.cs
@@ -22,6 +22,11 @@ namespace Models.CLEM
         [NonSerialized]
         private protected IEnumerable<ISort> sortList = null;
 
+        /// <inheritdoc/>
+        [Description("Remove inherent order before sorting")]
+        [Core.Display(Order = 7000)]
+        public bool RandomiseBeforeSorting { get; set; }
+
         /// <summary>
         /// The properties available for filtering
         /// </summary>
@@ -119,7 +124,7 @@ namespace Models.CLEM
 
             if(sortList?.Any()??false)
                 // add sorting and take specified
-                filtered = filtered.Sort(sortList);
+                filtered = filtered.Sort(sortList, RandomiseBeforeSorting);
 
             // do all takes and skips
             foreach (var take in FindAllChildren<TakeFromFiltered>())
