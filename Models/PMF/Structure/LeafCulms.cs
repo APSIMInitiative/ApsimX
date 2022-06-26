80ebc58b1b09e9e88e07f80fac56afe97e55e42d
Jason Brider <jason.brider@gmail.com>
2022-06-26 13:49:54 +1000

    Fixing up setting tillering methods

--- Models/PMF/Structure/LeafCulms.cs
+++ Models/PMF/Structure/LeafCulms.cs
@@ -25,8 +25,12 @@ namespace Models.PMF.Struct
 	[PresenterName("UserInterface.Presenters.PropertyPresenter")]
 	public class LeafCulms : Model
 	{
+        /// <summary>The plant</summary>
+        [Link]
+        private SorghumLeaf leaf = null; //todo change back to private
+        
 		/// <summary> Tillering Method that uses a fixed number of tillers</summary>
-		[Link(Type = LinkType.Child, ByName = true)]
+        [Link(Type = LinkType.Child, ByName = true)]
 		private ITilleringMethod fixedTillering = null;
 
 		/// <summary> Tillering Method that manages number of tillers dynamically</summary>
@@ -60,10 +64,15 @@ namespace Models.PMF.Struct
 		[Link(Type = LinkType.Child, ByName = true)]
 		private IFunction leafNoAtEmergence = null;
 
-		/// <summary> Enables Tillering method to be controlled via script</summary>
-		public double TilleringMethod { get; set; }
+        /// <summary> Set through Leaf</summary>
+        [JsonIgnore]
+        public int TilleringMethod { get => leaf.TilleringMethod; } 
 		
 		private ITilleringMethod tillering => TilleringMethod == 0 ? fixedTillering : dynamicTillering;
+
+        /// <summary> FertileTillerNumber is determined by the tillering method chosen</summary>
+        [JsonIgnore]
+        public double FertileTillerNumber { get => tillering.FertileTillerNumber; }
 
 		/// <summary> Subsequent tillers are slightly smaller - adjust that size using a percentage</summary>
 		[Link(Type = LinkType.Child, ByName = true)]
@@ -92,7 +101,7 @@ namespace Models.PMF.Struct
 		public List<Culm> Culms;
 
 		/// <summary>Total TT required to get from emergence to floral init.</summary>
-		[JsonIgnore]
+        [JsonIgnore]
 		public double TTTargetFI { get; private set; }
 
 		/// <summary> Constructor. </summary>
