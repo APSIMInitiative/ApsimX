666c3eff20b591bb8ac18dca3f0597f4746853b1
Hamish <hamish.brown@plantandfoodco.nz>
2022-08-24 17:54:16 +1200

    Adding files accidentally deleted

---
+++ Models/PMF/Biomass/BiomassDemand.cs
@@ -0,0 +1,43 @@
+﻿using APSIM.Shared.Documentation;
+using Models.Core;
+using Models.Functions;
+using Models.PMF.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Models.PMF
+{
+    /// <summary>
+    /// This class holds the functions for calculating the absolute demands for each biomass fraction. 
+    /// </summary>
+    [Serializable]
+    [ValidParent(ParentType = typeof(IOrgan))]
+    public class BiomassDemand : Model
+    {
+        /// <summary>The demand for the structural fraction.</summary>
+        [Link(Type = LinkType.Child, ByName = true)]
+        [Units("g/m2")]
+        public IFunction Structural = null;
+
+        /// <summary>The demand for the metabolic fraction.</summary>
+        [Link(Type = LinkType.Child, ByName = true)]
+        [Units("g/m2")]
+        public IFunction Metabolic = null;
+
+        /// <summary>The demand for the storage fraction.</summary>
+        [Link(Type = LinkType.Child, ByName = true)]
+        [Units("g/m2")]
+        public IFunction Storage = null;
+
+        /// <summary>Document the model.</summary>
+        public override IEnumerable<ITag> Document()
+        {
+            // Write description of this class from summary and remarks XML documentation.
+            foreach (var tag in GetModelDescription())
+                yield return tag;
+
+            foreach (var tag in DocumentChildren<IModel>())
+                yield return tag;
+        }
+    }
+}
