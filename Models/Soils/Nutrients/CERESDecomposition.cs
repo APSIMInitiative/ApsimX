4a1637dad58db4f7d3ecb7d6179b6495a631e042
Dean Holzworth <Dean.Holzworth@csiro.au>
2022-11-17 13:58:20 +1000

    Fix nutrient json and add potential rate to CERESDecomposition class

--- Models/Soils/Nutrients/CERESDecomposition.cs
+++ Models/Soils/Nutrients/CERESDecomposition.cs
@@ -7,10 +7,7 @@ namespace Models.Functions
     [Serializable]
     public class CERESDecomposition : Model, IFunction
     {
-        /// <summary>The child functions</summary>
-        private double potentialRate = 0.0095;
-
-        [Link(ByName = true, Type = LinkType.Child)]
+       [Link(ByName = true, Type = LinkType.Child)]
         private IFunction TF = null;
         
         [Link(ByName = true)]
@@ -19,10 +16,14 @@ namespace Models.Functions
         [Link(ByName = true, Type = LinkType.Child)]
         private IFunction CNRF = null;
 
+        /// <summary>The potential rate of decomposition</summary>
+        [Description("The potential rate of decomposition")]
+        public double PotentialRate { get; set; } = 0.0095;
+
         /// <summary>Gets the value.</summary>
         public double Value(int arrayIndex = -1)
         {
-            return potentialRate * TF.Value(arrayIndex) * WF.Value(arrayIndex) * CNRF.Value(arrayIndex);
+            return PotentialRate * TF.Value(arrayIndex) * WF.Value(arrayIndex) * CNRF.Value(arrayIndex);
         }
     }
 }
