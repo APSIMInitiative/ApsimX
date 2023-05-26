fffb34e6a01212e88bba6ee79e447c6b14f0a873
Dean Holzworth <Dean.Holzworth@csiro.au>
2023-05-26 11:32:37 +1000

    Improved emergence detection in Phenology

--- Models/PMF/Phenology/Phases/DAWSPhase.cs
+++ Models/PMF/Phenology/Phases/DAWSPhase.cs
@@ -1,10 +1,6 @@
 using System;
 using Models.Core;
-using Models.PMF.Organs;
 using Newtonsoft.Json;
-using Models.PMF.Struct;
-using System.IO;
-using Models.Functions;
 using Models.Climate;
 
 namespace Models.PMF.Phen
@@ -43,6 +39,10 @@ namespace Models.PMF.Phen
         /// <summary>The end</summary>
         [Description("End")]
         public string End { get; set; }
+
+        /// <summary>Is the phase emerged from the ground?</summary>
+        [Description("Is the phase emerged?")]
+        public bool IsEmerged { get; set; } = true;
 
         /// <summary>Days after winter solstice to progress from phase</summary>
         [Description("DAWStoProgress")]
--- Models/PMF/Phenology/Phases/EmergingPhase.cs
+++ Models/PMF/Phenology/Phases/EmergingPhase.cs
@@ -47,6 +47,10 @@ namespace Models.PMF.Phen
         [Models.Core.Description("End")]
         public string End { get; set; }
 
+        /// <summary>Is the phase emerged from the ground?</summary>
+        [Description("Is the phase emerged?")]
+        public bool IsEmerged { get; set; } = true;
+
         /// <summary>Fraction of phase that is complete (0-1).</summary>
         [JsonIgnore]
         public double FractionComplete
