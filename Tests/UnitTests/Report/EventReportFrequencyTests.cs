af427d20a42959bb746d2cef12997e1aea8fef83
Andrew Paroz <par456@csiro.au>
2023-06-05 09:56:58 +1000

    Updates to Event Report Frequency Unit tests

--- Tests/UnitTests/Report/EventReportFrequencyTests.cs
+++ Tests/UnitTests/Report/EventReportFrequencyTests.cs
@@ -1,4 +1,5 @@
 using APSIM.Shared.Utilities;
+using DocumentFormat.OpenXml.Bibliography;
 using Models;
 using Models.Core;
 using Moq;
@@ -28,14 +29,41 @@ namespace UnitTests.Reporting
         }
 
         /// <summary>
+        /// Ensure that the given reporting frequency date is a valid date to parse
+        /// </summary>
+        /// <param name="line">Input line - should be any valid reporting frequency using a model event.</param>
+        [TestCase("1-May")]
+        public void TestReportingDate(string line)
+        {
+            Assert.True(DateReportFrequency.TryParse(line, new Models.Report(), mockEvents.Object));
+        }
+
+        /// <summary>
         /// Ensure that the given reporting frequency is a valid event reporting frequency.
         /// </summary>
-        /// <param name="frequency">Input line - should be any valid reporting frequency using a model event.</param>
+        /// <param name="line">Input line - should be any valid reporting frequency using a model event.</param>
         [TestCase("[Clock].DoReport")]
         [TestCase("[Clock with space].DoReport")]
-        public void TestReportingFrequency(string frequency)
+        public void TestReportingFrequency(string line)
         {
-            Assert.True(EventReportFrequency.TryParse(frequency, new Models.Report(), mockEvents.Object));
+            //this is the order lines are parsed by Report.cs
+            Assert.False(DateReportFrequency.TryParse(line, new Models.Report(), mockEvents.Object));
+            Assert.True(EventReportFrequency.TryParse(line, new Models.Report(), mockEvents.Object));
+        }
+
+        /// <summary>
+        /// Ensure that the given reporting frequency expression is a valid expression to compile and
+        /// does not get parsed by an earlier method
+        /// </summary>
+        /// <param name="line">Input line - should be any valid reporting frequency using a model event.</param>
+        [TestCase("[Clock].Month == 1 && [Clock].Day == 1")]
+        public void TestReportingExpression(string line)
+        {
+            ScriptCompiler compiler = new ScriptCompiler();
+            //this is the order lines are parsed by Report.cs
+            Assert.False(DateReportFrequency.TryParse(line, new Models.Report(), mockEvents.Object));
+            Assert.False(EventReportFrequency.TryParse(line, new Models.Report(), mockEvents.Object));
+            Assert.True(ExpressionReportFrequency.TryParse(line, new Models.Report(), mockEvents.Object, compiler));
         }
     }
 }
