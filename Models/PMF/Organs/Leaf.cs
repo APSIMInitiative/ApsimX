77d7d3ea624ac3db3df79c0e7c73474524803e01
Dean Holzworth <Dean.Holzworth@csiro.au>
2023-06-05 14:06:36 +1000

    Remove FRGR property from Leaf

--- Models/PMF/Organs/Leaf.cs
+++ Models/PMF/Organs/Leaf.cs
@@ -106,7 +106,7 @@ namespace Models.PMF.Organs
         {
             get
             {
-                return Gsmax350 * FRGR * StomatalConductanceCO2Modifier.Value();
+                return Gsmax350 * FRGRFunction.Value() * StomatalConductanceCO2Modifier.Value();
             }
         }
 
@@ -183,11 +183,6 @@ namespace Models.PMF.Organs
 
         /// <summary>Gets the width of the canopy (mm).</summary>
         public double Width { get; set; }
-
-        /// <summary>Gets  FRGR.</summary>
-        [Description("Relative growth rate for calculating stomata conductance which fed the Penman-Monteith function")]
-        [Units("0-1")]
-        public double FRGR { get; set; }
 
         private double _PotentialEP;
         /// <summary>Sets the potential evapotranspiration. Set by MICROCLIMATE.</summary>
@@ -1286,7 +1281,6 @@ namespace Models.PMF.Organs
                     FractionNextleafExpanded = (L.FractionExpanded - StartFractionExpanded) / (1 - StartFractionExpanded);
                 }
             }
-            FRGR = FRGRFunction.Value();
         }
 
         /// <summary>Clears this instance.</summary>
@@ -1317,7 +1311,6 @@ namespace Models.PMF.Organs
             CohortParameters.CellDivisionStressValue = 0;
             CohortParameters.LagAccelerationValue = 0;
             CohortParameters.SenescenceAccelerationValue = 0;
-            FRGR = 0;
             FractionDied = 0;
         }
         /// <summary>Initialises the cohorts.</summary>
