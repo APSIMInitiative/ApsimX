442f391202e34d329cebbfe2eae2af39faefef3e
Neil Huth <neil.huth@csiro.au>
2023-06-30 10:26:17 +1000

    fix unit test

--- Tests/UnitTests/Soils/MockNutrientPool.cs
+++ Tests/UnitTests/Soils/MockNutrientPool.cs
@@ -12,12 +12,25 @@
         public double[] LayerFraction { get; set; }
         public double[] N { get; set; }
 
+        double[] INutrientPool.C => throw new System.NotImplementedException();
+
+        double[] INutrientPool.N => throw new System.NotImplementedException();
+
+        double[] INutrientPool.P => throw new System.NotImplementedException();
+
+        double[] INutrientPool.LayerFraction => throw new System.NotImplementedException();
+
         public void Add(double[] CAdded, double[] NAdded)
         {
             throw new System.NotImplementedException();
         }
 
         public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void INutrientPool.Reset()
         {
             throw new System.NotImplementedException();
         }
