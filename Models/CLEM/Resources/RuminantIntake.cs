50f2271c0a7e35f58aba5dc8c8675ac3fd80a193
lie112 <lie112@csiro.au>
2023-09-11 18:07:32 +0930

    CLEM update ruminant intake component

--- Models/CLEM/Resources/RuminantIntake.cs
+++ Models/CLEM/Resources/RuminantIntake.cs
@@ -11,28 +11,23 @@ namespace Models.CLEM.Resources
     /// </summary>
     public  class RuminantIntake
     {
-        ///// <summary>
-        ///// Store of combined feed details for the timestep based on broad feed type
-        ///// </summary>
-        //public Dictionary<FeedType, FoodResourcePacket> FeedDetails { get; set; } = new Dictionary<FeedType, FoodResourcePacket>();
-
-        public FoodResourcePacket CombinedDetails { get; set; } = new FoodResourcePacket();
+        public FoodResourcePacket CombinedDetails { get; private set; } = new FoodResourcePacket();
 
         /// <summary>
         /// The potential and actual milk intake of the individual
         /// </summary>
-        public ExpectedActualContainer Milk { get; set; }
+        public ExpectedActualContainer Milk { get; private set; }
 
         /// <summary>
         /// The potential and actual feed intake of the individual
         /// </summary>
-        public ExpectedActualContainer Feed { get; set; }
+        public ExpectedActualContainer Feed { get; private set; }
 
         /// <summary>
         /// A function to add intake and track totals of N, CP, DMD, Fat and energy
         /// </summary>
         /// <param name="packet">Feed packet containing intake information kg, %N, DMD</param>
-        public void AddIntake(FoodResourcePacket packet)
+        public void AddFeed(FoodResourcePacket packet)
         {
             if (packet.Amount > 0)
             {
@@ -48,49 +43,26 @@ namespace Models.CLEM.Resources
                 // Track combined MEContent
                 MEContent = ((MEContent * Feed.Actual) + (packet.MEContent * packet.Amount)) / (Feed.Actual + packet.Amount);
                 // Track combined METotal
-                METotal += packet.MEContent * packet.Amount;
+                MEIntakeFeed += packet.MEContent * packet.Amount;
 
                 CombinedDetails.Amount += packet.Amount;
                 Feed.Actual = CombinedDetails.Amount;
             }
         }
 
-        ///// <summary>
-        ///// A function to add intake and track totals of N, CP, DMD, Fat and energy
-        ///// </summary>
-        ///// <param name="packet">Feed packet containing intake information kg, %N, DMD</param>
-        //public void AddIntake(FoodResourcePacket packet)
-        //{
-        //    if (packet.Amount > 0)
-        //    {
-        //        // does feed pool exist for the intake type
-        //        FoodResourcePacket feedDetail;
-        //        if (!FeedDetails.ContainsKey(packet.TypeOfFeed))
-        //        {
-        //            // create new store.
-        //            feedDetail = new FoodResourcePacket() { TypeOfFeed = packet.TypeOfFeed };
-        //            FeedDetails[packet.TypeOfFeed] = feedDetail;
-        //        }
-        //        else
-        //        {
-        //            feedDetail = FeedDetails[packet.TypeOfFeed];
-        //        }
-        //        feedDetail.DryMatterDigestibility = ((feedDetail.DryMatterDigestibility * feedDetail.Amount) + (packet.DryMatterDigestibility * packet.Amount)) / (feedDetail.Amount + packet.Amount);
-        //        feedDetail.FatContent = ((feedDetail.FatContent * feedDetail.Amount) + (packet.FatContent * packet.Amount)) / (feedDetail.Amount + packet.Amount);
-        //        feedDetail.EnergyContent = ((feedDetail.EnergyContent * feedDetail.Amount) + (packet.EnergyContent * packet.Amount)) / (feedDetail.Amount + packet.Amount);
-        //        feedDetail.NitrogenContent = ((feedDetail.NitrogenContent * feedDetail.Amount) + (packet.NitrogenContent * packet.Amount)) / (feedDetail.Amount + packet.Amount);
-        //        feedDetail.CPDegradability = ((feedDetail.CPDegradability * feedDetail.Amount) + (packet.CPDegradability * packet.Amount)) / (feedDetail.Amount + packet.Amount);
-        //        feedDetail.Amount += packet.Amount;
-
-        //        // Track combined MEContent
-        //        MEContent = ((MEContent * Feed.Actual) + (packet.MEContent * packet.Amount)) / (Feed.Actual + packet.Amount);
-        //        // Track combined METotal
-        //        METotal = ((METotal * Feed.Actual) + (packet.EnergyContent * packet.Amount)) / (Feed.Actual + packet.Amount);
-
-        //        Feed.Actual += packet.Amount;
-        //    }
-        //}
-
+        /// <summary>
+        /// A function to add milk intake and track energy from milk
+        /// </summary>
+        /// <param name="amount">Amount consumed (L) in timestep</param>
+        /// <param name="energyContent">Energy content of milk consumed</param>
+        public void AddMilk(double amount, double energyContent)
+        {
+            if (amount > 0)
+            {
+                Milk.Actual += amount;
+                MEIntakeMilk += energyContent * amount;
+            }
+        }
 
         /// <summary>
         /// Reset all intake values
@@ -98,48 +70,31 @@ namespace Models.CLEM.Resources
         public void Reset()
         {
             MEContent = 0;
-            METotal = 0;
-            MEAvailableForGain = 0;
+            MEIntakeFeed = 0;
+            MEIntakeMilk = 0;
             Feed.Reset();
             Milk.Reset();
             CombinedDetails.Reset();
-            //foreach (var detail in FeedDetails)
-            //{
-            //    detail.Value.DryMatterDigestibility = 0;
-            //    detail.Value.FatContent = 0;
-            //    detail.Value.EnergyContent = 0;
-            //    detail.Value.NitrogenContent = 0;
-            //    detail.Value.CPDegradability = 0;
-            //    detail.Value.Amount = 0;
-            //}
         }
-
-        /// <summary>
-        /// Dictionary of all 
-        /// </summary>
-        public Dictionary<string, ExpectedActualContainer> Energy { get; set; } = new Dictionary<string, ExpectedActualContainer>()
-        {
-            { "Intake", new ExpectedActualContainer() },
-            { "Maintenance", new ExpectedActualContainer() },
-            { "Wool", new ExpectedActualContainer() },
-            { "Lactation", new ExpectedActualContainer() },
-            { "Pregnancy", new ExpectedActualContainer() },
-            { "Growth", new ExpectedActualContainer() }
-        };
-
-        /// <summary>
-        /// Metabolisable Energy available for gain
-        /// </summary>
-        public double MEAvailableForGain { get; set; }
 
         /// <summary>
         /// Metabolisable Energy content from combined intake
         /// </summary>
-        public double MEContent { get; set; }
+        public double MEContent { get; private set; }
 
         /// <summary>
-        /// Total Metabolisable Energy from combined intake
+        /// Total Metabolisable Energy from combined feed intake
         /// </summary>
-        public double METotal { get; set; }
+        public double MEIntakeFeed { get; private set; }
+
+        /// <summary>
+        /// Total Metabolisable Energy from milk intake
+        /// </summary>
+        public double MEIntakeMilk { get; private set; }
+
+        /// <summary>
+        /// Total Metabolisable Energy from combined milk and feed intake
+        /// </summary>
+        public double MEIntakeTotal { get { return MEIntakeFeed + MEIntakeMilk; } }
     }
 }
