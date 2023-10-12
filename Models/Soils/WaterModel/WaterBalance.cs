f3895b3a6633245d8ff02772445296888ac65899
Val Snow <val.snow@agresearch.co.nz>
2023-10-13 09:45:53 +1300

    Enabled ()hopefully!) chloride to be moved in SoilWater. Works with the Hudson simulation.

--- Models/Soils/WaterModel/WaterBalance.cs
+++ Models/Soils/WaterModel/WaterBalance.cs
@@ -96,7 +96,7 @@ namespace Models.WaterModel
         [Link(ByName = true)]
         ISolute urea = null;
 
-        [Link(ByName = true, IsOptional = true)]
+        [Link(ByName = true, IsOptional = true)]  
         ISolute cl = null;
 
         /// <summary>Irrigation information.</summary>
@@ -414,9 +414,9 @@ namespace Models.WaterModel
         [JsonIgnore]
         public double LeachUrea { get { if (FlowUrea == null) return 0; else return FlowUrea.Last(); } }
 
-        /// <summary>Amount of Cl leaching from the deepest soil layer (kg /ha). Note that SoilWater does not currently handle chlorid at all!</summary>
+        /// <summary>Amount of Cl leaching from the deepest soil layer (kg /ha)</summary>
         [JsonIgnore]
-        public double LeachCl => 0.0;
+        public double LeachCl { get { if (FlowCl == null) return 0; else return FlowCl.Last(); } }  // VOS //public double LeachCl => 0.0;
 
         /// <summary>Amount of N leaching as NO3 from each soil layer (kg /ha)</summary>
         [JsonIgnore]
@@ -429,6 +429,11 @@ namespace Models.WaterModel
         /// <summary>Amount of N leaching as urea from each soil layer (kg /ha)</summary>
         [JsonIgnore]
         public double[] FlowUrea { get; private set; }
+
+        /// <summary>Amount of Cl leaching as Cl from each soil layer (kg /ha)</summary>
+        [JsonIgnore]
+        public double[] FlowCl { get; private set; }
+
 
         /// <summary> This is set by Microclimate and is rainfall less that intercepted by the canopy and residue components </summary>
         [JsonIgnore]
@@ -548,12 +553,15 @@ namespace Models.WaterModel
 
             double[] no3Values = no3.kgha;
             double[] ureaValues = urea.kgha;
+            double[] clValues = cl.kgha;
 
-            // Calcualte solute movement down with water.
+            // Calculate solute movement down with water.
             double[] no3Down = CalculateSoluteMovementDown(no3Values, Water, Flux, SoluteFluxEfficiency);
             MoveDown(no3Values, no3Down);
             double[] ureaDown = CalculateSoluteMovementDown(ureaValues, Water, Flux, SoluteFluxEfficiency);
             MoveDown(ureaValues, ureaDown);
+            double[] clDown = CalculateSoluteMovementDown(clValues, Water, Flux, SoluteFluxEfficiency);
+            MoveDown(clValues, clDown);
 
             // Calculate evaporation and remove from top layer.
             double es = evaporationModel.Calculate();
@@ -574,14 +582,18 @@ namespace Models.WaterModel
             MoveUp(no3Values, no3Up);
             double[] ureaUp = CalculateNetSoluteMovement(ureaValues, Water, Flow, SoluteFlowEfficiency);
             MoveUp(ureaValues, ureaUp);
+            double[] clUp = CalculateNetSoluteMovement(clValues, Water, Flow, SoluteFlowEfficiency);
+            MoveUp(clValues, clUp);
 
             // Update flow output variables.
             FlowNO3 = MathUtilities.Subtract(no3Down, no3Up);
             FlowUrea = MathUtilities.Subtract(ureaDown, ureaUp);
+            FlowCl = MathUtilities.Subtract(clDown, clUp);
 
             // Set solute state variables.
             no3.SetKgHa(SoluteSetterType.Soil, no3Values);
             urea.SetKgHa(SoluteSetterType.Soil, ureaValues);
+            cl.SetKgHa(SoluteSetterType.Soil, clValues);
 
             // Now that we've finished moving water, calculate volumetric water
             waterVolumetric = MathUtilities.Divide(Water, soilPhysical.Thickness);
