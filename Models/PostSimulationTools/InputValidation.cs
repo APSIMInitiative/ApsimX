2c505a02f69f38fb3566ab90218731a025b7220f
Andrew Paroz <par456@csiro.au>
2023-10-20 11:51:08 +1000

    Resturucting for more tests

--- Models/PostSimulationTools/InputValidation.cs
+++ Models/PostSimulationTools/InputValidation.cs
@@ -8,7 +8,6 @@ using Models.Interfaces;
 using Models.Storage;
 using Models.Utilities;
 using Newtonsoft.Json;
-using static Models.Core.ScriptCompiler;
 
 namespace Models.PostSimulationTools
 {
@@ -32,6 +31,10 @@ namespace Models.PostSimulationTools
             /// <summary></summary>
             public string TableName;
             /// <summary></summary>
+            public string SimulationName;
+            /// <summary></summary>
+            public string Rule;
+            /// <summary></summary>
             public string Message;
             /// <summary></summary>
             public bool Error;
@@ -39,9 +42,6 @@ namespace Models.PostSimulationTools
 
         [Link]
         private DataStore dataStore = null;
-
-        /// <summary></summary>
-        public List<ValidationResult> Successes { get; set; }
 
         /// <summary></summary>
         public List<ValidationResult> Errors { get; set; }
@@ -58,14 +58,9 @@ namespace Models.PostSimulationTools
                 columns = new List<GridTableColumn>();
                 columns.Add(new GridTableColumn("InputName", new VariableProperty(this, GetType().GetProperty("Errors"))));
                 columns.Add(new GridTableColumn("TableName", new VariableProperty(this, GetType().GetProperty("Errors"))));
-                columns.Add(new GridTableColumn("ColumnName", new VariableProperty(this, GetType().GetProperty("Successes"))));
-                columns.Add(new GridTableColumn("Message", new VariableProperty(this, GetType().GetProperty("Successes"))));
-                tables.Add(new GridTable("Successes", columns, this));
-
-                columns = new List<GridTableColumn>();
-                columns.Add(new GridTableColumn("InputName", new VariableProperty(this, GetType().GetProperty("Errors"))));
-                columns.Add(new GridTableColumn("TableName", new VariableProperty(this, GetType().GetProperty("Errors"))));
                 columns.Add(new GridTableColumn("ColumnName", new VariableProperty(this, GetType().GetProperty("Errors"))));
+                columns.Add(new GridTableColumn("SimulationName", new VariableProperty(this, GetType().GetProperty("Errors"))));
+                columns.Add(new GridTableColumn("Rule", new VariableProperty(this, GetType().GetProperty("Errors"))));
                 columns.Add(new GridTableColumn("Message", new VariableProperty(this, GetType().GetProperty("Errors"))));
                 tables.Add(new GridTable("Errors", columns, this));
 
@@ -92,13 +87,14 @@ namespace Models.PostSimulationTools
         /// <summary>Main run method for performing our calculations and storing data.</summary>
         public void Run()
         {
+            
+            Model model = dataStore as Model;
+            Simulations sims = model.FindAncestor<Simulations>();
+            List<ValidationResult> newErrors = new List<ValidationResult>();
+
+            List<ExcelInput> excelInputs = model.FindAllChildren<ExcelInput>().ToList();
             List<string> tableNames = new List<string>();
             List<string> inputNames = new List<string>();
-            Model model = dataStore as Model;
-            List<ExcelInput> excelInputs = model.FindAllChildren<ExcelInput>().ToList();
-
-            Simulations sims = model.FindAncestor<Simulations>();
-
             foreach (ExcelInput input in excelInputs)
             {
                 foreach (string name in input.SheetNames)
@@ -108,26 +104,112 @@ namespace Models.PostSimulationTools
                 }
             }
 
-            List<ValidationResult> newErrors = new List<ValidationResult>();
-            List<ValidationResult> newGood = new List<ValidationResult>();
+            List<ValidationResult> columnErrors = ValidateColumnNames(tableNames, inputNames, sims);
+            foreach(ValidationResult err in columnErrors)
+                newErrors.Add(err);
+
+            List<ValidationResult> totalErrors = ValidateLiveDeadEqualsTotal(tableNames, inputNames, sims);
+            foreach (ValidationResult err in totalErrors)
+                newErrors.Add(err);
+
+            Errors = newErrors;
+            return;
+        }
+
+        private List<ValidationResult> ValidateLiveDeadEqualsTotal(List<string> tableNames, List<string> inputNames, Simulations sims)
+        {
+            List<string> totalNames = new List<string>();
+
+            List<ValidationResult> errors = new List<ValidationResult>();
             for (int i = 0; i < tableNames.Count; i++)
             {
                 string tableName = tableNames[i];
-                string inputName = inputNames[i];
 
                 List<string> columnNames = dataStore.Reader.ColumnNames(tableName);
                 for (int j = 0; j < columnNames.Count; j++)
                 {
                     string columnName = columnNames[j];
+                    if (columnName.Contains("Total"))
+                    {
+                        string cleanedName = columnName.Trim();
+                        int end = cleanedName.IndexOf("Total");
+                        int start = cleanedName.LastIndexOf('.') + 1;
+                        cleanedName = cleanedName.Substring(start, end - start);
+                        totalNames.Add(cleanedName);
+
+                    }
+                }
+            }
+            return errors;
+
+        }
+
+        private List<ValidationResult> ValidateLiveDeadEqualsTotal(List<string> tableNames, List<string> inputNames, Simulations sims)
+        {
+            List<string> totalNames = new List<string>();
+
+            List<ValidationResult> errors = new List<ValidationResult>();
+            for (int i = 0; i < tableNames.Count; i++)
+            {
+                string tableName = tableNames[i];
+
+                List<string> columnNames = dataStore.Reader.ColumnNames(tableName);
+                for (int j = 0; j < columnNames.Count; j++)
+                {
+                    string columnName = columnNames[j];
+                    if (columnName.Contains("Total"))
+                    {
+                        string cleanedName = columnName.Trim();
+                        int end = cleanedName.IndexOf("Total");
+                        int start = cleanedName.LastIndexOf('.')+1;
+                        cleanedName = cleanedName.Substring(start, end-start);
+                        totalNames.Add(cleanedName);
+
+                    }
+                }
+            }
+            return errors;
+
+        }
+
+        private List<ValidationResult> ValidateColumnNames(List<string> tableNames, List<string> inputNames, Simulations sims)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+            for (int i = 0; i < tableNames.Count; i++)
+            {
+                string tableName = tableNames[i];
+                string inputName = inputNames[i];
+
+                List<string> columnNamesRead = new List<string>();
+
+                List<string> columnNames = dataStore.Reader.ColumnNames(tableName);
+                for (int j = 0; j < columnNames.Count; j++)
+                {
+                    string columnName = columnNames[j];
+                    if (columnNamesRead.Contains(columnName))
+                    {
+                        ValidationResult result = new ValidationResult();
+                        result.TableName = tableName;
+                        result.InputName = inputName;
+                        result.SimulationName = "All";
+                        result.Rule = "Column Exists Twice";
+                        result.Message = $"{columnName} is listed more than once in {inputName}:{tableName}";
+                        errors.Add(result);
+                    } 
+                    else
+                    {
+                        columnNamesRead.Add(columnName);
+                    }
+
                     if (NameIsAPSIMFormat(columnName))
                     {
                         ValidationResult result = NameMatchesAPSIMModel(columnName, sims);
                         result.TableName = tableName;
                         result.InputName = inputName;
+                        result.SimulationName = "All";
+                        result.Rule = "Column != APSIM Column";
                         if (result.Error)
-                            newErrors.Add(result);
-                        else
-                            newGood.Add(result);
+                            errors.Add(result);
                     }
                     else
                     {
@@ -135,15 +217,15 @@ namespace Models.PostSimulationTools
                         result.ColumnName = columnName;
                         result.TableName = tableName;
                         result.InputName = inputName;
+                        result.SimulationName = "All";
+                        result.Rule = "No . in Column";
                         result.Message = $"{columnName} is not considered an APSIM variable";
                         result.Error = false;
-                        newErrors.Add(result);
+                        errors.Add(result);
                     }
                 }
             }
-            Errors = newErrors;
-            Successes = newGood;
-            return;
+            return errors;
         }
 
         /// <summary></summary>
