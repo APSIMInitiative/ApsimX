e7abf8a485aba591a6842f2b1a81fd3884a83dd8
Morgan Masters <mwmaster@ucsc.edu>
2024-02-02 14:12:34 -0800

    test2_talkingTiles.py for deliverable

---
+++ Tests/Simulation/ZMQ-Sync/test2_talkingTiles.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+
+import zmq
+import msgpack
+
+import matplotlib.pyplot as plt
+
+
+RAIN_DAY = 90
+RAIN_LOC = 0
+RUNOFF_FLAGS = [False, False]
+
+
+def open_zmq2(ports=[27746]):
+    #  Socket to talk to server
+    ret = []
+    for port in ports:
+        context = zmq.Context.instance()
+        print("Connecting to server...")
+        print(f"Port {port}")
+        socket = context.socket(zmq.REP)
+        socket.bind(f"tcp://0.0.0.0:{port}")
+        print('    ...connected.')
+        ret.append([context, socket])
+    # print(context.closed)
+    # print(socket.closed)
+    return ret
+
+
+def close_zmq2(socket : zmq.Socket, port=27746):
+    socket.disconnect(f"tcp://127.0.0.1:{port}")
+    print('disconnected from server')
+
+
+def sendCommand(socket : zmq.Socket, command : str, args=None):
+    """Sends a string command and optional arguments to the server
+
+    The objects in args can be any serializable type. Internally uses
+    msgpack.packb for serialization.
+
+    Args
+    ----
+    socket: Opened ZMQ created socket
+    command: Command string
+    args: List of arguments
+    """
+
+    # quick and dirty way dealing with this
+    if args:
+        socket.send_string(command, flags=zmq.SNDMORE)
+    else:
+        socket.send_string(command)
+
+    # check for additional arguments
+    if args:
+
+        # list of data to send
+        msg = []
+
+        # loop over them
+        for arg in args:
+            # serialize using msgpack
+            ser = msgpack.packb(arg)
+            msg.append(ser)
+
+        socket.send_multipart(msg)
+
+
+def poll_zmq(sockets):
+    """Runs the simulation and obtains simulated data
+
+    Implements a response loop to control the simulation and transfer data. The
+    sequence of commands in the format (server -> client) are as follows:
+        connect     -> ok
+        paused      -> resume/get/set/do
+        finished    -> ok
+
+    Args
+    ----
+    sockets: list of opened ZMQ created sockets
+
+    Returns
+    -------
+    Returns a tuple of (timestamp, extractable soil water (mm), rain)
+    """
+
+    ts_arrs = []
+    esw_arrs = []
+    rain_arrs = []
+
+    counter = [0]*len(sockets)
+
+    while (True):
+        ts_arr = []
+        sw_arr = []
+        rain_arr = []
+        for i, ret in enumerate(sockets):
+            _, socket = ret
+            msg = socket.recv_string()
+            print(f"recv msg: {msg}")
+            if msg == "connect":
+                sendCommand(socket, "ok")
+            elif msg == "paused":
+                sendCommand(socket, "get", ["[Clock].Today"])
+                ts = msgpack.unpackb(socket.recv())
+                ts_arr.append(ts.to_unix())
+
+                sendCommand(socket, "get", ["sum([Soil].Water.Volumetric)"])
+                sw = msgpack.unpackb(socket.recv())
+                sw_arr.append(sw)
+
+                if counter[RAIN_LOC] == RAIN_DAY and i == RAIN_LOC:
+                    print("Our rainy day.")
+                    sendCommand(socket, "do", ["applyIrrigation", "amount", 204200.0])
+                    socket.recv()
+                    global rain_day_ts
+                    rain_day_ts = ts.to_unix()
+
+                sendCommand(socket, "get", ["[Weather].Rain"])
+                rain = msgpack.unpackb(socket.recv())
+                rain_arr.append(rain)
+
+                sendCommand(socket, "get", "[Soil].Water.Runoff")
+                runoff = msgpack.unpackb(socket.recv())  # unknown units on runoff
+                if runoff > 0:
+                    RUNOFF_FLAG[i] = True
+
+                if RUNOFF_FLAG[(i-1)**2]:
+                    sendCommand(socket, "do", ["applyIrrigation", "amount", runoff])
+                    socket.recv()
+                    RUNOFF_FLAG[(i-1)**2] = False
+
+                # resume simulation until next ReportEvent
+                sendCommand(socket, "resume")
+            elif msg == "finished":
+                sendCommand(socket, "ok")
+                break
+
+            counter[i] += 1
+        ts_arrs.append(ts_arr)
+        sw_arrs.append(sw_arr)
+        rain_arrs.append(rain_arr)
+
+    return (ts_arrs, esw_arrs, rain_arrs)
+
+
+if __name__ == '__main__':
+    # initialize connection
+    ports = [27746, 27747]
+    ret = open_zmq2(ports=ports)
+
+    ts_arr, esw_arr, rain_arr = poll_zmq(ret)
+
+    # Code for testing with echo server
+    #start_str = socket.recv_string()
+    #if start_str != "connect":
+    #    raise ValueError(f"Did not get correct start starting, got {start_str}, expected 'connect'")
+
+    #sendCommand(socket, "get", ["[Manager].Script.cumsumfert"])
+
+    #print('Do we get a reply?')
+    #reply = socket.recv_multipart()
+    #print(reply)
+
+    plt.figure()
+
+    plt.plot(ts_arr, esw_arr)
+    plt.xlabel("Time (Days)")
+    plt.ylabel("Volumetric Water Content")
+    plt.axvline(x=rain_day_ts, color='red', linestyle='--', label="Rain day")
+
+    plt.twinx()
+    plt.plot(ts_arr, rain_arr, color="orange")
+    plt.ylabel("Rain")
+
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    # make plot
+
+    # make a clean getaway
+    # close_zmq2(socket)
+    for context, socket in ret:
+        context.destroy()
