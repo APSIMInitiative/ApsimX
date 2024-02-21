2430a215dc3b62590f391256056dc79d511384ce
BrianCollinss <brcollins2020@gmail.com>
2024-02-21 14:04:13 +1000

    Minor changes in texts

--- Models/Functions/DemandFunctions/LAIncrementWithBranching.cs
+++ Models/Functions/DemandFunctions/LAIncrementWithBranching.cs
@@ -7,7 +7,7 @@ using Models.PMF.Phen;
 
 namespace Models.Functions.DemandFunctions
 {
-    /// <summary>Daily increment in leaf area is calculated from plant population, number of nodes on the main stem, number of branchers,
+    /// <summary>Daily increment in leaf area is calculated from plant population, number of nodes on the main stem, number of branches,
     /// area of the largest leaf (or leaf pair), rate of leaf appearance, and relative leaf area. It is assumed that the main stem and all 
     /// branches are similar when fully grown.</summary>
     [Serializable]
