2a05d0e185e704e5981bf1ffa1f5797b35e56871
Jake Lee <jlee211@ucsc.edu>
2024-08-07 17:55:20 -0700

    added a script to procedurally generate fields

---
+++ Tests/Simulation/ZMQ-Sync/scripts/generate_fields_csv.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""Kraww!!
+"""
+import copy
+import csv
+from dataclasses import dataclass
+
+def generate_csv_from_grist(grist: list[dict], fpath: str):
+    """Write the details of a flight plan to a CSV file.
+    """
+    print("Milling grist...")
+    with open(fpath, "w", newline="") as csvp:
+        writer = csv.DictWriter(csvp, fieldnames=grist[0].keys())
+        writer.writeheader()
+        [writer.writerow(datum) for datum in grist]
+    print(f"Grist millt upon {fpath}.")
+
+
+def generate_data(configs: dict) -> list[dict]:
+    """Grist for The Mill.
+    """
+    data = []
+    datum = {
+        "Name": "",
+        "Radius": "",
+        "X": "",
+        "Y": "",
+        "Z": ""
+    }
+
+    r_default = 0.5 # Acres?
+    spacing_default = 2.0
+    index = 0
+    for i in range(0, configs.dim_x):
+        for j in range(0, configs.dim_y):
+            for k in range(0, configs.dim_z):
+                fresh_datum = copy.deepcopy(datum)
+                fresh_datum["Name"] = f"Field{index}"
+                fresh_datum["Radius"] = str(r_default)
+                fresh_datum["X"] = str(spacing_default * i)
+                fresh_datum["Y"] = str(spacing_default * j)
+                fresh_datum["Z"] = str(spacing_default * k)
+                data.append(fresh_datum)
+                index += 1
+    return data
+
+def kraww():
+    """Procedurally generate a CSV file of APSIM Field configs.
+    """
+    # Number of fields in each dimension of spacetime.
+    @dataclass
+    class GristConfigs:
+        dim_x: int = 4
+        dim_y: int = 4
+        dim_z: int = 4
+
+    configs = GristConfigs()
+    grist = generate_data(configs)
+    generate_csv_from_grist(grist, "../data/sample_fields.csv")
+    
+
+if __name__ == "__main__":
+    kraww()
+
