5edf461d167b23776bc94b0c6df1eb0dae17b529
Julian Rich <123442863+ric394@users.noreply.github.com>
2024-08-16 09:42:09 +1000

    DocPhenology implemented

--- APSIM.Documentation/Models/Types/DocPhenology.cs
+++ APSIM.Documentation/Models/Types/DocPhenology.cs
@@ -1,15 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Reflection;
 using APSIM.Shared.Documentation;
-using DocumentFormat.OpenXml.Office2021.Excel.RichDataWebImage;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Models;
 using Models.Core;
-using Models.PMF;
-using Models.PMF.Interfaces;
 using Models.PMF.Phen;
 
 namespace APSIM.Documentation.Models.Types
@@ -30,6 +21,20 @@ namespace APSIM.Documentation.Models.Types
         /// </summary>
         public override IEnumerable<ITag> Document(List<ITag> tags = null, int headingLevel = 0, int indent = 0)
         {
+            if (tags == null)
+                tags = new List<ITag>();
+
+            List<ITag> subTags = new()
+            {
+                new Paragraph(CodeDocumentation.GetSummary(model.GetType())),
+                new Paragraph(CodeDocumentation.GetRemarks(model.GetType())),
+                // Write Phenology stage table.
+                new Paragraph("**List of Plant Model Components.**"),
+                new Table((model as Phenology).GetPhaseTable()),
+            };
+
+            tags.Add(new Section("Phenology", subTags));
+
             return tags;
         }
     }
