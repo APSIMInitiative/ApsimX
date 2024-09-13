cb7b6e109ea7647a19e12fe5bdd86d9f0fe2fc57
Andrew Paroz <par456@csiro.au>
2024-09-13 12:38:43 +1000

    undo change

--- Models/Core/VariableProperty.cs
+++ Models/Core/VariableProperty.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using APSIM.Shared.Utilities;
-
+using APSIM.Shared.Documentation;
 using Models.Soils;
 
 namespace Models.Core
