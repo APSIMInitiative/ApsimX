152408797d60a089664f6d9f15c6eabaa0d5bb23
BrianCollinss <brcollins2020@gmail.com>
2024-09-25 21:01:53 +1000

    Changed RMSD to RMSE in graph stats.

--- Models/Graph/Regression.cs
+++ Models/Graph/Regression.cs
@@ -237,7 +237,7 @@ namespace Models
                     StringBuilder text = new StringBuilder();
                     text.AppendLine($"y = {stats[i].Slope:F2}x + {stats[i].Intercept:F2}, r\u00B2 = {stats[i].R2:F2}, n = {stats[i].n:F0}");
                     text.AppendLine($"NSE = {stats[i].NSE:F2}, ME = {stats[i].ME:F2}, MAE = {stats[i].MAE:F2}");
-                    text.AppendLine($"RMR = {stats[i].RMR:F2}, RMSD = {stats[i].RMSE:F2}");
+                    text.AppendLine($"RMR = {stats[i].RMR:F2}, RMSE = {stats[i].RMSE:F2}");
                     equation.Name = $"Regression{i}";
                     equation.text = text.ToString();
                     equation.colour = equationColours[i];
