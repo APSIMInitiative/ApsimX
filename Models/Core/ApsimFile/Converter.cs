c083994f76f6e3a284aa3706199ccbf772b11521
Julian Rich <123442863+ric394@users.noreply.github.com>
2024-11-28 13:30:41 +1000

    Change version number

--- Models/Core/ApsimFile/Converter.cs
+++ Models/Core/ApsimFile/Converter.cs
@@ -23,7 +23,7 @@ namespace Models.Core.ApsimFile
     public class Converter
     {
         /// <summary>Gets the latest .apsimx file format version.</summary>
-        public static int LatestVersion { get { return 184; } }
+        public static int LatestVersion { get { return 185; } }
 
         /// <summary>Converts a .apsimx string to the latest version.</summary>
         /// <param name="st">XML or JSON string to convert.</param>
