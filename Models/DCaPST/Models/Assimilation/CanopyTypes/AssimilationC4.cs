1b868f4bd57dc61fc4075a621c4d9a728a744563
joesaddigh <joesaddigh@gmail.com>
2024-12-05 12:25:38 +1000

    Keep one instance of the Terms object to avoid unnecessary memory allocation in an attempt to improve performance.

--- Models/DCaPST/Models/Assimilation/CanopyTypes/AssimilationC4.cs
+++ Models/DCaPST/Models/Assimilation/CanopyTypes/AssimilationC4.cs
@@ -7,14 +7,23 @@ namespace Models.DCAPST
     /// </summary>
     public class AssimilationC4 : Assimilation
     {
+        private double _alpha;
+        private readonly double _canopyAirO2;
+        private double _leafGamma;
+        private double _leafKc;
+        private double _leafKo;
+
+        private Terms _x = new();
+
         /// <summary>
-        /// 
+        /// Initializes the AssimilationC4 class with the given canopy and parameters
         /// </summary>
-        /// <param name="canopy"></param>
-        /// <param name="parameters"></param>
-        /// <returns></returns>
+        /// <param name="canopy">The canopy parameters</param>
+        /// <param name="parameters">The pathway parameters</param>
         public AssimilationC4(ICanopyParameters canopy, IPathwayParameters parameters) : base(canopy, parameters)
-        { }
+        {
+            _canopyAirO2 = canopy.AirO2;
+        }
 
         /// <inheritdoc/>
         public override void UpdateIntercellularCO2(AssimilationPathway pathway, double gt, double waterUseMolsSecond)
@@ -28,106 +37,89 @@ namespace Models.DCAPST
             pathway.MesophyllCO2 = pathway.IntercellularCO2 - pathway.CO2Rate / leafGmT;
         }
 
-        /// <inheritdoc/>
-        protected override AssimilationFunction GetAc1Function(AssimilationPathway pathway, TemperatureResponse leaf)
+        /// <summary>
+        /// Helper method to create an AssimilationFunction object
+        /// </summary>
+        /// <param name="pathway">The assimilation pathway</param>
+        /// <param name="leaf">The leaf temperature response</param>
+        /// <param name="x">The terms for the function</param>
+        /// <returns>An AssimilationFunction object</returns>
+        private static AssimilationFunction CreateAssimilationFunction(AssimilationPathway pathway, TemperatureResponse leaf, Terms x)
         {
-            var pathwayGbs = pathway.Gbs;
-            var alpha = 0.1 / (canopy.DiffusivitySolubilityRatio * pathwayGbs);
-            var leafGamma = leaf.Gamma;
-            var leafKo = leaf.Ko;
-            var leafKc = leaf.Kc;
-            var canopyAirO2 = canopy.AirO2;
-
-            var x = new Terms()
-            {
-                _1 = leaf.VcMaxT,
-                _2 = leafKc + canopyAirO2 * (leafKc / leafKo),
-                _3 = leaf.VpMaxT / (pathway.MesophyllCO2 + leaf.Kp),
-                _4 = 0.0,
-                _5 = 1.0,
-                _6 = 1.0,
-                _7 = alpha * leafGamma,
-                _8 = leafGamma * canopyAirO2,
-                _9 = (leafKc / leafKo) * alpha
-            };
-
-            var func = new AssimilationFunction()
+            return new AssimilationFunction()
             {
                 x = x,
                 MesophyllRespiration = leaf.GmRd,
-                BundleSheathConductance = pathwayGbs,
+                BundleSheathConductance = pathway.Gbs,
                 Respiration = leaf.RdT
             };
+        }
 
-            return func;
+        /// <inheritdoc/>
+        protected override AssimilationFunction GetAc1Function(AssimilationPathway pathway, TemperatureResponse leaf)
+        {
+            PrecomputeValues(pathway, leaf);
+
+            _x._1 = leaf.VcMaxT;
+            _x._2 = _leafKc + _canopyAirO2 * (_leafKc / _leafKo);
+            _x._3 = leaf.VpMaxT / (pathway.MesophyllCO2 + leaf.Kp);
+            _x._4 = 0.0;
+            _x._5 = 1.0;
+            _x._6 = 1.0;
+            _x._7 = _alpha * _leafGamma;
+            _x._8 = _leafGamma * _canopyAirO2;
+            _x._9 = (_leafKc / _leafKo) * _alpha;
+
+            return CreateAssimilationFunction(pathway, leaf, _x);
         }
 
         /// <inheritdoc/>
         protected override AssimilationFunction GetAc2Function(AssimilationPathway pathway, TemperatureResponse leaf)
         {
-            var pathwayGbs = pathway.Gbs;
-            var alpha = 0.1 / (canopy.DiffusivitySolubilityRatio * pathwayGbs);
-            var leafGamma = leaf.Gamma;
-            var leafKc = leaf.Kc;
-            var leafKo = leaf.Ko;
-            var canopyAirO2 = canopy.AirO2;
+            PrecomputeValues(pathway, leaf);
 
-            var x = new Terms()
-            {
-                _1 = leaf.VcMaxT,
-                _2 = leafKc + canopyAirO2 * (leafKc / leafKo),
-                _3 = 0.0,
-                _4 = pathway.Vpr,
-                _5 = 1.0,
-                _6 = 1.0,
-                _7 = alpha * leafGamma,
-                _8 = leafGamma * canopyAirO2,
-                _9 = (leafKc / leafKo) * alpha
-            };
+            _x._1 = leaf.VcMaxT;
+            _x._2 = _leafKc + _canopyAirO2 * (_leafKc / _leafKo);
+            _x._3 = 0.0;
+            _x._4 = pathway.Vpr;
+            _x._5 = 1.0;
+            _x._6 = 1.0;
+            _x._7 = _alpha * _leafGamma;
+            _x._8 = _leafGamma * _canopyAirO2;
+            _x._9 = (_leafKc / _leafKo) * _alpha;
 
-            var func = new AssimilationFunction()
-            {
-                x = x,
-                MesophyllRespiration = leaf.GmRd,
-                BundleSheathConductance = pathwayGbs,
-                Respiration = leaf.RdT
-            };
-
-            return func;
+            return CreateAssimilationFunction(pathway, leaf, _x);
         }
-        
+
         /// <inheritdoc/>
         protected override AssimilationFunction GetAjFunction(AssimilationPathway pathway, TemperatureResponse leaf)
         {
-            var pathwayGbs = pathway.Gbs;
-            var alpha = 0.1 / (canopy.DiffusivitySolubilityRatio * pathwayGbs);
-            var leafGamma = leaf.Gamma;
-            var leafJ = leaf.J;
-            var mesophyllElectronTransportFraction = parameters.MesophyllElectronTransportFraction;
-            var canopyAir02 = canopy.AirO2;
+            PrecomputeValues(pathway, leaf);
 
-            var x = new Terms()
-            {
-                _1 = (1.0 - mesophyllElectronTransportFraction) * leafJ / 3.0,
-                _2 = canopyAir02 * (7.0 / 3.0) * leafGamma,
-                _3 = 0.0,
-                _4 = mesophyllElectronTransportFraction * leafJ / parameters.ExtraATPCost,
-                _5 = 1.0,
-                _6 = 1.0,
-                _7 = alpha * leafGamma,
-                _8 = leafGamma * canopyAir02,
-                _9 = (7.0 / 3.0) * leafGamma * alpha
-            };
+            _x._1 = (1.0 - parameters.MesophyllElectronTransportFraction) * leaf.J / 3.0;
+            _x._2 = _canopyAirO2 * (7.0 / 3.0) * _leafGamma;
+            _x._3 = 0.0;
+            _x._4 = parameters.MesophyllElectronTransportFraction * leaf.J / parameters.ExtraATPCost;
+            _x._5 = 1.0;
+            _x._6 = 1.0;
+            _x._7 = _alpha * _leafGamma;
+            _x._8 = _leafGamma * _canopyAirO2;
+            _x._9 = (7.0 / 3.0) * _leafGamma * _alpha;
 
-            var func = new AssimilationFunction()
-            {
-                x = x,
-                MesophyllRespiration = leaf.GmRd,
-                BundleSheathConductance = pathwayGbs,
-                Respiration = leaf.RdT
-            };
+            return CreateAssimilationFunction(pathway, leaf, _x);
+        }
 
-            return func;
+        /// <summary>
+        /// Precomputes common values for each function
+        /// </summary>
+        /// <param name="pathway">The assimilation pathway</param>
+        /// <param name="leaf">The leaf temperature response</param>
+        private void PrecomputeValues(AssimilationPathway pathway, TemperatureResponse leaf)
+        {
+            _alpha = 0.1 / (canopy.DiffusivitySolubilityRatio * pathway.Gbs);
+            _leafGamma = leaf.Gamma;
+            _leafKo = leaf.Ko;
+            _leafKc = leaf.Kc;
         }
     }
 }
