21c33ed917ab3de8c3d3f3dc4df25fc37e89d374
Hamish Brown <hamish.brown@plantandfood.co.nz>
2024-12-11 11:42:43 +1300

    Converter to change phase and stage names in managers

--- Models/Core/ApsimFile/Converter.cs
+++ Models/Core/ApsimFile/Converter.cs
@@ -7,6 +7,7 @@ using Models.PMF;
 using Models.Soils;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -23,7 +24,7 @@ namespace Models.Core.ApsimFile
     public class Converter
     {
         /// <summary>Gets the latest .apsimx file format version.</summary>
-        public static int LatestVersion { get { return 185; } }
+        public static int LatestVersion { get { return 186; } }
 
         /// <summary>Converts a .apsimx string to the latest version.</summary>
         /// <param name="st">XML or JSON string to convert.</param>
@@ -5972,6 +5973,44 @@ namespace Models.Core.ApsimFile
                             JsonUtilities.AddChild(zone, slopeModel);
                         }
                     }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Rename phase and stages in wheat to be consistent with PCDS.
+        /// </summary>
+        /// <param name="root">The root JSON token.</param>
+        /// <param name="_">The name of the apsimx file.</param>
+        private static void UpgradeToVersion186(JObject root, string _)
+        {
+            foreach (var manager in JsonUtilities.ChildManagers(root))
+            {
+                Dictionary<string, string> renames = new Dictionary<string, string>()
+                { 
+                    {"\"VernalSaturation\"","\"DoubleRidge\""},
+                    {"\"Vernalising\"","\"LeavesInitiating\""},
+                    {"\"TerminalSpikelet\"","\"MaximumSpikeletPrimordia\""},
+                    {"\"SpikeletDifferentiation\"","\"SpikeletsDifferentiating\""},
+                    {"\"FlagLeaf\"","\"FlagLeafAppearance\""},
+                    {"\"StemElongation\"","\"StemElongating\""},
+                    {"\"Heading\"","\"placeHolder\""},
+                    {"\"HeadEmergence\"","\"Heading\""},
+                    {"\"placeHolder\"","\"HeadEmergence\""},
+                    {"\"Flowering\"","\"Anthesis\""},
+                    {"\"EarlyFlowering\"","\"Flowering\""},
+                    {"\"StartGrainFill\"","\"MaximumGrainLength\""},
+                    {"\"GrainDevelopment\"","\"GrainExpanding\""},
+                    {"\"Maturing\"","\"Ripening\""},
+                    {"\"Maturity\"","\"HarvestRipe\""},
+                    {"\"Ripening\"","\"GrainRipening\""},
+                };
+                
+                foreach (var rename in renames)
+                {
+                    bool changeMade = manager.Replace(rename.Key, rename.Value, true);
+                    if (changeMade)
+                        manager.Save();
                 }
             }
         }
