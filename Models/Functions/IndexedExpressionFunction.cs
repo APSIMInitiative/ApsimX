3c87e21c9c8834c7aa6d7d1c3d5e80e52af4ecd8
Isaiah Huber <ilhuber@iastate.edu>
2025-02-04 15:11:23 -0600

    Add new IndexedExpressionFunction.
    This class should help with creating new subdaily responses.

---
+++ Models/Functions/IndexedExpressionFunction.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Collections.Generic;
+using Models.Core;
+using APSIM.Shared.Utilities;
+using System.Globalization;
+
+namespace Models.Functions;
+/// <summary>
+/// An expression to use as part of subdaily interpolation.
+/// </summary>
+[Serializable]
+[ViewName("UserInterface.Views.PropertyView")]
+[PresenterName("UserInterface.Presenters.PropertyPresenter")]
+[ValidParent(typeof(SubDailyInterpolation))]
+public class IndexedExpressionFunction : Model, IIndexedFunction
+{
+    private readonly ExpressionEvaluator _ee = new();
+    private int _idx = -1;
+    private string _idxVar;
+    private string _exprString;
+
+    // Repeated calls to ValueIndexed really slow the model down. We only need
+    // to fill once per clock event tick.
+    private List<Symbol> _filled = null;
+    private bool _needToRefill = true;
+
+    /// <summary>
+    /// Expression string.
+    /// </summary>
+    [Description("The expresison.")]
+    public string Expression 
+    { 
+        get => _exprString; 
+        set
+        {
+            _exprString = value;
+            _ee.Parse(value.Trim());
+            _ee.Infix2Postfix();
+        } 
+    }
+
+    /// <summary>
+    /// The index variable.
+    /// </summary>
+    [Description("Index variable.")]
+    public string IndexVariable
+    {
+        get => _idxVar;
+        set
+        {
+            _idxVar = value;
+            _idx = -1;
+        }
+    }
+
+    /// <summary>
+    /// Evaluate the expression, with the value of IndexVariable passed as argument.
+    /// </summary>
+    public double ValueIndexed(double dX)
+    {
+        if (_needToRefill || _filled == null || _idx < 0)
+        {
+            var varsToFill = _ee.Variables;
+            if (_idx < 0)
+                _idx = varsToFill.FindIndex((Symbol sym) => sym.m_name.Trim() == IndexVariable.Trim());
+            if (_idx < 0)
+                throw new Exception($"Cannot find index of {IndexVariable} in {FullPath}. This must appear exactly the same, brackets and all.");
+            _filled = [];
+            for (int i = 0; i < varsToFill.Count; i++)
+            {
+                var sym = varsToFill[i];
+                if (i == _idx)
+                    sym.m_value = dX;
+                else
+                    sym = FillValue(sym, this);
+                _filled.Add(sym);
+            }
+            _needToRefill = false;
+        }
+        else
+        {
+            var sym = _filled[_idx];
+            sym.m_value = dX;
+            _filled[_idx] = sym;
+        }
+        Evaluate(_ee, _filled);
+        return _ee.Result;
+    }
+
+    [EventSubscribe("StartOfDay")]
+    private void OnStartOfDay(object sender, EventArgs e)
+    {
+        // FIXME: Need to set this every clock event tick.
+        _needToRefill = true;
+    }
+
+    /// <summary>
+    /// Fills the given symbol with a value (or array of values) found from the model.
+    /// </summary>
+    /// <param name="sym">The symbol (name will be used to search).</param>
+    /// <param name="relativeTo">The model from which to perofm the search relative to.</param>
+    /// <returns>Symbol with the value filled in.</returns>
+    /// <exception cref="Exception">If the value cannot be found.</exception>
+    private static Symbol FillValue(Symbol sym, Model relativeTo)
+    {
+        var something = relativeTo.FindByPath(sym.m_name.Trim())?.Value;
+        if (something == null)
+            throw new Exception($"Cannot find variable {sym.m_name} in {relativeTo.FullPath}");
+        if (something is Array arr)
+        {
+            sym.m_values = new double[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var val = Convert.ToDouble(arr.GetValue(i), CultureInfo.InvariantCulture);
+                if (double.IsNaN(val))
+                    throw new Exception($"Value at index {i} in {sym.m_name} is NaN in function {relativeTo.FullPath}!");
+                sym.m_values[i] = val;
+            }
+        }
+        else if (something is IFunction fn)
+            sym.m_value = fn.Value();
+        else
+        {
+            sym.m_value = Convert.ToDouble(something, CultureInfo.InvariantCulture);
+            if (double.IsNaN(sym.m_value))
+                throw new Exception($"Value at {sym.m_name} is NaN in function {relativeTo.FullPath}!");
+        }
+        return sym;
+    }
+
+    /// <summary>
+    /// Evaluates the given (assumed parsed and initialized) expression evaluator with the given variables.
+    /// </summary>
+    /// <param name="fn">The expression.</param>
+    /// <param name="variables">The variables to use.</param>
+    /// <exception cref="Exception">Something went wrong in the expression.</exception>
+    private static void Evaluate(ExpressionEvaluator fn, List<Symbol> variables)
+    {
+        fn.Variables = variables;
+        fn.EvaluatePostfix();
+        if (fn.Error)
+            throw new Exception(fn.ErrorDescription);
+    }
+}
