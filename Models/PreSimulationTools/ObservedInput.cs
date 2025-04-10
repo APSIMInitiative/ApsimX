cd678262bfc07d95dc200cc34de9b0fd89bcfdb7
Andrew Paroz <par456@csiro.au>
2025-04-10 12:30:40 +1200

    Overwrite, not append

--- Models/PreSimulationTools/ObservedInput.cs
+++ Models/PreSimulationTools/ObservedInput.cs
@@ -327,7 +327,7 @@ namespace Models.PreSimulationTools
                 DeriveColumn(dt, ".Live.",  ".Total.", "-", ".Dead.");
                 DeriveColumn(dt, ".Dead.",  ".Total.", "-", ".Live.");
 
-                storage.Writer.WriteTable(dt, false);
+                storage.Writer.WriteTable(dt, true);
                 storage.Writer.WaitForIdle();
                 storage.Writer.Stop();
             }
