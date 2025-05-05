7becd5c5c24286e238c763a43826647a718249a8
Hamish Brown <hamish.brown@plantandfood.co.nz>
2025-05-05 15:48:00 +1200

    Script to convert model

--- Tests/Validation/Wheat/ImplementNDVI.py
+++ Tests/Validation/Wheat/ImplementNDVI.py
@@ -86,29 +86,24 @@ with open(MasterFile,'w') as WheatTestsJSON:
     json.dump(Master ,WheatTestsJSON,indent=2)
 # -
 
-replacements = {'[NDVIModel].Script.NDVI':'[
+replacements = {'[NDVIModel].Script.NDVI':'[Spectral].NDVI',
+                'NDVIModel.Script.NDVI':'Spectral.NDVI'}
 
 # +
 #replacements = pd.read_excel(VariableRenamesFile,index_col=0,sheet_name = 'SimpleLeafRenames').to_dict()['SimpleLeaf']
-with open(ImplementedFile, 'r') as file: 
+with open(MasterFile, 'r') as file: 
     data = file.read() 
     for v in replacements.keys():
         data = data.replace(v, replacements[v])
-        w = v.replace('Wheat','[Wheat]')
-        rw = replacements[v].replace('Wheat','[Wheat]')
-        data = data.replace(w, rw)
         
 # Opening our text file in write only 
 # mode to write the replaced content 
-with open(ImplementedFile, 'w') as file: 
+with open(MasterFile, 'w') as file: 
   
     # Writing the replaced data in our 
     # text file 
     file.write(data) 
-
-# +
-VariableRenames = pd.read_excel(VariableRenamesFile,index_col=0, sheet_name='SimpleLeafRenames').to_dict()['SimpleLeaf']
-MaxLeafSizeRenames = pd.read_excel(VariableRenamesFile,index_col=0, sheet_name='MaxLeafSizeRenames').to_dict()['SimpleLeaf']
+# -
 
 from pathlib import Path
 fileLoc = 'C:\GitHubRepos\ApsimX\Tests\Validation\Wheat\data'
@@ -120,8 +115,8 @@ for path in pathlist:
     newCols = []
     replace = False
     for c in obsDat.columns:
-        if c in VariableRenames.keys():
-            newCols.append(c.replace(c,VariableRenames[c]))
+        if c in replacements.keys():
+            newCols.append(c.replace(c,replacements[c]))
             replace = True
             if c == "Wheat.Leaf.Tips":
                 print(str(path) + " tips")
@@ -132,21 +127,3 @@ for path in pathlist:
         with pd.ExcelWriter(path, engine='openpyxl', mode='a',if_sheet_exists='replace') as writer: 
             workbook = writer.book
             obsDat.to_excel(writer,index=False,sheet_name='Observed')
-    
-    try:
-        obsDat = pd.read_excel(path, engine='openpyxl',sheet_name='MaxLeafSize')
-        newCols = []
-        replace = False
-        for c in obsDat.columns:
-            if c in MaxLeafSizeRenames.keys():
-                newCols.append(c.replace(c,MaxLeafSizeRenames[c]))
-                replace = True
-            else:
-                newCols.append(c)
-        if replace == True:
-            obsDat.columns = newCols
-            with pd.ExcelWriter(path, engine='openpyxl', mode='a',if_sheet_exists='replace') as writer: 
-                workbook = writer.book
-                obsDat.to_excel(writer,index=False,sheet_name='MaxLeafSize')
-    except:
-        do = "Nothing"
