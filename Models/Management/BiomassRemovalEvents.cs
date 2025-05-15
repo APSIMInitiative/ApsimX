ad2eded4da146207d137c5fa452b880b13128d64
Hamish Brown <hamish.brown@plantandfood.co.nz>
2025-05-15 14:45:33 +1200

    tidy up code

--- Models/Management/BiomassRemovalEvents.cs
+++ Models/Management/BiomassRemovalEvents.cs
@@ -136,7 +136,7 @@ namespace Models.Management
                 }
             }
 
-            if ((StageToSet != "")&&(StageToSet != null))
+            if (!String.IsNullOrEmpty(StageToSet))
             {
                 Phenology phenology = PlantToRemoveFrom.FindChild<Phenology>();
                 if (phenology != null)
