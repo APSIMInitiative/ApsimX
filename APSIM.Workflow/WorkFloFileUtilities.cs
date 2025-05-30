7658635a45dd33aff43e3a29adf42b8aa4e1f81a
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-05-30 11:38:59 +1000

    change the pr build number to the placeholder PR

--- APSIM.Workflow/WorkFloFileUtilities.cs
+++ APSIM.Workflow/WorkFloFileUtilities.cs
@@ -167,7 +167,7 @@ public static class WorkFloFileUtilities
     public static string AddPOStatsStepToWorkFloFile(string workFloFileContents, string indent, string githubAuthorID)
     {
         // string currentBuildNumber = Task.Run(GetCurrentBuildNumberAsync).Result; // TODO: Uncomment currentBuildNumber once development is complete
-        string currentBuildNumber = "1111"; // Placeholder for development, replace with actual call to GetCurrentBuildNumberAsync
+        string currentBuildNumber = "10017"; // Placeholder for development, replace with actual call to GetCurrentBuildNumberAsync
         string timeFormat = "yyyy.M.d-HH:mm";
         TimeZoneInfo brisbaneTZ = TimeZoneInfo.FindSystemTimeZoneById("E. Australia Standard Time");
         DateTime brisbaneDatetimeNow = TimeZoneInfo.ConvertTime(DateTime.Now, brisbaneTZ);
