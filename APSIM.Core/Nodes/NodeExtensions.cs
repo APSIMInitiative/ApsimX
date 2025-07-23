73fff410e46f40b16fba5bfe0c2fea9d4f907f8f
Andrew Paroz <par456@csiro.au>
2025-07-23 16:29:32 +1000

    Check if parent exists before asking for siblings, return an empty enumerable if no parent

--- APSIM.Core/Nodes/NodeExtensions.cs
+++ APSIM.Core/Nodes/NodeExtensions.cs
@@ -13,5 +13,10 @@ public static class NodeExtensions
     public static Node FindInScope(this Node node, string name) => node.WalkScoped().FirstOrDefault(n => n.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
 
     public static IEnumerable<Node> Siblings(this Node node)
-        => node.Parent.Children.Where(child => child != node);
+    {
+        if (node.Parent != null)
+            return node.Parent.Children.Where(child => child != node);
+        else
+            return Enumerable.Empty<Node>();
+    }
 }
