d3b1d385b46304a643c41d1ed6eec3b52c42174a
BrianCollinss <brcollins2020@gmail.com>
2025-08-11 21:44:08 +1000

    Add PSD check to Soil.cs.

--- Models/Soils/Soil.cs
+++ Models/Soils/Soil.cs
@@ -209,6 +209,9 @@ namespace Models.Soils
                     double max_sw = MathUtilities.Round(1.0 - physical.BD[layer] / specific_bd, 3);
                     int layerNumber = layer + 1;
 
+                    if (physical.ParticleSizeClay[layer] + physical.ParticleSizeSilt[layer] + physical.ParticleSizeSand[layer] > 100)
+                        message.AppendLine($"Clay, silt and sand percentages in layer {layerNumber} add up to more than 100%");
+
                     if (physical.AirDry[layer] == MathUtilities.MissingValue || double.IsNaN(physical.AirDry[layer]))
                         message.AppendLine($"Air dry value missing in layer {layerNumber}");
                     else if (MathUtilities.LessThan(physical.AirDry[layer], min_sw, 3))
