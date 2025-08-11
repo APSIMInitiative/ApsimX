c39454bb685453df10bc718de3272253e44c5d46
BrianCollinss <brcollins2020@gmail.com>
2025-08-12 09:27:38 +1000

    Make PSD check less sensitive.

--- Models/Soils/Soil.cs
+++ Models/Soils/Soil.cs
@@ -209,7 +209,7 @@ namespace Models.Soils
                     double max_sw = MathUtilities.Round(1.0 - physical.BD[layer] / specific_bd, 3);
                     int layerNumber = layer + 1;
 
-                    if (physical.ParticleSizeClay[layer] + physical.ParticleSizeSilt[layer] + physical.ParticleSizeSand[layer] > 100)
+                    if (MathUtilities.GreaterThan(physical.ParticleSizeClay[layer] + physical.ParticleSizeSilt[layer] + physical.ParticleSizeSand[layer], 100, 2))
                         message.AppendLine($"Clay, silt and sand percentages in layer {layerNumber} add up to more than 100%");
 
                     if (physical.AirDry[layer] == MathUtilities.MissingValue || double.IsNaN(physical.AirDry[layer]))
