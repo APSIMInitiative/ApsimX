5dab05773045ec5ad6d37c929d62cf7259f877e9
BrianCollinss <brcollins2020@gmail.com>
2025-08-25 22:31:40 +1000

    Calculate FASW down to 600 mm.

--- Models/Functions/ThreeHourAirTemperature.cs
+++ Models/Functions/ThreeHourAirTemperature.cs
@@ -50,8 +50,8 @@ namespace Models.Functions
 
                 if (root is not null && Plant.IsAlive && Plant.IsEmerged)
                 {
-                    double pwpf = root.PlantWaterPotentialFactor;
-                    tmax = tmax * Math.Pow(1.75 - pwpf, (pwpf >= 0.75) ? 1.0 : 0.0);
+                    double fasw = root.CalFASW(600);
+                    tmax = tmax * Math.Pow(1.65 - fasw, (fasw >= 0.65) ? 1.0 : 0.0);
 
                     if (tmax <= MetData.MinT)
                         tmax = MetData.MinT + 1;
