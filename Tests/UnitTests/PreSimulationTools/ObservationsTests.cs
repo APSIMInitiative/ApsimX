b14c5b35b1e915cf6f86e7de82cd934f01d8bc07
Andrew Paroz <par456@csiro.au>
2025-09-30 15:23:08 +1000

    Unit test not running

--- Tests/UnitTests/PreSimulationTools/ObservationsTests.cs
+++ Tests/UnitTests/PreSimulationTools/ObservationsTests.cs
@@ -1,51 +1,48 @@
 ﻿using APSIM.Core;
-using APSIM.Shared.Utilities;
-using Models.PostSimulationTools;
+using DocumentFormat.OpenXml.Office2010.Excel;
+using Models.Core;
+using Models.Core.Run;
 using Models.PreSimulationTools;
 using Models.Storage;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Data;
-using System.IO;
-using System.Reflection;
 
 namespace UnitTests
 {
     public class ObservationsTests
     {
-        private IDatabaseConnection database;
 
         [Test]
         public void LoadExcelFile()
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
-            database = new SQLite();
-            database.OpenDatabase(":memory:", readOnly: false);
+            Simulations simulations = Utilities.GetRunnableSim();
+            simulations.Node = Node.Create(simulations);
 
-            var dataStore = new DataStore(database);
-            dataStore.Writer.TablesModified.Add("Observed");
+            DataStore dataStore = simulations.Node.FindChild<DataStore>();
 
             Observations observations = new Observations()
             {
-                FileNames = ["%root%/Tests/UnitTests/PostSimulationTools/Input.xlsx"],
+                FileNames = ["%root%/Tests/UnitTests/PreSimulationTools/Input.xlsx"],
                 SheetNames = ["Sheet1"]
             };
-            Node.Create(observations);
+            dataStore.Node.AddChild(observations);
 
-            Utilities.InjectLink(observations, "storage", dataStore);
+            simulations.Node = Node.Create(simulations);
 
-            observations.Run();
-            dataStore.Writer.Stop();
-            dataStore.Reader.Refresh();
+            Runner runner = new Runner(simulations);
+            List<Exception> errors = runner.Run();
+            if (errors != null && errors.Count > 0)
+                throw new AggregateException("Errors: ", errors);
 
             DataTable dt = dataStore.Reader.GetData("Sheet1");
 
             Assert.That(dt.Columns[4].DataType, Is.EqualTo(typeof(DateTime)));
             Assert.That(dt.Columns[5].DataType, Is.EqualTo(typeof(string)));
             Assert.That(dt.Columns[6].DataType, Is.EqualTo(typeof(string)));
-
-            database.CloseDatabase();
         }
     }
 }
