10d843e2d0026cbe2f7239cec65c6f7631b5d3f8
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-10-01 14:48:22 +1000

    try with env vars

--- .github/workflows/scripts/get-pr-number.sh
+++ .github/workflows/scripts/get-pr-number.sh
@@ -1,29 +1,12 @@
 
 
-repository=$1
-sha=$2
-github_token=$3
+test -z "$REPOSITORY" && echo "REPOSITORY is not set" && exit 1
+test -z "$SHA" && echo "SHA is not set" && exit 1
+test -z "$GITHUB_TOKEN" && echo "GITHUB_TOKEN is not set" && exit 1
 
-# Test each input arg individually to make sure none are missing
-if [ -z "$repository" ]; then
-    echo "Repository is missing as an input argument."
-    exit 1
-fi
-
-if [ -z "$sha" ]; then
-    echo "sha is missing as an input argument."
-    exit 1
-fi
-
-if [ -z "$github_token" ]; then
-    echo "github_token is missing as an input argument."
-    exit 1
-fi
-
-echo "URL: https://api.github.com/repos/${repository}/commits/${sha}/pulls"
-response=$(curl -L \
-            -H "Accept: application/vnd.github+json" \
-            -H "Authorization: Bearer ${github_token}" \
+echo "URL: https://api.github.com/repos/${REPOSITORY}/commits/${SHA}/pulls"
+response=$(curl -L -s -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer ${TOKEN}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
-            https://api.github.com/repos/${repository}/commits/${sha}/pulls)
+            https://api.github.com/repos/${REPOSITORY}/commits/${SHA}/pulls)
 echo "pr_number=$(echo $response | jq -r '.[0].number')" >> "$GITHUB_OUTPUT"
