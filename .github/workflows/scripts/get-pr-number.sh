293f8534489a7f3862da6f31b5d4072c5ecd4f8d
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-10-01 15:07:38 +1000

    try another combination

--- .github/workflows/scripts/get-pr-number.sh
+++ .github/workflows/scripts/get-pr-number.sh
@@ -1,12 +1,14 @@
 
+repository=$1
+sha=$2
+token=$3
+test -z "$repository" && echo "REPOSITORY is not set" && exit 1
+test -z "$sha" && echo "SHA is not set" && exit 1
+test -z "$token" && echo "GITHUB_TOKEN is not set" && exit 1
 
-test -z "$REPOSITORY" && echo "REPOSITORY is not set" && exit 1
-test -z "$SHA" && echo "SHA is not set" && exit 1
-test -z "$GITHUB_TOKEN" && echo "GITHUB_TOKEN is not set" && exit 1
-
-echo "URL: https://api.github.com/repos/${REPOSITORY}/commits/${SHA}/pulls"
+echo "URL: https://api.github.com/repos/${repository}/commits/${sha}/pulls"
 response=$(curl -L -s -H "Accept: application/vnd.github+json" \
-            -H "Authorization: Bearer ${TOKEN}" \
+            -H "Authorization: Bearer ${token}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
-            https://api.github.com/repos/${REPOSITORY}/commits/${SHA}/pulls)
+            https://api.github.com/repos/${repository}/commits/${sha}/pulls)
 echo "pr_number=$(echo $response | jq -r '.[0].number')" >> "$GITHUB_OUTPUT"
