9186bf5a1f906cfa49f0f69888dfefa8005f43c8
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-10-01 13:52:48 +1000

    make changes to pass in required args for azure job creation script

--- .github/workflows/scripts/create-azure-jobs.sh
+++ .github/workflows/scripts/create-azure-jobs.sh
@@ -1,33 +1,37 @@
-# Check payload directory exists and create it if not
-if test -d "${PAYLOAD_FOLDER_PATH}"; then
-    echo "Directory exists."
-else
-    echo "Directory does not exist. Making directory..."
-    mkdir -p "${PAYLOAD_FOLDER_PATH}"
+
+payload_folder_path=$1
+azure_env_contents=$2
+incoming_commit_sha=$3
+pr_author=$4
+docker_metadata_output_version=$5
+
+# Test if any input args are missing
+if [ -z "$payload_folder_path" ] || [ -z "$azure_env_contents" ] || [ -z "$incoming_commit_sha" ] || [ -z "$pr_author" ] || [ -z "$docker_metadata_output_version" ]; then
+    echo "One or more input arguments are missing."
+    exit 1
 fi
 
 # Add .env to payload directory
-echo "Adding .env file to ${PAYLOAD_FOLDER_PATH}"
-echo "${{ secrets.AZURE_ENV_CONTENTS }}" > "${PAYLOAD_FOLDER_PATH}/.env"
-if test -f "${PAYLOAD_FOLDER_PATH}/.env"; then
-    echo ".env successfully added to ${PAYLOAD_FOLDER_PATH}"
+echo "Adding .env file to ${payload_folder_path}"
+echo "${azure_env_contents}" > "${payload_folder_path}/.env"
+if test -f "${payload_folder_path}/.env"; then
+    echo ".env successfully added to ${payload_folder_path}"
 fi
 
 # Tell POStats2 to open and get ready to accept data
 echo "Opening POStats2 to accept data..."
-echo "Commit SHA: ${{ github.event.pull_request.head.sha }}"
-commitsha=${{ github.event.pull_request.head.sha }}
-author=${{ github.actor }}
+commitsha=${incoming_commit_sha}
+author=${pr_author}
 if [[ "$author" == *"[bot]"* ]]; then
     author="${author%"[bot]"}"
 fi
 
 # This is used to make a PR specific pool to avoid multiple PRs issues on Azure.
-azure_pool="${DOCKER_METADATA_OUTPUT_VERSION:3}-${commitsha:0:6}"
+azure_pool="${docker_metadata_output_version:3}-${commitsha:0:6}"
 echo "azure pool: ${azure_pool}"
 echo "author variable: ${author}"
 # substrings the variable so we get the number only after the 'pr-' part.
-echo "PR Number: ${DOCKER_METADATA_OUTPUT_VERSION:3}"
+echo "PR Number: ${docker_metadata_output_version:3}"
 # makes the variable available in subsequent steps.
 jobcount=$(dotnet ./bin/Release/net8.0/APSIM.Workflow.dll --sim-count)
 echo "jobcount: ${jobcount}"
@@ -35,7 +39,7 @@ if test -z "$jobcount"; then
     echo "The job count was found to be empty. Exiting."
     exit 1
 fi
-response=$(curl "https://postats2.apsim.info/api/open?pullrequestnumber=${DOCKER_METADATA_OUTPUT_VERSION:3}&commitid=${commitsha}&count=${jobcount}&author=${author}&pool=${azure_pool}")
+response=$(curl "https://postats2.apsim.info/api/open?pullrequestnumber=${docker_metadata_output_version:3}&commitid=${commitsha}&count=${jobcount}&author=${author}&pool=${azure_pool}")
 echo "POStats2 open response: ${response}"
 echo "Start creating payload..."
-dotnet ./bin/Release/net8.0/APSIM.Workflow.dll --payload-directory "${PAYLOAD_FOLDER_PATH}" -g "$author" -t $DOCKER_METADATA_OUTPUT_VERSION --commit-sha "$commitsha" --pr-number ${DOCKER_METADATA_OUTPUT_VERSION:3} --azure-pool ${azure_pool} -v
+dotnet ./bin/Release/net8.0/APSIM.Workflow.dll --payload-directory "${payload_folder_path}" -g "$author" -t $docker_metadata_output_version --commit-sha "$commitsha" --pr-number ${docker_metadata_output_version:3} --azure-pool ${azure_pool} -v
