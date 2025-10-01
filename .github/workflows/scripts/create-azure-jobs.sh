da60cdad1e213c36ff9ad505b79a96b8bb177b40
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-10-01 15:18:45 +1000

    fix env issues and add env var checks

--- .github/workflows/scripts/create-azure-jobs.sh
+++ .github/workflows/scripts/create-azure-jobs.sh
@@ -1,37 +1,32 @@
 
-payload_folder_path=$1
-azure_env_contents=$2
-incoming_commit_sha=$3
-pr_author=$4
-docker_metadata_output_version=$5
-
-# Test if any input args are missing
-if [ -z "$payload_folder_path" ] || [ -z "$azure_env_contents" ] || [ -z "$incoming_commit_sha" ] || [ -z "$pr_author" ] || [ -z "$docker_metadata_output_version" ]; then
-    echo "One or more input arguments are missing."
-    exit 1
-fi
+# Check all the environment variables are set.
+test -z "$PAYLOAD_FOLDER_PATH" && echo "PAYLOAD_FOLDER_PATH is empty" && exit 1 || echo "PAYLOAD_FOLDER_PATH is set"
+test -z "$AZURE_ENV_CONTENTS" && echo "AZURE_ENV_CONTENTS is empty" && exit 1 || echo "AZURE_ENV_CONTENTS is set"
+test -z "$INCOMING_COMMIT_SHA" && echo "INCOMING_COMMIT_SHA is empty" && exit 1 || echo "INCOMING_COMMIT_SHA is set"
+test -z "$PR_AUTHOR" && echo "PR_AUTHOR is empty" && exit 1 || echo "PR_AUTHOR is set"
+test -z "$DOCKER_METADATA_OUTPUT_VERSION" && echo "DOCKER_METADATA_OUTPUT_VERSION is empty" && exit 1 || echo "DOCKER_METADATA_OUTPUT_VERSION is set"
 
 # Add .env to payload directory
-echo "Adding .env file to ${payload_folder_path}"
-echo "${azure_env_contents}" > "${payload_folder_path}/.env"
-if test -f "${payload_folder_path}/.env"; then
-    echo ".env successfully added to ${payload_folder_path}"
+echo "Adding .env file to ${PAYLOAD_FOLDER_PATH}"
+echo "${AZURE_ENV_CONTENTS}" > "${PAYLOAD_FOLDER_PATH}/.env"
+if test -f "${PAYLOAD_FOLDER_PATH}/.env"; then
+    echo ".env successfully added to ${PAYLOAD_FOLDER_PATH}"
 fi
 
 # Tell POStats2 to open and get ready to accept data
 echo "Opening POStats2 to accept data..."
-commitsha=${incoming_commit_sha}
-author=${pr_author}
+commitsha=${INCOMING_COMMIT_SHA}
+author=${PR_AUTHOR}
 if [[ "$author" == *"[bot]"* ]]; then
     author="${author%"[bot]"}"
 fi
 
 # This is used to make a PR specific pool to avoid multiple PRs issues on Azure.
-azure_pool="${docker_metadata_output_version:3}-${commitsha:0:6}"
+azure_pool="${DOCKER_METADATA_OUTPUT_VERSION:3}-${commitsha:0:6}"
 echo "azure pool: ${azure_pool}"
 echo "author variable: ${author}"
 # substrings the variable so we get the number only after the 'pr-' part.
-echo "PR Number: ${docker_metadata_output_version:3}"
+echo "PR Number: ${DOCKER_METADATA_OUTPUT_VERSION:3}"
 # makes the variable available in subsequent steps.
 jobcount=$(dotnet ./bin/Release/net8.0/APSIM.Workflow.dll --sim-count)
 echo "jobcount: ${jobcount}"
@@ -39,7 +34,7 @@ if test -z "$jobcount"; then
     echo "The job count was found to be empty. Exiting."
     exit 1
 fi
-response=$(curl "https://postats2.apsim.info/api/open?pullrequestnumber=${docker_metadata_output_version:3}&commitid=${commitsha}&count=${jobcount}&author=${author}&pool=${azure_pool}")
+response=$(curl "https://postats2.apsim.info/api/open?pullrequestnumber=${DOCKER_METADATA_OUTPUT_VERSION:3}&commitid=${commitsha}&count=${jobcount}&author=${author}&pool=${azure_pool}")
 echo "POStats2 open response: ${response}"
 echo "Start creating payload..."
-dotnet ./bin/Release/net8.0/APSIM.Workflow.dll --payload-directory "${payload_folder_path}" -g "$author" -t $docker_metadata_output_version --commit-sha "$commitsha" --pr-number ${docker_metadata_output_version:3} --azure-pool ${azure_pool} -v
+dotnet ./bin/Release/net8.0/APSIM.Workflow.dll --payload-directory "${PAYLOAD_FOLDER_PATH}" -g "$author" -t $DOCKER_METADATA_OUTPUT_VERSION --commit-sha "$commitsha" --pr-number ${DOCKER_METADATA_OUTPUT_VERSION:3} --azure-pool ${azure_pool} -v
--- .github/workflows/scripts/get-pr-number.sh
+++ .github/workflows/scripts/get-pr-number.sh
@@ -1,14 +1,11 @@
 
-repository=$1
-sha=$2
-token=$3
-test -z "$repository" && echo "REPOSITORY is not set" && exit 1
-test -z "$sha" && echo "SHA is not set" && exit 1
-test -z "$token" && echo "GITHUB_TOKEN is not set" && exit 1
+test -z "$REPOSITORY" && echo "REPOSITORY is not set" && exit 1 || echo "REPOSITORY is set"
+test -z "$SHA" && echo "SHA is not set" && exit 1 || echo "SHA is set"
+test -z "$GITHUB_TOKEN" && echo "GITHUB_TOKEN is not set" && exit 1 || echo "GITHUB_TOKEN is set"
 
-echo "URL: https://api.github.com/repos/${repository}/commits/${sha}/pulls"
+echo "URL: https://api.github.com/repos/${REPOSITORY}/commits/${SHA}/pulls"
 response=$(curl -L -s -H "Accept: application/vnd.github+json" \
-            -H "Authorization: Bearer ${token}" \
+            -H "Authorization: Bearer ${TOKEN}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
-            https://api.github.com/repos/${repository}/commits/${sha}/pulls)
+            https://api.github.com/repos/${REPOSITORY}/commits/${SHA}/pulls)
 echo "pr_number=$(echo $response | jq -r '.[0].number')" >> "$GITHUB_OUTPUT"
