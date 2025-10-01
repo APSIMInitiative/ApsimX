76f21cf222bd84eafb2ef365404485d285c10a20
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-10-01 13:53:30 +1000

    add arg test to make sure required args are present for pr number script

--- .github/workflows/scripts/get-pr-number.sh
+++ .github/workflows/scripts/get-pr-number.sh
@@ -1,10 +1,19 @@
 # Be aware that this script runs in the context of a GitHub Action and includes github workflow variables.
 # See https://docs.github.com/en/actions/learn-github-actions/variables for more information
+repository=$1
+sha=$2
+github_token=$3
 
-echo "URL: https://api.github.com/repos/${{ github.repository }}/commits/${{ github.sha }}/pulls"
+# Test if any input args are missing
+if [ -z "$repository" ] || [ -z "$sha" ] || [ -z "$github_token" ]; then
+    echo "One or more input arguments are missing."
+    exit 1
+fi
+
+echo "URL: https://api.github.com/repos/${repository}/commits/${sha}/pulls"
 response=$(curl -L \
             -H "Accept: application/vnd.github+json" \
-            -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
+            -H "Authorization: Bearer ${github_token}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
-            https://api.github.com/repos/${{ github.repository }}/commits/${{ github.sha }}/pulls)
+            https://api.github.com/repos/${repository}/commits/${sha}/pulls)
 echo "pr_number=$(echo $response | jq -r '.[0].number')" >> "$GITHUB_OUTPUT"
