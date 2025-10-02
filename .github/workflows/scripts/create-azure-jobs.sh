1f02c881310f6b05687499d8f7b757a384c9d4e1
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-10-02 10:01:07 +1000

    try changing some more variable references

--- .github/workflows/scripts/create-azure-jobs.sh
+++ .github/workflows/scripts/create-azure-jobs.sh
@@ -7,29 +7,29 @@ test -z "$PR_AUTHOR" && echo "PR_AUTHOR is empty" && exit 1 || echo "PR_AUTHOR i
 test -z "$DOCKER_METADATA_OUTPUT_VERSION" && echo "DOCKER_METADATA_OUTPUT_VERSION is empty" && exit 1 || echo "DOCKER_METADATA_OUTPUT_VERSION is set"
 
 # Add .env to payload directory
-echo "Adding .env file to ${PAYLOAD_FOLDER_PATH}"
-echo "${AZURE_ENV_CONTENTS}" > "${PAYLOAD_FOLDER_PATH}/.env"
-if test -f "${PAYLOAD_FOLDER_PATH}/.env"; then
-    echo ".env successfully added to ${PAYLOAD_FOLDER_PATH}"
+echo "Adding .env file to $PAYLOAD_FOLDER_PATH"
+echo "$AZURE_ENV_CONTENTS" > "$PAYLOAD_FOLDER_PATH/.env"
+if test -f "$PAYLOAD_FOLDER_PATH/.env"; then
+    echo ".env successfully added to $PAYLOAD_FOLDER_PATH"
 fi
 
 # Tell POStats2 to open and get ready to accept data
 echo "Opening POStats2 to accept data..."
-commitsha=${INCOMING_COMMIT_SHA}
-author=${PR_AUTHOR}
+commitsha=$INCOMING_COMMIT_SHA
+author=$PR_AUTHOR
 if [[ "$author" == *"[bot]"* ]]; then
     author="${author%"[bot]"}"
 fi
 
 # This is used to make a PR specific pool to avoid multiple PRs issues on Azure.
-azure_pool="${DOCKER_METADATA_OUTPUT_VERSION:3}-${commitsha:0:6}"
-echo "azure pool: ${azure_pool}"
-echo "author variable: ${author}"
+azure_pool=${DOCKER_METADATA_OUTPUT_VERSION:3}-${commitsha:0:6}
+echo "azure pool: $azure_pool"
+echo "author variable: $author"
 # substrings the variable so we get the number only after the 'pr-' part.
 echo "PR Number: ${DOCKER_METADATA_OUTPUT_VERSION:3}"
 # makes the variable available in subsequent steps.
 jobcount=$(dotnet ./bin/Release/net8.0/APSIM.Workflow.dll --sim-count)
-echo "jobcount: ${jobcount}"
+echo "jobcount: $jobcount"
 if test -z "$jobcount"; then
     echo "The job count was found to be empty. Exiting."
     exit 1
