250f4b777a72812fae644f0a5877ad597c14ed69
lie112 <adam.liedloff@csiro.au>
2025-10-02 11:53:04 +0930

    CLEM fix intage required calculation

--- Models/CLEM/Resources/ExpectedActualContainer.cs
+++ Models/CLEM/Resources/ExpectedActualContainer.cs
@@ -41,7 +41,7 @@ namespace Models.CLEM.Resources
         /// <summary>
         /// Determine the amount still required to attain expected
         /// </summary>
-        public double Required { get { return Math.Max(0, Expected - Actual + Unneeded); } }
+        public double Required { get { return Math.Max(0, Expected - Actual - Unneeded); } }
 
         /// <summary>
         /// Expected or potential value required over specified time period
