34cadd16a608d7e2294eb70d6d0d7ea5b14679c1
Neil Huth <neil.huth@csiro.au>
2026-01-02 15:46:50 +1000

    Add doco for photosynthesis

--- Models/PMF/Organs/SimpleLeaf.cs
+++ Models/PMF/Organs/SimpleLeaf.cs
@@ -70,6 +70,28 @@ namespace Models.PMF.Organs
     /// T | The daily average temperature | <sup>o</sup>C
     /// 
     /// 
+    /// **Photosynthesis**
+    /// 
+    /// Potential daily photosynthesis is calculated simply using a Radiation Use Efficiency (RUE) approach.  In this approach, daily potential photosynthesis is directly proportional to the amount of total short wave radiation intercepted by the plant canopy.
+    /// The radiation use efficienty is adjusted from a base value appropriate for historical levels of atmospheric CO2 concentration (ie 350ppm - see previous section).  Daily values of potential photosynthesis are then modified for effects of plant nitrogen status, temperature and atmospheric vapour pressure deficit.  These same relative growth factors are provided to the MicroClimate model to moderate the stomatal conductance terms incorporated into the Penman-Monteith formulation.
+    /// Finally, the daily growth rate is moderated in response to the relative water supply:demand ratio (F<sub>W</sub>) to capture the effect of daily plant water status.
+    /// 
+    /// This calculation for photosynthesis is then provided to the organ arbitrator as a potential daily DM fixation supply for arbitration with all other DM supplies and demands.
+    /// 
+    /// 
+    /// ```
+    /// DMFixationSupply = RUE x PhotosynthesisCO2Modifier x Min(F<sub>T</sub>, F<sub>N</sub>, F<sub>VPD</sub>) x F<sub>W</sub>
+    /// ```
+    /// Name | Description | Units
+    /// -|-|-
+    /// RUE | Radiation Use Efficiency for potential daily growth | g/MJ/m<sup>2</sup>
+    /// F<sub>T</sub> | Relative growth rate factor for Temperature | 0-1
+    /// F<sub>N</sub> | Relative growth rate factor for Nitrogen status | 0-1
+    /// F<sub>VPD</sub> | Relative growth rate factor for Vapour Pressure Deficit  | 0-1
+    /// F<sub>W</sub> | Relative growth rate factor for plant water status | 0-1
+    /// 
+    /// where
+    /// 
     /// 
     /// </summary>
     /// <remarks>
