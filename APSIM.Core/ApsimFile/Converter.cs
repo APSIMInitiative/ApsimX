8ca6ff9d59c40f6e727f661a8feab9f298657b64
Isaiah Huber <ilhuber@iastate.edu>
2026-01-06 10:54:14 -0600

    oops forgot this

--- APSIM.Core/ApsimFile/Converter.cs
+++ APSIM.Core/ApsimFile/Converter.cs
@@ -20,7 +20,7 @@ namespace APSIM.Core;
 internal class Converter
 {
     /// <summary>Gets the latest .apsimx file format version.</summary>
-    public static int LatestVersion { get { return 208; } }
+    public static int LatestVersion { get { return 209; } }
 
     /// <summary>Converts a .apsimx string to the latest version.</summary>
     /// <param name="st">XML or JSON string to convert.</param>
