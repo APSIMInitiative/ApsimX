c65623285a604223f884ce799985e5cab465fc1b
Andrew Paroz <par456@csiro.au>
2026-02-19 16:36:44 +1000

    Starting to write new MetFile library

--- APSIM.Shared/Utilities/MetBinaryReader.cs
+++ APSIM.Shared/Utilities/MetBinaryReader.cs
@@ -92,6 +92,173 @@ namespace APSIM.Shared.Utilities
             return GetMetfileString(constants, columns, values);
         }
 
+        public static Stream Save(string filepath)
+        {
+            //byte[] fileBytes = File.ReadAllBytes(filepath);
+            //string output = Read(fileBytes);
+            //return new MemoryStream(Encoding.UTF8.GetBytes(output));
+        }
+
+        public static byte[] Write(string input)
+        {
+            //A list of all comments. These are text after ! marks, or lines that don't fit into other categories.
+            //Stored with commented text and file position so they can be restored.
+            List<BinaryData> comments = new List<BinaryData>();
+
+            //A list of tuples of constants and metadata. Stored as name and value, both as strings
+            List<StringPair> contants = new List<StringPair>();
+
+            //A list of tuples of columns. Stored as name and units, both as strings
+            List<StringPair> columns = new List<StringPair>();
+
+            //A 2d array of rows x column, where each row has a value for each column.
+            List<List<string>> data = new List<List<string>>();
+
+            //read all comments
+            List<int> commentPositions = new List<int>();
+            int length = input.Length;
+            for (int i = 0; i < length; i++)
+                if (input[i] == '!')
+                    commentPositions.Add(i);
+
+            StringBuilder builder = new StringBuilder(length);
+            int previous = 0;
+            foreach(int position in commentPositions)
+            {
+                int nextEndline = input.IndexOf('\n', position);
+                string comment = input.Substring(position, nextEndline - position);
+                comments.Add(new BinaryData(comment, position));
+                builder.Append(input.Substring(previous, position));
+                previous = nextEndline;
+            }
+            string inputWithoutComments = builder.ToString();
+
+            //Start reading file
+            string[] lines = inputWithoutComments.Split('\n');
+            bool headerRemoved = false;
+            int step = 0;
+            string columnNameLine = "";
+            string columnUnitsLine = "";
+            List<string> dataLines = new List<string>();
+            foreach(string line in lines)
+            {
+                //clean line
+                string trimmed = line.Replace('\t', ' ').Trim();
+                if (!headerRemoved && trimmed == "[weather.met.weather]")
+                    trimmed = "";
+
+                if (trimmed.Length > 0)
+                {
+                    if (step == 0) //reading constants
+                    {
+                        if (trimmed.Contains('=')) //we have a constant
+                        {
+                            string[] parts = trimmed.Split('=');
+                            contants.Add(new StringPair(parts[0], parts[1]));
+                        }
+                        else // we might have the header row, so should check
+                        {
+                            if (trimmed.Contains("radn") && trimmed.Contains("maxt") && trimmed.Contains("mint") && trimmed.Contains("rain"))
+                            {
+                                step = 1; // set this to one so the next line is read for units
+                                columnNameLine = trimmed;
+                            }
+                        }
+                    }
+                    else if (step == 1) //found header last line, reading units
+                    {
+                        step = 2; // set this to two further lines are read for data
+                        columnUnitsLine = trimmed;
+                    }
+                    else if (step == 2) //reading data
+                    {
+                        dataLines.Add(trimmed);
+                    }
+                }
+            }
+            
+            //work out our column names and units
+            string[] columnParts = columnNameLine.Split(" ");
+            List<string> columnNames = new List<string>();
+            foreach(string part in columnParts)
+                if (part.Length > 0)
+                    columnNames.Add(part);
+
+            columnParts = columnUnitsLine.Split(" ");
+            List<string> columnUnits = new List<string>();
+            foreach(string part in columnParts)
+                if (part.Length > 0)
+                    columnUnits.Add(part);
+
+            if (columnNames.Count != columnUnits.Count)
+                throw new Exception($"Cannot read met file. {columnNames.Count} column names found ({columnNames}), {columnUnits.Count} units founds. {columnUnits}");
+            
+            length = columnNames.Count;
+            for(int i = 0; i < length; i++)
+            {
+                string units = columnUnits[i];
+                if (units.StartsWith('('))
+                    units = units.Remove(0);
+                if (units.EndsWith(')'))
+                    units = units.Remove(units.Length-1);
+                columns.Add(new StringPair(columnNames[i], units));
+            }
+
+            foreach(string line in lines)
+            {
+                
+            }
+
+            /*
+
+            #add start date to constants
+            constants["start_date"] = data[0][0]
+
+            new_data = []
+            #remove date from data
+            if "date" in columns:
+                for i in range(0, len(columns)):
+                    if columns[i] == "date":
+                        index = i
+                columns.pop(index)
+                units.pop(index)
+                for row in data:
+                    new_row = row.copy()
+                    new_row.pop(0)
+                    new_data.append(new_row)
+
+            #version
+            output = BinaryMetfile.str_to_hex("met-bin-1")
+            
+            #constants
+            output += BinaryMetfile.int_to_hex(len(constants.items()))
+            for key, value in constants.items():
+                output += BinaryMetfile.str_to_hex(key)
+                output += BinaryMetfile.str_to_hex(str(value))
+
+            #headers
+            #titles
+            output += BinaryMetfile.int_to_hex(len(columns))
+            for value in columns:
+                output += BinaryMetfile.str_to_hex(value)
+            #units
+            for value in units:
+                output += BinaryMetfile.str_to_hex(value)
+
+            #data
+            output += BinaryMetfile.int_to_hex(len(new_data), 8)
+            for row in new_data:
+                for value in row:
+                    output += BinaryMetfile.encode_num(value)
+
+            #if output is an odd length
+            if len(output) % 2: 
+                output += "0"
+
+            return bytes.fromhex(output)
+            */
+        }
+
         /// <summary>
         /// Convert a hex string to an int
         /// </summary>
@@ -151,6 +318,151 @@ namespace APSIM.Shared.Utilities
 
             return output;
         }
+
+        private static BinaryData IntToHex()
+        {
+            
+        }
+
+        private static BinaryData StringToHex()
+        {
+            
+        }
+
+        private static BinaryData EncodeNumber()
+        {
+            
+        }
+
+        /*
+        @staticmethod
+    def int_to_hex(data:int, size:int = 2) -> str:
+        '''
+        Convert an int to hex string
+        Arguments:
+            data: the int value
+            size: how many digits it should be stored as. 1 hex = 4 bits
+        Returns:
+            hex string representing the bits of the given value
+        '''
+        output = format(data, '0x')
+        while(len(output) < size):
+            output = "0" + output
+
+        return output
+
+    @staticmethod
+    def str_to_hex(data:str) -> str:
+        '''
+        Convert a character string to hex string
+        Arguments:
+            data: the string value
+        Returns:
+            hex string representing the bits of the given value
+        '''
+        length = BinaryMetfile.int_to_hex(len(data))
+
+        text = ""
+        for char in data:
+            text += BinaryMetfile.int_to_hex(ord(char))
+
+        return length + text
+
+        @staticmethod
+    def encode_num(data) -> str:
+        '''
+        Convert a metfile number to hex string.
+        Number is assumed to potentially be negatives, have decimal places
+        or have other notation so is stored as list character symbols, not
+        as a number.
+        Arguments:
+            data: the value
+        Returns:
+            hex string representing the bits of the given value
+        '''
+
+        data = str(data)
+        output = ""
+
+        if data == "nan":
+            output = BinaryMetfile.int_to_hex(1)
+            output += BinaryMetfile.__symbol_hex['nan']
+
+        else:
+            #numbers ending in .0 are just the number before the decimal
+            data = data.replace(".0", "")
+            data = data.replace("-0.", "-.")
+
+            output = BinaryMetfile.int_to_hex(len(data), 1)
+            for char in data:
+                output += BinaryMetfile.__symbol_hex[char]
+
+        return output
+
+        @staticmethod
+    def write_bytes(constants:dict, columns:list, units:list, data:list) -> bytes:
+        '''
+        Given the data for a metfile, returns the metfile in binary format in bytes
+        Arguments:
+            constants: A dictionary of constants and metadata. Stored as name and value, both as strings
+            columns: A list of string column names
+            units: A list of string unit names for each column
+            data: A list of rows where each row has a value for each column. All values must be numbers.
+        Returns:
+            the metfile as bytes
+        '''
+
+        #make copies of these inputs so we can edit them as needed
+        constants = constants.copy()
+        columns = columns.copy()
+        units = units.copy()
+
+        #add start date to constants
+        constants["start_date"] = data[0][0]
+
+        new_data = []
+        #remove date from data
+        if "date" in columns:
+            for i in range(0, len(columns)):
+                if columns[i] == "date":
+                    index = i
+            columns.pop(index)
+            units.pop(index)
+            for row in data:
+                new_row = row.copy()
+                new_row.pop(0)
+                new_data.append(new_row)
+
+        #version
+        output = BinaryMetfile.str_to_hex("met-bin-1")
+        
+        #constants
+        output += BinaryMetfile.int_to_hex(len(constants.items()))
+        for key, value in constants.items():
+            output += BinaryMetfile.str_to_hex(key)
+            output += BinaryMetfile.str_to_hex(str(value))
+
+        #headers
+        #titles
+        output += BinaryMetfile.int_to_hex(len(columns))
+        for value in columns:
+            output += BinaryMetfile.str_to_hex(value)
+        #units
+        for value in units:
+            output += BinaryMetfile.str_to_hex(value)
+
+        #data
+        output += BinaryMetfile.int_to_hex(len(new_data), 8)
+        for row in new_data:
+            for value in row:
+                output += BinaryMetfile.encode_num(value)
+
+        #if output is an odd length
+        if len(output) % 2: 
+            output += "0"
+
+        return bytes.fromhex(output)
+        */
 
         /// <summary>
         /// Takes the different parts of the met file after being converted to strings and builds a normal
