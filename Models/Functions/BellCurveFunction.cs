b5971e0b23c886d4204db4d8a9d9d0675e911a19
Hamish Brown <hamish.brown@plantandfood.co.nz>
2026-03-19 14:40:39 +1300

    Change leaf size model

--- Models/Functions/BellCurveFunction.cs
+++ Models/Functions/BellCurveFunction.cs
@@ -12,23 +12,23 @@ namespace Models.Functions
     public class BellCurveFunction : Model, IFunction
     {
         /// <summary>The largest leaf position</summary>
-        [Link(Type = LinkType.Child, ByName = true)] IFunction LargestLeafPosition = null; // Node position where the largest leaf occurs (e.g. 10 is the 10th leaf from bottom to top)
+        [Link(Type = LinkType.Child, ByName = true)] IFunction X0 = null; // Node position where the largest leaf occurs (e.g. 10 is the 10th leaf from bottom to top)
         /// <summary>The area maximum</summary>
-        [Link(Type = LinkType.Child, ByName = true)] IFunction AreaMax = null;             // Area of the largest leaf of a plant (mm2)
+        [Link(Type = LinkType.Child, ByName = true)] IFunction aMax = null;             // Area of the largest leaf of a plant (mm2)
         /// <summary>The breadth</summary>
-        [Link(Type = LinkType.Child, ByName = true)] IFunction Breadth = null;
+        [Link(Type = LinkType.Child, ByName = true)] IFunction a = null;
         /// <summary>The skewness</summary>
-        [Link(Type = LinkType.Child, ByName = true)] IFunction Skewness = null;
+        [Link(Type = LinkType.Child, ByName = true)] IFunction b = null;
         /// <summary>The structure</summary>
         [Link] Structure Structure = null;
 
         /// <summary>Gets the value.</summary>
         public double Value(int arrayIndex = -1)
         {
-            double LeafNo = Structure.LeafTipsAppeared;
+            double FLN = Structure.LeafTipsAppeared;
 
-            return AreaMax.Value(arrayIndex) * Math.Exp(Breadth.Value(arrayIndex) * Math.Pow(LeafNo - LargestLeafPosition.Value(arrayIndex), 2.0)
-                                + Skewness.Value(arrayIndex) * (Math.Pow(LeafNo - LargestLeafPosition.Value(arrayIndex), 3.0)));
+            return aMax.Value(arrayIndex) * Math.Exp(a.Value(arrayIndex) * Math.Pow(FLN - X0.Value(arrayIndex), 2.0)
+                                + b.Value(arrayIndex) * (Math.Pow(FLN - X0.Value(arrayIndex), 3.0)));
         }
     }
 }
