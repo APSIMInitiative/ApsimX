03e287b80bef2eec25fdae4c9f1b91a83d020cd2
Edmar Teixeira <Edmar.Teixeira@plantandfood.co.nz>
2026-04-08 08:14:49 +1200

    Functions to create Dookie 2025 met file

--- Tests/Validation/Wheat/UoM_WheatProject/Dookie2025/_targets.R
+++ Tests/Validation/Wheat/UoM_WheatProject/Dookie2025/_targets.R
@@ -1,20 +1,26 @@
 library(targets)
 library(here)
-# Dookie 2024
+# Dookie 2025
 
 # libraries
 tar_option_set(packages = c("here", "tidyverse", "lubridate", "readxl"))
 
 # functions
-source("R/read_and_merge_phenology_observed.R")
-source("R/check_manual_params.R")
-source("R/read_csv_sowDates.R")
-source("R/find_first_stage_dates.R")
-source("R/interpolate_and_create_phenoStages.R")
-source("R/spread_and_csv_save.R")
-source("R/read_and_merge_obs_files.R")
-source("R/save_df_to_excel.R")
-source("R/check_project_dependencies.R")
+
+source("R/createWeatherFile.R")
+source("R/save_met_file.R")
+
+
+# source("R/read_and_merge_phenology_observed.R")
+# source("R/check_manual_params.R")
+# source("R/read_csv_sowDates.R")
+# source("R/find_first_stage_dates.R")
+# source("R/interpolate_and_create_phenoStages.R")
+# source("R/spread_and_csv_save.R")
+# source("R/read_and_merge_obs_files.R")
+# source("R/save_df_to_excel.R")
+# source("R/check_project_dependencies.R")
+
 
 
 # Read Wheat.Phenology.Stage from Observed (2 sets)
@@ -23,9 +29,12 @@ source("R/check_project_dependencies.R")
 # set a haun manual-input check
 
 
+# ADD NEW MET DATA TO THIS ONE!!!!!!!!!!!!!!!!!!!!!!!!!!! - Same for EVA and WWHI
+# FIND EMERGENCE AND POPULATION FOR EACH TREAT TO BE ADDED INTO APSIMX SIM
+
 #-------------------
 
-proj_name <- "Dookie2024"
+proj_name <- "Dookie2025"
 
 
 # target objects
@@ -41,11 +50,13 @@ list(
       folder_met                  = here::here("..", "met"),
       folder_apsimx               = here::here(), # a level up from where Analysis is
       folder_rawData              = here::here(proj_name),  
-      file_rawData_excel          = c("DookieEVA2024.xlsx",
-                                      "DookieWWHI2024.xlsx"), # raw observed data (pre-defined file name),
+      file_rawData_excel          = c("UOM2312-001RTX 25 DOO JH EVA WHT.xlsx", 
+                                      "UOM2312-001RTX 25 DOO JH WWHI WHT.xlsx"), # raw observed data (pre-defined file name),
+      sheetExcel_weather          = "Met Data",
+      coord_thisLatLon            = data.frame(lat = -36.39, lon = 145.70), # To check
       file_saved_obs_excel         = paste0(proj_name,"_Observed.xlsx"), #produced observed data (pre-defined file name)
       sheet_name_observed         = "Observed",
-      date_DOY_ref                = "01-01-2024", # date to transform DOY output into ddmmyy within simulations
+      date_DOY_ref                = "01-01-2025", # date to transform DOY output into ddmmyy within simulations
       btwStgFrac                  = 0.5, # fraction of time in-between two pheno-stages when we assume a missing stage 
       file_name_input_pheno       = paste0(proj_name,"_PhenoDatesInput.csv"),
       file_name_input_haun        = paste0(proj_name,"_HaunStagesInput.csv"),
@@ -56,62 +67,98 @@ list(
       file_name_cult_by_sowDate  = "CultivarBySowingDatesTemplate.csv"
         )),
   
-
-  # read observations of phenology and haun
-  tar_target(file_pheno_haun_obs, read_and_merge_phenology_observed(config$folder_rawData,
-                                               config$file_rawData_excel, 
-                                               config$sheet_name_observed,
-                                               config$cols_to_extract)),
+  # # Config: Get pre-defined simulation names per treatment from APSIM file (via APSIM-UI) - NOTE: func this might have to change with exp
+  tar_target(df_simNameByCult,read.csv2(file.path(config$folder_rawData,
+                                                  config$file_name_cult_by_sowDate),
+                                        header = TRUE, stringsAsFactors = FALSE, sep = ",")),
   
-  
-  
-  # check if haun manual-parameters are correct
-  tar_target(msg_haun_input_checked, check_manual_params(config$folder_inputs,
-                                                     config$file_name_input_haun,
-                                                     file_pheno_haun_obs)),
-    
-  # prepare the phenology input 
-  tar_target(df_sowDates_by_sim, read_csv_sowDates(config$folder_rawData,
-                                             config$file_name_cult_by_sowDate)),
-  
-   tar_target(df_pheno_start_date, 
-              find_first_stage_dates(df_sowDates_by_sim, 
-                                                 file_pheno_haun_obs, 
-                                                 sow_date_col = "SowingDate")),
-  tar_target(df_pheno_interp, 
-             interpolate_and_create_phenoStages(df_pheno_start_date, config$btwStgFrac)),
-  
-  tar_target(msg_pheno_input_saved, spread_and_csv_save(config$folder_inputs, 
-                                                               config$file_name_input_pheno, 
-                                                   df_pheno_interp)),
-  
-  tar_target(df_all_obs_files, read_and_merge_obs_files(file.path(config$folder_rawData),
-                                                        config$file_rawData_excel,
-                                                        config$sheet_name_observed)),
-  
-  tar_target(msg_obs_saved,save_df_to_excel(config$folder_apsimx,
-                                             config$file_saved_obs_excel, 
-                                             config$sheet_name_observed, 
-                                             df_all_obs_files)),
-  
-  # Post-flight dependency check for APSIM
+  # 1. Process the weather data
   tar_target(
-    name = check_depend, 
-    command = {
-      # 1. List the targets here to force `{targets}` to wait for them
-      msg_obs_saved
-      msg_haun_input_checked
-      msg_pheno_input_saved
-      
-      # 2. Now run the actual function
-      check_project_dependencies(
-        projects = config$proj_name,
-        dir_met = config$folder_met,
-        dir_inputs = config$folder_inputs,
-        dir_obs = config$folder_apsimx
-      )
-    }
+    name = processed_met_data, 
+    command = createWeatherFile(
+      thisFolder = config$folder_rawData, 
+     # thisExcelFile = config$file_rawData_excel[[2]], # data only in WWHI excel file
+     # Search the array for "WWHI". The [1] ensures it only takes the first match
+     # just in case multiple files accidentally contain that string.
+      thisExcelFile = grep("WWHI", config$file_rawData_excel, value = TRUE)[1],
+      thisSheet = config$sheetExcel_weather
+    )
+  ),
+  
+  # 2. Save the weather file
+  tar_target(
+    name = msg_met_saved,
+    command = save_met_file(
+      met_list = processed_met_data,
+      folder_path = config$folder_met,
+      file_name = paste0(config$proj_name, ".met"),
+      lat = config$coord_thisLatLon$lat,
+      lon = config$coord_thisLatLon$lon
+    ),
+    format = "file" # Track the saved output!
   )
+  
+  
+  # Find emergence dates and population
+  # # Config: Get pre-defined simulation names per treatment from APSIM file (via APSIM-UI) - NOTE: func this might have to change with exp
+  #tar_target(df_em_pop,get_em_pop())
+  # 
+  # 
+  # # read observations of phenology and haun
+  # tar_target(file_pheno_haun_obs, read_and_merge_phenology_observed(config$folder_rawData,
+  #                                              config$file_rawData_excel, 
+  #                                              config$sheet_name_observed,
+  #                                              config$cols_to_extract)),
+  # 
+  # 
+  # 
+  # # check if haun manual-parameters are correct
+  # tar_target(msg_haun_input_checked, check_manual_params(config$folder_inputs,
+  #                                                    config$file_name_input_haun,
+  #                                                    file_pheno_haun_obs)),
+  #   
+  # # prepare the phenology input 
+  # tar_target(df_sowDates_by_sim, read_csv_sowDates(config$folder_rawData,
+  #                                            config$file_name_cult_by_sowDate)),
+  # 
+  #  tar_target(df_pheno_start_date, 
+  #             find_first_stage_dates(df_sowDates_by_sim, 
+  #                                                file_pheno_haun_obs, 
+  #                                                sow_date_col = "SowingDate")),
+  # tar_target(df_pheno_interp, 
+  #            interpolate_and_create_phenoStages(df_pheno_start_date, config$btwStgFrac)),
+  # 
+  # tar_target(msg_pheno_input_saved, spread_and_csv_save(config$folder_inputs, 
+  #                                                              config$file_name_input_pheno, 
+  #                                                  df_pheno_interp)),
+  # 
+  # tar_target(df_all_obs_files, read_and_merge_obs_files(file.path(config$folder_rawData),
+  #                                                       config$file_rawData_excel,
+  #                                                       config$sheet_name_observed)),
+  # 
+  # tar_target(msg_obs_saved,save_df_to_excel(config$folder_apsimx,
+  #                                            config$file_saved_obs_excel, 
+  #                                            config$sheet_name_observed, 
+  #                                            df_all_obs_files)),
+  # 
+  # # Post-flight dependency check for APSIM
+  # tar_target(
+  #   name = check_depend, 
+  #   command = {
+  #     # 1. List the targets here to force `{targets}` to wait for them
+  #     msg_obs_saved
+  #     msg_haun_input_checked
+  #     msg_pheno_input_saved
+  #     
+  #     # 2. Now run the actual function
+  #     check_project_dependencies(
+  #       projects = config$proj_name,
+  #       dir_met = config$folder_met,
+  #       dir_inputs = config$folder_inputs,
+  #       dir_obs = config$folder_apsimx
+  #     )
+  #   }
+  # )
   
   
 )
