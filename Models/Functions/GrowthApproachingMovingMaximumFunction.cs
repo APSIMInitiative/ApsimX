64a10eb95ec560834520880a9b56c76d07a93480
Andrew Paroz <par456@csiro.au>
2026-05-19 10:50:01 +1000

    Checking if new IFunction has correct calculation from PMF structure

---
+++ Models/Functions/GrowthApproachingMovingMaximumFunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using APSIM.Core;
+using Models.Core;
+
+namespace Models.Functions
+{
+    /// <summary>
+    /// This function returns the daily delta for its child function
+    /// </summary>
+    [Serializable]
+    [ViewName("UserInterface.Views.PropertyView")]
+    [PresenterName("UserInterface.Presenters.PropertyPresenter")]
+    [Description("")]
+    public class ValueApproachingMovingMaximumFunction : Model, IFunction
+    {
+        /// <summary></summary>
+        [Link(Type = LinkType.Child)]
+        private IFunction child = null;
+
+        private double _today = 0;
+        private double _yesterday = 0;
+        private double _maximum = 0;
+
+        /// <summary>
+        /// Invoked at the end of each day. Updates the stored value.
+        /// </summary>
+        [EventSubscribe("EndOfDay")]
+        private void OnEndOfDay(object sender, EventArgs args)
+        {
+            _yesterday = _today;
+            _today = child.Value();
+        }
+
+        /// <summary>Gets the value.</summary>
+        /// <value>The value.</value>
+        public double Value(int arrayIndex = -1)
+        {
+            double delta = _today - _yesterday;
+
+            if (delta > _maximum)
+                _maximum = delta;
+
+            double deltaOverMax = delta / _maximum;
+
+            return 1 - deltaOverMax;
+        }
+    }
+}
