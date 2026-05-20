1583e711810eaaaed7d1cfaa6dbbabeb5551fb74
Andrew Paroz <par456@csiro.au>
2026-05-20 16:50:22 +1000

    Testing New Maturity

--- Models/Functions/DeltaOverMaximum.cs
+++ Models/Functions/DeltaOverMaximum.cs
@@ -5,13 +5,18 @@ using Models.Core;
 namespace Models.Functions
 {
     /// <summary>
-    /// This function returns the daily delta for its child function
+    /// This function returns change (delta) of a value from yesterday to today, over the maximum that delta that has been recorded.
+    /// When the value is requested, if that day's delta exceed the maximum, it will update the maximum to the new calculated delta.
+    /// Today and yesterday values are calculated at the start of day to prevent event ordering issues.
+    /// If Maximum is 0, will return 0 instead.
+    /// 
+    /// First used in the Cotton model.
     /// </summary>
     [Serializable]
     [ViewName("UserInterface.Views.PropertyView")]
     [PresenterName("UserInterface.Presenters.PropertyPresenter")]
     [Description("")]
-    public class ValueApproachingMovingMaximumFunction : Model, IFunction
+    public class DeltaOverMaximum : Model, IFunction
     {
         /// <summary></summary>
         [Link(Type = LinkType.Child)]
@@ -19,6 +24,7 @@ namespace Models.Functions
 
         private double _yesterday = 0;
         private double _today = 0;
+        private double _delta = 0;
         private double _maximum = 0;
 
         /// <summary>
@@ -29,19 +35,19 @@ namespace Models.Functions
         {
             _yesterday = _today;
             _today = child.Value();
+            _delta = _today - _yesterday;
+            if (_delta > _maximum)
+                _maximum = _delta;
         }
 
         /// <summary>Gets the value.</summary>
         /// <value>The value.</value>
         public double Value(int arrayIndex = -1)
         {
-            double delta = _today - _yesterday;
-
-            if (delta > _maximum)
-                _maximum = delta;
-
-            double deltaOverMax = delta / _maximum;
-            return deltaOverMax;
+            if (_maximum == 0)
+                return 0;
+            else
+                return _delta / _maximum;
         }
     }
 }
