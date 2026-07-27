83f0a57789e7b58ee7bf09c146c121f72dd8f60b
Val Snow <val.snow@agresearch.co.nz>
2026-07-27 16:42:25 +1200

    Fix error in expression of the unit conversion factors

--- Models/Soils/Nutrients/NH3Volatilisation.cs
+++ Models/Soils/Nutrients/NH3Volatilisation.cs
@@ -352,8 +352,8 @@ namespace Models.Soils.Nutrients
                 if (GasExchangeNH[z] > 0)
                 {
                     EmissionNH3[z] = NH3Gas[z] * GasExchangeNH[z] * EmissabilityNH3[z];      // mg/m2
-                    EmissionNH3[z] = EmissionNH3[z] * 10E+4;                      // mg/ha
-                    EmissionNH3[z] = EmissionNH3[z] * 10E-6;                      // kg/ha
+                    EmissionNH3[z] = EmissionNH3[z] * 1E+4;                      // mg/ha
+                    EmissionNH3[z] = EmissionNH3[z] * 1E-6;                      // kg/ha
                 }
                 else
                     EmissionNH3[z] = 0;
