63eaa40095cf643454e707e9bfe51d5cee18236d
Andrew <andrew.paroz@csiro.au>
2026-09-10 10:42:29 +1000

    Rerun stats

--- APSIM.Core/ApsimFile/Converter.cs
+++ APSIM.Core/ApsimFile/Converter.cs
@@ -8115,7 +8115,6 @@ internal class Converter
                     pasture[change.Item2] = pasture[change.Item1];
             }
         }
-
         JsonUtilities.RenameVariables(root, changes);
     }
 }
