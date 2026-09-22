69bd5120324ea003f5f16602175a17daab11f572
Andrew <andrew.paroz@csiro.au>
2026-09-22 15:22:10 +1000

    Only check if child is enabled

--- Models/Functions/FunctionUtilities.cs
+++ Models/Functions/FunctionUtilities.cs
@@ -16,8 +16,8 @@ namespace Models.Functions
         {
             IModel model = node.Model as IModel;
             foreach (Node n in node.Children)
-                if (!(n.Model is IFunction || n.Model is IText))
-                    throw new System.Exception($"{model.FullPath}: {model.GetType().Name} cannot have children that are not a Function or memo/documentation. A child of {node.Model.GetType().Name} was found.");
+                if (n.Model.Enabled && !(n.Model is IFunction || n.Model is IText))
+                    throw new System.Exception($"{model.FullPath}: {model.GetType().Name} cannot have children that are not a Function or memo/documentation. A child of {n.Model.GetType().Name} was found.");
         }
     }
 }
